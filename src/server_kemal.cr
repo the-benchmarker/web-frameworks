4a809ce5b2a84a1804495bca179931b36b552331
Serdar Dogruyol <dogruyolserdar@gmail.com>
2017-03-29 16:55:59 +0300

    Remove redundant status code
    Kemal already returns `200` by default so no need to set

--- src/server_kemal.cr
+++ src/server_kemal.cr
@@ -1,17 +1,14 @@
 require "kemal"
 
 get "/" do |env|
-  env.response.status_code = 200
   nil
 end
 
 get "/user/:id" do |env|
-  env.response.status_code = 200
   env.params.url["id"]
 end
 
 post "/user" do |env|
-  env.response.status_code = 200
   nil
 end
 
