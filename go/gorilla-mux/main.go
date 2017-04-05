c26ecfe05b7e8f66bfd4fce56b841d399bd66730
tbrand <taichiro0709@gmail.com>
2017-04-06 00:01:25 +0900

    gorilla/mux

---
+++ go/gorilla-mux/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method == "GET" {
+		w.Write([]byte(""))
+	}
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method == "GET" {
+		vars := mux.Vars(r)
+		w.Write([]byte(vars["id"]))
+	}
+}
+
+func RegisterUserHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method == "POST" {
+		w.Write([]byte(""))
+	}
+}
+
+func main(){
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", IndexHandler)
+	r.HandleFunc("/user/{id}", UserHandler)
+	r.HandleFunc("/user", RegisterUserHandler)
+
+	http.Handle("/", r)
+	http.ListenAndServe("localhost:3000", r)
+}
