0a50542f536afa387d0c9722a113a5562f324f66
Kilian Koeltzsch <me@kilian.io>
2017-05-07 14:26:45 +0200

    add basic rocket project

---
+++ rust/rocket/src/main.rs
@@ -0,0 +1,27 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+use rocket::config::{Config, Environment};
+
+#[get("/")]
+fn index_route() {}
+
+#[get("/user/<id>")]
+fn user_route(id: &str) -> String {
+    format!("{}", id)
+}
+
+#[post("/user")]
+fn user_register_route() {}
+
+fn main() {
+    let config = Config::build(Environment::Production)
+        .address("127.0.0.1")
+        .port(3000)
+        .finalize()
+        .unwrap();
+    let app = rocket::custom(config, false);
+    app.mount("/", routes![index_route, user_route, user_register_route]).launch();
+}
