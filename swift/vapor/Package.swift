0e3c5bcbe6aca73e050053d9ec53f913b24487b1
Kilian Koeltzsch <me@kilian.io>
2017-05-07 18:21:09 +0200

    add swift/vapor server

---
+++ swift/vapor/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "server_swift_vapor",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+    ]
+)
