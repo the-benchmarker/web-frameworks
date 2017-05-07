9720930a47cb84494e34a3f3849a56e69d0a7562
Kilian Koeltzsch <me@kilian.io>
2017-05-07 20:51:14 +0200

    use most current stable vapor version

--- swift/vapor/Package.swift
+++ swift/vapor/Package.swift
@@ -5,6 +5,6 @@ import PackageDescription
 let package = Package(
     name: "server_swift_vapor",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
     ]
 )
