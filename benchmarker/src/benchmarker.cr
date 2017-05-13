b9f6c1aa5d06e12928ecad70496d2bf9c510a18c
tbrand <taichiro0709@gmail.com>
2017-05-14 00:21:14 +0900

    fix

--- benchmarker/src/benchmarker.cr
+++ benchmarker/src/benchmarker.cr
@@ -65,7 +65,7 @@ class ExecServer
       Process.run("bash #{path} stop", shell: true)
     end
     # kill the server_lang_framework @process AND it's children
-    Process.run("pkill -9 -P #{@process.pid}", shell: true)
+    Process.run("pkill -9 -g $(ps -o pgid= #{@process.pid} | grep -o [0-9]*)", shell: true)
   end
 end
 
