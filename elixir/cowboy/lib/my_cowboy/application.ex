19e1ecb841a91ef68e5f29d3ed2fb159f5214643
idi-ot <yacht@idiot.trade>
2017-05-31 22:08:38 +0300

    update cowboy opts again

--- elixir/cowboy/lib/my_cowboy/application.ex
+++ elixir/cowboy/lib/my_cowboy/application.ex
@@ -2,8 +2,11 @@ defmodule MyCowboy.Application do
   @moduledoc false
 
   def start(_type, _args) do
-    opts = [max_connections: 16_384, acceptors: 100, port: 3000]
     dispatch = :cowboy_router.compile([{:_, [{:_, MyCowboy.Handler, []}]}])
-    {:ok, _} = :cowboy.start_clear(:http, 100, opts, %{env: %{dispatch: dispatch}})
+    {:ok, _} = :cowboy.start_clear(:http, 100, [port: 3000], %{
+      env: %{dispatch: dispatch},
+      max_connections: 16_384,
+      acceptors: 100
+    })
   end
 end
