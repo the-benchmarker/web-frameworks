4826980836275f82fc440cceb82451370c34fcfa
idi-ot <yacht@idiot.trade>
2017-06-01 00:04:25 +0300

    separate cowboy1 and cowboy2 in benchmarker and fix elli

--- elixir/elli/lib/my_elli/application.ex
+++ elixir/elli/lib/my_elli/application.ex
@@ -1,7 +1,15 @@
 defmodule MyElli.Application do
   @moduledoc false
+  use Application
 
   def start(_type, _args) do
-    {:ok, _} = :elli.start_link(callback: MyElli.Callback, port: 3000)
+    import Supervisor.Spec
+
+    children = [
+      worker(:elli, [[callback: MyElli.Callback, port: 3000]])
+    ]
+
+    opts = [strategy: :one_for_one, name: __MODULE__.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
