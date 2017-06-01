e7370fca2aeb9bad9c0641a426d924f929a02dd8
idi-ot <yacht@idiot.trade>
2017-06-01 20:58:35 +0300

    simplify supervisor

--- elixir/cowboy2/lib/my_cowboy/application.ex
+++ elixir/cowboy2/lib/my_cowboy/application.ex
@@ -3,17 +3,6 @@ defmodule MyCowboy.Application do
   use Application
 
   def start(_type, _args) do
-    nb_acceptors = 100
-    trans_opts = [port: 3000]
-    proto_opts = %{max_connections: 16_384,
-                   max_keepalive: 5_000_000,
-                   stream_handlers: [MyCowboy.StreamHandler]}
-
-    children = [{{:ranch_listener_sup, MyRanch},
-                 {:cowboy, :start_clear, [MyCowboy, nb_acceptors, trans_opts, proto_opts]},
-                :permanent, :infinity, :supervisor, [:ranch_listener_sup]}]
-
-    opts = [strategy: :one_for_one, name: __MODULE__.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, _pid} = :cowboy.start_clear(:http, 100, trans_opts, proto_opts)
   end
 end
