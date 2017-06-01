5910c0d89659e85353bfd41042ba836393f22386
idi-ot <yacht@idiot.trade>
2017-06-01 21:01:19 +0300

    simplify supervisor

--- elixir/cowboy2/lib/my_cowboy/application.ex
+++ elixir/cowboy2/lib/my_cowboy/application.ex
@@ -3,6 +3,12 @@ defmodule MyCowboy.Application do
   use Application
 
   def start(_type, _args) do
+    nb_acceptors = 100
+    trans_opts = [port: 3000]
+    proto_opts = %{max_connections: 16_384,
+                   max_keepalive: 5_000_000,
+                   stream_handlers: [MyCowboy.StreamHandler]}
+
     {:ok, _pid} = :cowboy.start_clear(:http, 100, trans_opts, proto_opts)
   end
 end
