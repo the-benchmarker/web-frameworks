7c17e4545a8abc6af4f245c8a31dd4a9012fcb62
greed2411 <jaiimmortal@gmail.com>
2018-02-24 18:47:39 +0530

    add the scripts

---
+++ python/tornado/server_python_tornado.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define('port', default=8000, help='run on the given port', type=int)
+
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write('')
+
+
+class UserHandler(tornado.web.RequestHandler):
+
+    def post(self):
+        self.write('')
+
+
+class UserInfoHandler(tornado.web.RequestHandler):
+
+    def get(self, id):
+        self.write(id)
+
+
+if __name__ == '__main__':
+    options.logging = None
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r'/', MainHandler),
+                                  (r"/user", UserHandler),
+                                  (r"/user/(\d+)", UserInfoHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
