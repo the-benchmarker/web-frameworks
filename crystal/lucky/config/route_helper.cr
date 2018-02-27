08b54246ef83e23e949449c86a5e509d7285bf1d
Marwan <waghanza@gmail.com>
2018-02-27 15:35:49 +0100

    use production mode

--- crystal/lucky/config/route_helper.cr
+++ crystal/lucky/config/route_helper.cr
@@ -1,10 +1,5 @@
 # This is used when generating URLs for your application
 Lucky::RouteHelper.configure do
-  if Lucky::Env.production?
-    # Example: https://my_app.com
-    settings.domain = ENV.fetch("APP_DOMAIN")
-  else
-    # Set domain to the default host/port in development
-    settings.domain = "http:://localhost:3001"
-  end
+  # Set domain to the default host/port in development
+  settings.domain = "http:://localhost:3001"
 end
--- crystal/lucky/config/server.cr
+++ crystal/lucky/config/server.cr
@@ -1,17 +1,5 @@
 Lucky::Server.configure do
-  if Lucky::Env.production?
-    settings.secret_key_base = secret_key_from_env
-  else
-    settings.secret_key_base = "u4PWnhZfOFXdTOtoiSBF+6jn0zHbYS6/yumo3WXYNSw="
-  end
+  settings.secret_key_base = "u4PWnhZfOFXdTOtoiSBF+6jn0zHbYS6/yumo3WXYNSw="
   settings.host = "0.0.0.0"
   settings.port = (ENV["PORT"]? || 3000).to_i
-end
-
-private def secret_key_from_env
-  ENV["SECRET_KEY_BASE"]? || raise_missing_secret_key_in_production
-end
-
-private def raise_missing_secret_key_in_production
-  raise "Please set the SECRET_KEY_BASE environment variable. You can generate a secret key with 'lucky gen.secret_key'"
 end
