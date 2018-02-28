b2d1c9601c07a758465b9b5c126f80ab9737a0d1
Marwan <waghanza@gmail.com>
2018-02-28 16:07:36 +0100

    show official github repo for rack-routing

--- tools/src/benchmarker.cr
+++ tools/src/benchmarker.cr
@@ -13,7 +13,7 @@ LANGS = [
     {name: "rails", repo: "rails/rails"},
     {name: "sinatra", repo: "sinatra/sinatra"},
     {name: "roda", repo: "jeremyevans/roda"},
-    {name: "rack-routing", repo: "georgeu2000/rack-routing"},
+    {name: "rack-routing", repo: "iAmPlus/rack-routing"},
   ]},
   {lang: "crystal", targets: [
     {name: "kemal", repo: "kemalcr/kemal"},
