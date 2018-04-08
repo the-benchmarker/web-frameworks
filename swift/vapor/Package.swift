c5e67b16c2ad317808f8f45f56851d044eaf2348
Marwan Rabbâa <waghanza@gmail.com>
2018-04-08 16:29:05 +0200

    update to last major

--- swift/vapor/Package.swift
+++ swift/vapor/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "server_swift_vapor",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/vapor.git",  majorVersion: 2, minor: 4)
     ],
     exclude: [
         "Config",
