e816a1378d8d0908f2dab2e69e14d38b8d028682
Thiago <thia.mdossantos@gmail.com>
2018-04-28 10:54:47 -0300

    feat: add fastify

---
+++ node/fastify/app.js
@@ -0,0 +1,26 @@
+const fastify = require('fastify')
+const app = fastify()
+
+app.get('/', function(request, reply) {
+  reply.send()
+})
+
+const opts = {
+  schema: {
+    response: {
+      200: {
+        type: 'string'
+      }
+    }
+  }
+}
+
+app.get('/user/:id', opts, function(request, reply) {
+  reply.send(request.params.id)
+})
+
+app.post('/user', function(request, reply) {
+  reply.send()
+})
+
+app.listen(3000, function() {})
