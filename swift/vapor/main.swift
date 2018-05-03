3ad4991a1fada3247f4b986cca88647ee90c2527
tanner0101 <me@tanner.xyz>
2018-05-03 17:14:47 -0400

    fulfull benchmark route reqs for vapor 3

--- swift/vapor/main.swift
+++ swift/vapor/main.swift
@@ -4,8 +4,15 @@ var services = Services.default()
 services.register(MiddlewareConfig())
 
 let router = EngineRouter.default()
-router.get("ping") { req in
-    return "pong" as StaticString
+let empty = HTTPResponse()
+router.get("/") { req in
+    return empty
+}
+router.get("user", String.parameter) { req in
+	return try req.parameters.next(String.self)
+}
+router.post("user") { req in
+	return empty
 }
 services.register(router, as: Router.self)
 
