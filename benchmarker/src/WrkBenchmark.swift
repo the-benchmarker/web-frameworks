4bcccfe28a2a63d0b7a00f5190b20b77fad042d3
tanner0101 <me@tanner.xyz>
2018-05-03 21:25:04 -0400

    benchmarker + docker updates

--- benchmarker/src/WrkBenchmark.swift
+++ benchmarker/src/WrkBenchmark.swift
@@ -2,34 +2,47 @@ import Vapor
 
 final class WrkBenchmark: Command {
     let arguments: [CommandArgument] = [.argument(name: "hostname")]
-    let options: [CommandOption] = [.value(name: "runs", short: "r")]
+    let options: [CommandOption] = [
+        .value(name: "runs", short: "r"),
+        .value(name: "threads", short: "t"),
+        .value(name: "connections", short: "c"),
+        .value(name: "duration", short: "d"),
+    ]
     let help: [String] = []
 
     func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
         var runs: [WrkRun] = []
         let hostname = try context.argument("hostname")
         let runCount = context.options["runs"].flatMap(Int.init) ?? 1
+        let duration = context.options["duration"] ?? "5"
+        let threads = context.options["threads"] ?? "4"
+        let connections = context.options["connections"] ?? "128"
 
         let frameworks = try Framework.readAll()
-
         let client = try context.container.make(Client.self)
 
         context.console.output("testing...".consoleText(.success))
-        return frameworks.enumerated().map { (i, framework) -> Future<Void> in
-            context.console.print("[\(i+1)/\(frameworks.count)] \(framework) ", newLine: false)
-            return client.get("http://\(hostname):\(framework.port)/").map { res -> Void in
-                context.console.output(res.description.consoleText())
-                if res.http.contentType != .plainText {
-                    context.console.output("⚠️ wrong content type")
-                }
-                if res.http.headers.firstValue(name: .date) == nil {
-                    context.console.output("⚠️ missing date header")
-                }
-                if res.http.body.count != 4 {
-                    context.console.output("⚠️ wrong body size")
+        return frameworks.enumerated().map { (i, framework) -> () -> Future<Void> in
+            return {
+                return client.get("http://\(hostname):\(framework.port)/").map { res -> Response in
+                    context.console.print("[\(i+1)/\(frameworks.count)] \(framework)")
+                    return res
+                }.map { res -> Void in
+                    if res.http.status != .ok {
+                        context.console.print("⚠️  invalid response status")
+                    }
+                    if res.http.headers.firstValue(name: .date) == nil {
+                        context.console.print("⚠️  missing date header")
+                    }
+                    if res.http.body.count != 0 {
+                        context.console.print("⚠️  body size should be zero")
+                    }
+                }.mapIfError { error in
+                    context.console.print("❌  could not connect: \(error)")
                 }
             }
-        }.flatten(on: context.container).map {
+        }.syncFlatten(on: context.container).map {
+            context.console.print("\(runCount) runs: wrk -t \(threads) -c \(connections) -d \(duration)")
             context.console.output("benchmarking...".consoleText(.success))
             for (i, framework) in frameworks.enumerated() {
                 var results: [WrkResult] = []
@@ -37,7 +50,7 @@ final class WrkBenchmark: Command {
                 context.console.print("[\(i+1)/\(frameworks.count)] \(framework) ", newLine: false)
                 for _ in 1...runCount {
                     do {
-                        let result = try self.wrk(hostname: hostname, port: framework.port)
+                        let result = try self.wrk(hostname: hostname, port: framework.port, threads: threads, connections: connections, duration: duration)
                         results.append(result)
                         context.console.print(".", newLine: false)
                     } catch {
@@ -50,13 +63,13 @@ final class WrkBenchmark: Command {
                 runs.append(run)
             }
 
-            context.console.report(\.requests, title: "total requests", suffix: " reqs", runs: runs)
-            context.console.report(\.latency.mean, title: "average latency", suffix: "µs", runs: runs)
+            context.console.report(\.requests, title: "total requests", suffix: " reqs", op: >, runs: runs)
+            context.console.report(\.latency.mean, title: "average latency", suffix: "µs", op: <, runs: runs)
         }
     }
 
-    func wrk(hostname: String, port: Int) throws -> WrkResult {
-        let res = try Process.execute("wrk", "-s", "wrk-report.lua", "-t", "4", "-c", "128", "-d", "1", "http://\(hostname):\(port)/ping")
+    func wrk(hostname: String, port: Int, threads: String, connections: String, duration: String) throws -> WrkResult {
+        let res = try Process.execute("wrk", "-s", "wrk-report.lua", "-t", threads, "-c", connections, "-d", duration, "http://\(hostname):\(port)/ping")
         let parts = res.split(separator: "$", maxSplits: 1).map(String.init)
         guard parts.count == 2 else {
             throw "no $ found in wrk output"
@@ -68,9 +81,11 @@ final class WrkBenchmark: Command {
 // MARK: Report
 
 extension Console {
-    func report(_ stat: KeyPath<WrkResult, Int>, title: String, suffix: String, runs: [WrkRun]) {
+    func report(_ stat: KeyPath<WrkResult, Int>, title: String, suffix: String, op: (Int, Int) -> Bool, runs: [WrkRun]) {
         let sorted = runs.sorted {
-            $0.results.map { $0[keyPath: stat] }.average > $1.results.map { $0[keyPath: stat] }.average
+            let l = $0.results.map { $0[keyPath: stat] }.average
+            let r = $1.results.map { $0[keyPath: stat] }.average
+            return op(l, r)
         }
         output("\(title):".consoleText(.success))
         for (i, run) in sorted.enumerated() {
--- frameworks/python/flask/server.py
+++ frameworks/python/flask/server.py
@@ -27,4 +27,4 @@ def user():
 
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(port=8006)
