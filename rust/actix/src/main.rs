1d21ec33266a7c0c42d2e91988f4135539a96957
Nikolay Kim <fafhrd91@gmail.com>
2018-05-08 16:13:21 -0700

    use actix-web 0.6 release

--- rust/actix/src/main.rs
+++ rust/actix/src/main.rs
@@ -6,17 +6,18 @@ use actix_web::*;
 fn main() {
     let sys = actix::System::new("test");
 
-    HttpServer::new(
-        || Application::default()
-            .resource("/",
-                      |r| r.f(|_| httpcodes::HTTPOk))
-            .resource("/user",
-                      |r| r.f(|_| httpcodes::HTTPOk))
-            .resource("/user/{id}",
-                      |r| r.f(|req| {
-                          let id = Binary::from_slice(req.match_info()["id"].as_ref());
-                          httpcodes::HTTPOk.with_body(id)})))
-        .bind("0.0.0.0:3000").unwrap()
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(|r| HttpResponse::build_from(&r)))
+            .resource("/user", |r| r.f(|r| HttpResponse::build_from(&r)))
+            .resource("/user/{id}", |r| {
+                r.f(|req| {
+                    let id = Binary::from_slice(req.match_info()["id"].as_ref());
+                    HttpResponse::build_from(&req).body(id)
+                })
+            })
+    }).bind("0.0.0.0:3000")
+        .unwrap()
         .start();
 
     println!("Started http server: 0.0.0.0:3000");
