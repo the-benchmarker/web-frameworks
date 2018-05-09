0f179227dfe3bcbbdeaf3e8cd70da7ea03150d65
Nikolay Kim <fafhrd91@gmail.com>
2018-05-09 06:44:20 -0700

    use actix-web 0.6 release (#216)
    * use actix-web 0.6 release
    
    * specify that actix is not use but actix-web => the web framework based one it

---
+++ rust/actix-web/src/main.rs
@@ -0,0 +1,25 @@
+extern crate actix;
+extern crate actix_web;
+
+use actix_web::*;
+
+fn main() {
+    let sys = actix::System::new("test");
+
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(|r| HttpResponse::build_from(&r)))
+            .resource("/user", |r| r.f(|r| HttpResponse::build_from(&r)))
+            .resource("/user/{id}", |r| {
+                r.f(|req| {
+                    let id = Binary::from_slice(req.match_info()["id"].as_ref());
+                    HttpResponse::build_from(&req).body(id)
+                })
+            })
+    }).bind("0.0.0.0:3000")
+        .unwrap()
+        .start();
+
+    println!("Started http server: 0.0.0.0:3000");
+    let _ = sys.run();
+}
