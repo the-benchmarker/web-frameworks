ae2b6ab9671c3e7abffdab5d1a8e933e19f52473
Marwan Rabbâa <waghanza@gmail.com>
2018-06-11 17:02:30 +0200

    re-enable rayo

--- tools/src/benchmarker.cr
+++ tools/src/benchmarker.cr
@@ -76,6 +76,7 @@ LANGS = [
     {name: "express", repo: "expressjs/express"},
     {name: "fastify", repo: "fastify/fastify"},
     {name: "polka", repo: "lukeed/polka"},
+    {name: "rayo", repo: "GetRayo/rayo.js"},
   ]},
   {lang: "elixir", targets: [
     {name: "plug", repo: "elixir-lang/plug"},
