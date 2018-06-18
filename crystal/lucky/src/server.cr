f962789181ba22f3faf0b383197b18d64434e0cd
Marwan Rabbâa <waghanza@gmail.com>
2018-06-18 22:29:31 +0200

    upgrade lucky to latest version

--- crystal/lucky/src/server.cr
+++ crystal/lucky/src/server.cr
@@ -3,12 +3,13 @@ require "./app"
 host = Lucky::Server.settings.host
 port = Lucky::Server.settings.port
 
-server = HTTP::Server.new(host, port, [
+server = HTTP::Server.new([
   Lucky::HttpMethodOverrideHandler.new,
   Lucky::ErrorHandler.new(action: Errors::Show),
   Lucky::RouteHandler.new,
 ])
 
 puts "Listening on http://#{host}:#{port}"
+server.bind_tcp(host, port, true)
 
 server.listen
