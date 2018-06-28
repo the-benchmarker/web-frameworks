be635245459d4548fdabe851862ece52d228a444
Marwan Rabbâa <waghanza@gmail.com>
2018-06-28 13:46:15 +0200

    use gunicorn as application server (tornado)

--- python/tornado/server.py
+++ python/tornado/server.py
@@ -1,40 +1,26 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from tornado.web import Application, RequestHandler, asynchronous
+from tornado.ioloop import IOLoop
 
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
-
-from tornado.options import define, options
-define('host', default='0.0.0.0', help="TCP server host")
-define('port', default=3000, help='run on the given port', type=int)
-
-
-class MainHandler(tornado.web.RequestHandler):
+class MainHandler(RequestHandler):
 
     def get(self):
         self.write('')
 
 
-class UserHandler(tornado.web.RequestHandler):
+class UserHandler(RequestHandler):
 
     def post(self):
         self.write('')
 
 
-class UserInfoHandler(tornado.web.RequestHandler):
+class UserInfoHandler(RequestHandler):
 
     def get(self, id):
         self.write(id)
 
 
-if __name__ == '__main__':
-    options.logging = None
-    tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r'/', MainHandler),
-                                  (r"/user", UserHandler),
-                                  (r"/user/(\d+)", UserInfoHandler)])
-    http_server = tornado.httpserver.HTTPServer(app)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+app = Application([
+    (r"/", MainHandler),
+    (r"/user", UserHandler),
+    (r"/user/(\d+)", UserInfoHandler)
+])
