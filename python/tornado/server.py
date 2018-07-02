2c89d047c7e2de2bb2e4a45d8120e5c864c6b607
Marwan Rabbâa <waghanza@gmail.com>
2018-07-02 11:04:40 +0200

    do not use gunicorn for tornado

--- python/tornado/server.py
+++ python/tornado/server.py
@@ -1,26 +1,41 @@
-from tornado.web import Application, RequestHandler, asynchronous
-from tornado.ioloop import IOLoop
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-class MainHandler(RequestHandler):
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define('host', default='0.0.0.0', help="TCP server host")
+define('port', default=3000, help='run on the given port', type=int)
+
+
+class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
         self.write('')
 
 
-class UserHandler(RequestHandler):
+class UserHandler(tornado.web.RequestHandler):
 
     def post(self):
         self.write('')
 
 
-class UserInfoHandler(RequestHandler):
+class UserInfoHandler(tornado.web.RequestHandler):
 
     def get(self, id):
         self.write(id)
 
 
-app = Application([
-    (r"/", MainHandler),
-    (r"/user", UserHandler),
-    (r"/user/(\d+)", UserInfoHandler)
-])
+if __name__ == '__main__':
+    options.logging = None
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r'/', MainHandler),
+                                  (r"/user", UserHandler),
+                                  (r"/user/(\d+)", UserInfoHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.bind(options.port)
+    http_server.start(0)
+    tornado.ioloop.IOLoop.current().start()
