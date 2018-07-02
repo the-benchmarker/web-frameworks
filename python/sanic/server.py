2faf0873e12adb5df512a49398ed047552519d42
Marwan Rabbâa <waghanza@gmail.com>
2018-07-02 17:54:31 +0200

    use gunicorn for sanic

--- python/sanic/server.py
+++ python/sanic/server.py
@@ -1,8 +1,5 @@
 from sanic import Sanic
 from sanic.response import text
-from signal import signal, SIGINT
-import asyncio
-import uvloop
 
 app = Sanic(log_config=None)
 
@@ -20,14 +17,3 @@ async def user_info(request, id):
 @app.route("/user", methods=['POST'])
 async def user(request):
     return text('')
-
-if __name__ == "__main__":
-    asyncio.set_event_loop(uvloop.new_event_loop())
-    server = app.create_server(host="0.0.0.0", port=3000)
-    loop = asyncio.get_event_loop()
-    task = asyncio.ensure_future(server)
-    signal(SIGINT, lambda s, f: loop.stop())
-    try:
-        loop.run_forever()
-    except:
-        loop.stop()
