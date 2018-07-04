4f1d80daf06a6889fb268d0dad18bf9598b8eb67
Marwan Rabbâa <waghanza@gmail.com>
2018-07-04 16:24:12 +0200

    use default worker numbers on vibora (more performing)

--- python/vibora/server.py
+++ python/vibora/server.py
@@ -17,4 +17,4 @@ async def user():
     return Response(b'')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, workers=multiprocessing.cpu_count(), debug=False)
+    app.run(host='0.0.0.0', port=3000, debug=False)
