40222c201ffc1da5ef2de430cd05b85ade67efe8
Marwan Rabbâa <waghanza@gmail.com>
2018-07-04 14:53:04 +0200

    remove headers specifications on vibora implementations

--- python/vibora/server.py
+++ python/vibora/server.py
@@ -5,16 +5,16 @@ app = Vibora()
 
 @app.route('/')
 async def index():
-    return Response(b'', headers={'content-type': 'html'})
+    return Response(b'')
 
 @app.route("/user/<id>", methods=['GET'])
 async def user_info(id : int):
-    return Response(f'{id}', headers={'content-type': 'html'})
+    return Response(f'{id}')
 
 
 @app.route("/user", methods=['POST'])
 async def user():
-    return Response(b'', headers={'content-type': 'html'})
+    return Response(b'')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, workers=multiprocessing.cpu_count(), debug=False)
