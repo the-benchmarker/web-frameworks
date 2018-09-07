0136a1df86ce65e9281e4b8e64721ab1762d7011
Jaivarsan <jaiimmortal@gmail.com>
2018-09-07 19:57:29 +0530

    adding quart (#351)
    * added quart
    
    * add job config for quart
    
    * use hypercorn to deploy quart

---
+++ python/quart/server.py
@@ -0,0 +1,18 @@
+from quart import Quart
+
+app = Quart(__name__)
+
+
+@app.route('/')
+async def index():
+    return ''
+
+
+@app.route('/user/<id>')
+async def user_info(id):
+    return id
+
+
+@app.route('/user', methods=['POST'])
+async def user():
+    return ''
