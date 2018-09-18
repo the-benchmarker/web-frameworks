99944515baff369a48ae918b16f6f8ee71d7034f
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-09-19 02:25:41 +0800

    fix(go): coding style (#379)

--- go/gorilla-mux/main.go
+++ go/gorilla-mux/main.go
@@ -2,34 +2,29 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method == "GET" {
-		w.Write([]byte(""))
-	}
-}
-
-func UserHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method == "GET" {
-		vars := mux.Vars(r)
-		w.Write([]byte(vars["id"]))
-	}
-}
-
-func RegisterUserHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method == "POST" {
-		w.Write([]byte(""))
-	}
-}
-
-func main(){
+func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", IndexHandler)
-	r.HandleFunc("/user/{id}", UserHandler)
-	r.HandleFunc("/user", RegisterUserHandler)
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			w.Write([]byte(""))
+		}
+	})
+	r.HandleFunc("/user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			vars := mux.Vars(r)
+			w.Write([]byte(vars["id"]))
+		}
+	})
+	r.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			w.Write([]byte(""))
+		}
+	})
 
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", r)
