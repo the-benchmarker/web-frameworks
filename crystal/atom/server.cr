0594a477703744a6e1073b1e5587a2a83f0bfc61
Marwan Rabbâa <waghanza@gmail.com>
2018-10-09 12:02:04 +0200

    update to 0.5 (alpha)

--- crystal/atom/server.cr
+++ crystal/atom/server.cr
@@ -1,24 +1,35 @@
-require "prism"
+require "atom-web"
 
 struct UserAction
-  include Prism::Action
-  include Prism::Action::Params
+  include Atom::Action
 
   params do
     type id : Int32
   end
 
   def call
-    text(params[:id])
+    text(id)
   end
 end
 
-router = Prism::Router.new do
+router = Atom::Handlers::Router.new do
   get "/"
   post "/user"
   get "/user/:id", UserAction
 end
 
-server = Prism::Server.new([router])
+logger = Logger.new(STDOUT, Logger::DEBUG)
+request_logger = Atom::Handlers::RequestLogger.new(logger)
+
+server = HTTP::Server.new([request_logger, router]) do |context|
+  if proc = context.proc
+    proc.call(context)
+  else
+    context.response.respond_with_error("Not Found: #{context.request.path}", 404)
+  end
+rescue ex : Params::Error
+  context.response.respond_with_error(ex.message, 400)
+end
+
 server.bind_tcp("0.0.0.0", 3000, reuse_port: true)
 server.listen
