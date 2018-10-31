05ad277cc13a2d187a3ef6c4ed4d1971e81019ab
Marwan Rabbâa <waghanza@gmail.com>
2018-10-31 11:38:47 +0100

    introduce falcon

---
+++ python/falcon/server.py
@@ -0,0 +1,23 @@
+from meinheld import patch
+patch.patch_all()
+
+import falcon
+
+
+class EmptyResponse:
+    def on_get(self, req, resp):
+        resp.data = bytes("", 'utf8')
+
+    def on_post(self, req, resp):
+        resp.data = bytes("", 'utf8')
+
+
+class StringResponse:
+    def on_get(self, req, resp, id):
+        resp.data = bytes(id, 'utf8')
+
+
+app = falcon.API()
+app.add_route('/', EmptyResponse())
+app.add_route('/user', EmptyResponse())
+app.add_route('/user/{id}', StringResponse())
