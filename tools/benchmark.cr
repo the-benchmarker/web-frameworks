c60464d3e1f5479c752a400dcc49ea4fd3510642
Marwan Rabbâa <waghanza@gmail.com>
2018-10-31 13:12:07 +0100

    add simple exporter

--- tools/benchmark.cr
+++ tools/benchmark.cr
@@ -93,6 +93,29 @@ def benchmark(host, threads, connections, duration) : Hash(String, Hash(String, 
   return results
 end
 
+record Target, lang : String, name : String, link : String, version : String, langver : String
+record Filter, req : Float64, lat : Float64
+record Ranked, res : Filter, target : Target
+
+def frameworks : Array(Target)
+  targets = [] of Target
+
+  YAML.parse(File.read("FRAMEWORKS.yml")).as_h.each do |lang, data|
+    data.as_h.each do |framework, line|
+      row = line.as_h
+      if row.has_key?("github")
+        link = "github.com/#{row["github"]}"
+      elsif row.has_key?("website")
+        link = row["website"]
+      end
+      target = Target.new(lang.as_s, framework.as_s, "http://#{link.to_s}", row["version"].to_s, row["language"].to_s)
+      targets.push(target)
+    end
+  end
+
+  targets
+end
+
 class App < Admiral::Command
   class Extract < Admiral::Command
     define_flag language : String, description: "language selected, to set-up environment", required: true, short: l
@@ -107,7 +130,25 @@ class App < Admiral::Command
     end
   end
 
+  class Export < Admiral::Command
+    def run
+      results = Kiwi::FileStore.new("results.db")
+      frameworks.each do |framework|
+        raw = results.get("#{framework.lang}:#{framework.name}")
+
+        if raw.nil?
+          next STDERR.puts "Skipping export for #{framework.name} since no metrics"
+        end
+
+        result = Result.from_json(raw)
+
+        puts "| %s (%s) | [%s](%s) (%s) | %.2f ms | %.2f ms | %.2f ms | %.2f ms | %.2f ms | %.2f | " % [framework.lang, framework.langver, framework.name, framework.link, framework.version, (result.latency.average/1000), (result.percentile.fifty/1000), (result.percentile.ninety/1000), (result.percentile.ninety_nine/1000), (result.percentile.ninety_nine_ninety/1000), (result.latency.deviation)]
+      end
+    end
+  end
+
   register_sub_command extract : Extract, description "Get result and store them"
+  register_sub_command export : Export, description "Export all availables results"
 
   def run
     puts "help"
