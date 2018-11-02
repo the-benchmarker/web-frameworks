b8d4f2f616cb8a0bb7f79a74f14976e57cfdb0ab
Peter Ohler <peter@ohler.com>
2018-11-02 02:57:33 -0400

    Changed format to match what is desired by the owner

--- ruby/agoo/app.rb
+++ ruby/agoo/app.rb
@@ -2,30 +2,32 @@
 require 'agoo'
 
 Agoo::Log.configure(dir: '',
-		    console: true,
-		    classic: true,
-		    colorize: true,
-		    states: {
-		      INFO: false,
-		      DEBUG: false,
-		      connect: false,
-		      request: false,
-		      response: false,
-		      eval: false,
-		      push: false,
-		    })
+                    console: true,
+                    classic: true,
+                    colorize: true,
+                    states: {
+                      INFO: false,
+                      DEBUG: false,
+                      connect: false,
+                      request: false,
+                      response: false,
+                      eval: false,
+                      push: false,
+                    })
 
 Agoo::Server.init(3000, '.', thread_count: 0, worker_count: 4)
 
+# Empty response.
 class Empty
-  def self.call(req)
-    [ 200, { }, [ ] ]
+  def self.call(_req)
+    [200, {}, []]
   end
 end
 
+# Reflects the id as the returned value.
 class Reflect
   def self.call(req)
-    [ 200, { }, [ req['PATH_INFO'][6..-1] ] ]
+    [200, {}, [req['PATH_INFO'][6..-1]]]
   end
 end
 
@@ -33,4 +35,4 @@ Agoo::Server.handle(:GET, '/', Empty)
 Agoo::Server.handle(:GET, '/user/*', Reflect)
 Agoo::Server.handle(:POST, '/user', Empty)
 
-Agoo::Server.start()
+Agoo::Server.start
