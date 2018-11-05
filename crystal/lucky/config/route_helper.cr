dc3a690ce7d720fc1f421234b3083725ce50621b
Marwan Rabbâa <waghanza@gmail.com>
2018-11-05 22:00:21 +0100

    use latest crystal for lucky

--- crystal/lucky/config/route_helper.cr
+++ crystal/lucky/config/route_helper.cr
@@ -1,5 +1,5 @@
 # This is used when generating URLs for your application
-Lucky::RouteHelper.configure do
+Lucky::RouteHelper.configure do |settings|
   # Set domain to the default host/port in development
   settings.base_uri = "http://localhost:3001"
 end
--- crystal/lucky/config/server.cr
+++ crystal/lucky/config/server.cr
@@ -1,4 +1,4 @@
-Lucky::Server.configure do
+Lucky::Server.configure do |settings|
   settings.secret_key_base = "u4PWnhZfOFXdTOtoiSBF+6jn0zHbYS6/yumo3WXYNSw="
   settings.host = "0.0.0.0"
   settings.port = (ENV["PORT"]? || 3000).to_i
