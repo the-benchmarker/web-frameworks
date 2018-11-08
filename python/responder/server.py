2c0b6dad02e16d62eac04f7954e1331c73aa836a
Marwan Rabbâa <waghanza@gmail.com>
2018-11-08 16:35:42 +0100

    specify header -> we do not want json

--- python/responder/server.py
+++ python/responder/server.py
@@ -5,15 +5,18 @@ app = responder.API()
 
 @app.route("/")
 async def index(req, resp):
+    resp.headers['Content-Type'] = 'text/plain'
     resp.text = ''
 
 
 @app.route('/user/{id}')
 async def user_info(req, resp, *, id):
+    resp.headers['Content-Type'] = 'text/plain'
     resp.text = id
 
 
 @app.route('/user')
 async def user(req, resp):
     if req.method == 'post':
+        resp.headers['Content-Type'] = 'text/plain'
         resp.text = ''
