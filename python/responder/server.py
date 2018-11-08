759f3515a02ed8a093a61f631c5543192ea31ff4
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2018-11-08 17:27:34 +0100

    Fix responder (temporary fix) (#606)
    * use <master> branch for respondfer -> temporary fix
    
    * strict contraint for gunicorn on responder
    
    * run on the latest python version available in docker
    
    * no need of <EmptyString> class -> has been fixed on responder
    
    * specify header -> we do not want json

--- python/responder/server.py
+++ python/responder/server.py
@@ -1,26 +1,22 @@
 import responder
-from marshmallow import fields
-
-
-class EmptyString(fields.Field):
-    def encode(field, options):
-        return ''
-
 
 app = responder.API()
 
 
 @app.route("/")
 async def index(req, resp):
-    resp.text = EmptyString()
+    resp.headers['Content-Type'] = 'text/plain'
+    resp.text = ''
 
 
 @app.route('/user/{id}')
 async def user_info(req, resp, *, id):
+    resp.headers['Content-Type'] = 'text/plain'
     resp.text = id
 
 
 @app.route('/user')
 async def user(req, resp):
     if req.method == 'post':
-        resp.text = EmptyString()
+        resp.headers['Content-Type'] = 'text/plain'
+        resp.text = ''
