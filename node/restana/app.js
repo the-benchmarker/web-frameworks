0712fbf3f32a95d04f5a66a77aeac591848bf4b6
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2018-11-13 20:11:39 +0100

    Introduce restana (#623)
    * adding restana web framework
    
    * adding semicolons
    
    * use X.Y format instead of X.Y.Z
    
    * use github as source link
    
    * use es6 on node frameworks

---
+++ node/restana/app.js
@@ -0,0 +1,16 @@
+const restana = require('restana');
+const service = restana();
+
+service.get('/', function (req, res) {
+  res.send(200);
+});
+
+service.get('/user/:id', function (req, res) {
+  res.send(req.params.id);
+});
+
+service.post('/user', function (req, res) {
+  res.send(200);
+});
+
+service.start(3000);
