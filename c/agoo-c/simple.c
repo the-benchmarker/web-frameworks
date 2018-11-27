129433b4ace78134901ba0b2319d446073b941f5
Peter Ohler <peter@ohler.com>
2018-11-27 03:48:27 -0500

    Latest Agoo-C (#651)
    * Latest Agoo-C
    
    * use clang style formatting

--- c/agoo-c/simple.c
+++ c/agoo-c/simple.c
@@ -6,45 +6,60 @@
 #include <agoo.h>
 #include <agoo/log.h>
 #include <agoo/page.h>
+#include <agoo/res.h>
 #include <agoo/server.h>
 
-static int	user_off = 6;
+static agooText emptyResp = NULL;
 
-static void
-user_handler(Req req) {
-    agoo_respond(req, 200, req->path.start + user_off, req->path.len - user_off, NULL);
+static void empty_handler(agooReq req) {
+  if (NULL == emptyResp) {
+    emptyResp = agoo_respond(200, NULL, 0, NULL);
+    agoo_text_ref(emptyResp);
+  }
+  agoo_res_set_message(req->res, emptyResp);
 }
 
-static void
-empty_handler(Req req) {
-    agoo_respond(req, 200, NULL, 0, NULL);
+static int user_off = 6;
+
+user_handler(agooReq req) {
+  agooText t = agoo_respond(200, req->path.start + user_off,
+                            req->path.len - user_off, NULL);
+
+  agoo_res_set_message(req->res, t);
 }
 
-int
-main(int argc, char **argv) {
-    struct _Err	err = ERR_INIT;
-    
-    agoo_init("simple");
+int main(int argc, char **argv) {
+  struct _agooErr err = AGOO_ERR_INIT;
+  int port = 3000;
 
-    // Set the number of eval threads.
-    the_server.thread_cnt = 1;
+  if (AGOO_ERR_OK != agoo_init(&err, "simple")) {
+    printf("Failed to initialize Agoo. %s\n", err.msg);
+    return err.code;
+  }
+  // Set the number of eval threads. No need for more than one in this
+  // case. The multiple read/write threads do most of the work and are
+  // adjusted automatically.
+  agoo_server.thread_cnt = 1;
 
-    pages_set_root(".");
+  agoo_pages_set_root(".");
 
-    if (ERR_OK != agoo_bind_port(&err, 3000)) {
-	printf("Failed to bind to port. %s\n", err.msg);
-	return err.code;
-    }
-    // set up hooks or routes
-    if (ERR_OK != agoo_add_func_hook(&err, GET, "/", empty_handler, true) ||
-	ERR_OK != agoo_add_func_hook(&err, GET, "/user/*", user_handler, true) ||
-	ERR_OK != agoo_add_func_hook(&err, POST, "/user", empty_handler, true)) {
-	return err.code;
-    }
-    // start the server and wait for it to be shutdown
-    if (ERR_OK != agoo_start(&err, "0.1")) {
-	printf("%s\n", err.msg);
-	return err.code;
-    }
-    return 0;
+  if (AGOO_ERR_OK != agoo_bind_to_port(&err, port)) {
+    printf("Failed to bind to port %d. %s\n", port, err.msg);
+    return err.code;
+  }
+  // set up hooks or routes
+  if (AGOO_ERR_OK !=
+          agoo_add_func_hook(&err, AGOO_GET, "/", empty_handler, true) ||
+      AGOO_ERR_OK !=
+          agoo_add_func_hook(&err, AGOO_GET, "/user/*", user_handler, true) ||
+      AGOO_ERR_OK !=
+          agoo_add_func_hook(&err, AGOO_POST, "/user", empty_handler, true)) {
+    return err.code;
+  }
+  // start the server and wait for it to be shutdown
+  if (AGOO_ERR_OK != agoo_start(&err, AGOO_VERSION)) {
+    printf("%s\n", err.msg);
+    return err.code;
+  }
+  return 0;
 }
