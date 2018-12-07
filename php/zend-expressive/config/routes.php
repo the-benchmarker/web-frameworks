18c26f4225e3a86eb8c30e57a6f5fecc00939b72
Marwan Rabbâa <waghanza@gmail.com>
2018-12-07 20:16:42 +0100

    wrong method for route 3

--- php/zend-expressive/config/routes.php
+++ php/zend-expressive/config/routes.php
@@ -22,5 +22,5 @@ use Zend\Expressive\MiddlewareFactory;
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
     $app->route('/', \App\Handler\Home::class, ['GET']);
     $app->route('/user/{id}', \App\Handler\UserId::class, ['GET']);
-    $app->route('/user', \App\Handler\User::class, ['GET']);
+    $app->route('/user', \App\Handler\User::class, ['POST']);
 };
