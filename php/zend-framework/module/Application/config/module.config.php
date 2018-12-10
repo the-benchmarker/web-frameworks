d7447a340a88f0d6fc7437356c5ff8f4452908d3
Mateusz Łopaciński <lopacimat@gmail.com>
2018-12-10 18:16:41 +0100

    Add Zend Framework to PHP (#706)

---
+++ php/zend-framework/module/Application/config/module.config.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application;
+
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'home' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route'    => '/',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'user_id' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/user[/:id]',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'userId',
+                    ],
+                ],
+            ],
+            'user' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route' => '/user',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action' => 'user'
+                    ]
+                ]
+            ]
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ],
+];
