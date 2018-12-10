7fbcea353c938a858081b92a7c6a3e38b46c69f1
Mateusz Lopacinski <mateusz.lopacinski@xcaliber.com>
2018-12-10 11:06:52 +0100

    Add Zend Framework to PHP

---
+++ php/zend-framework/config/modules.config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Zend\Router',
+    'Zend\Validator',
+    'Application',
+];
