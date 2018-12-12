873bc854d10fbc66b23213bb1a5d81d5b669f74a
Stephen von Takach <steve@acaprojects.com>
2018-12-12 12:28:44 +1100

    update spider-gazelle
    fix spec - welcome controller no longer exists

--- crystal/spider-gazelle/spec/welcome_spec.cr
+++ crystal/spider-gazelle/spec/welcome_spec.cr
@@ -2,11 +2,6 @@ require "./spec_helper"
 
 describe Application do
   with_server do
-    it "should welcome you" do
-      result = curl("GET", "/")
-      result.body.should eq("")
-    end
-
     it "should list users" do
       result = curl("GET", "/user")
       result.body.should eq("")
