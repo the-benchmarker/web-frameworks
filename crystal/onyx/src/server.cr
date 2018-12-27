2dc801850d6cfed91333cf9af87f7b2c363d9e38
Vlad Faust <vladislav.faust@gmail.com>
2018-12-27 15:19:05 +0300

    Introduce Onyx::REST (#737)
    * Introduce Onyx::REST
    
    * Do not print newline into the response
    
    * Rename onyx-rest to onyx
    
    * update results

---
+++ crystal/onyx/src/server.cr
@@ -0,0 +1,23 @@
+require "onyx-rest"
+
+router = Onyx::REST::Router.new do
+  get "/" { }
+
+  get "/user/:id" do |env|
+    env.response.output << env.request.path_params["id"]
+  end
+
+  post "/user" { }
+end
+
+server = Onyx::REST::Server.new(router)
+
+(System.cpu_count - 1).times do |i|
+  Process.fork do
+    server.bind_tcp("0.0.0.0", 3000, reuse_port: true)
+    server.listen
+  end
+end
+
+server.bind_tcp("0.0.0.0", 3000, reuse_port: true)
+server.listen
