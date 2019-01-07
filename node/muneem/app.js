f193418adaff43bb1524482205b926d7eca4cf8d
Amit Gupta <amitguptagwl@users.noreply.github.com>
2019-01-07 18:10:47 +0530

    Add Muneem node framework (#749)
    * #748 Add Muneem nodejs framework
    
    * update muneem framework detail
    
    * update muneem version and neph.yaml entry
    
    * update the app to use post call

---
+++ node/muneem/app.js
@@ -0,0 +1,16 @@
+const app = require('muneem')();
+
+app.get( "/", function(req, res){
+  res.end(200);
+})
+
+app.get("/user/:id", function(req, res){
+  res.write(req.params.id);
+  res.end();
+})
+
+app.post( "/user", function(req, res){
+  res.end(200);
+})
+
+app.start(3000);
