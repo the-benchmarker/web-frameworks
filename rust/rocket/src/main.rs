cd7eb40443cd4bbf9b8c39cb84645ca7c39428f8
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2019-01-15 22:24:06 +0100

    Disable logging on Rocket (#768)
    * disable logging
    
    * use rocket 0.4 new syntax
    
    * import logging module

--- rust/rocket/src/main.rs
+++ rust/rocket/src/main.rs
@@ -2,7 +2,7 @@
 
 #[macro_use] extern crate rocket;
 
-use rocket::config::{Config, Environment};
+use rocket::config::{Config, Environment,LoggingLevel};
 
 #[get("/")]
 fn index_route() {}
@@ -19,8 +19,11 @@ fn main() {
     let config = Config::build(Environment::Production)
         .address("0.0.0.0")
         .port(3000)
+        .log_level(LoggingLevel::Off)
         .finalize()
         .unwrap();
     let app = rocket::custom(config);
-    app.mount("/", routes![index_route, user_route, user_register_route]).launch();
+    app
+        .mount("/", routes![index_route, user_route, user_register_route])
+        .launch();
 }
