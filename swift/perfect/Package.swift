6f62637e02c80dcbd9c19786b043c1a78cafeb4f
Marwan Rabbâa <waghanza@gmail.com>
2019-02-28 00:16:32 +0100

    upgrade perfect

--- swift/perfect/Package.swift
+++ swift/perfect/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "server",
     dependencies: [
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"3.1.0")
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.1.0"..<"3.2.0")
     ],
     targets: [
         .target(name: "server", dependencies: ["PerfectHTTPServer"], path: ".")
