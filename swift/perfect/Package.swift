39e4056433afdfede9f06980f206104c4337ad45
Marwan Rabbâa <waghanza@gmail.com>
2019-02-28 19:10:26 +0100

    use perfect main repo as depenency

--- swift/perfect/Package.swift
+++ swift/perfect/Package.swift
@@ -4,9 +4,9 @@ import PackageDescription
 let package = Package(
     name: "server",
     dependencies: [
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.1.0"..<"3.2.0")
+        .package(url: "https://github.com/PerfectlySoft/Perfect", "3.1.0"..<"3.2.0")
     ],
     targets: [
-        .target(name: "server", dependencies: ["PerfectHTTPServer"], path: ".")
+        .target(name: "server", dependencies: ["Perfect"], path: ".")
     ]
 )
