221fc00909608851b163be3a817aad907eaac04a
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2019-03-04 09:01:50 +0100

    upgrade perfect (#1001)
    * upgrade perfect
    
    * use perfect main repo as depenency
    
    * homogenize swif dependency update
    
    * fix hound warnings

--- swift/perfect/Package.swift
+++ swift/perfect/Package.swift
@@ -1,12 +1,13 @@
 // swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "server",
     dependencies: [
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"3.1.0")
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .upToNextMinor(from: "3.0.0"))
     ],
     targets: [
         .target(name: "server", dependencies: ["PerfectHTTPServer"], path: ".")
     ]
-)
+)   
