d7c3e45e00e98776281e248a31b377bcb719dfdf
Marwan Rabbâa <waghanza@gmail.com>
2019-03-31 15:28:21 +0200

    add violetear

---
+++ go/violetear/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/nbari/violetear"
+)
+
+func showId(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, violetear.GetParam("id", r))
+}
+
+func showEmpty(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "")
+}
+
+func main() {
+	router := violetear.New()
+
+	router.AddRegex(":id", `\d`)
+
+	router.HandleFunc("/", showEmpty, "GET")
+	router.HandleFunc("/user/:id", showId, "GET")
+	router.HandleFunc("/user", showEmpty, "POST")
+
+	srv := &http.Server{
+		Addr:           ":3000",
+		Handler:        router,
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   7 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(srv.ListenAndServe())
+
+}
