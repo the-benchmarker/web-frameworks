13c1d177ac85613bd9fb8c24cf7be43c960886db
Adam Hopkins <admhpkns@gmail.com>
2019-04-29 14:47:37 +0300

    Change sanic test to use built-in server instead of gunicorn

--- python/sanic/server.py
+++ python/sanic/server.py
@@ -1,19 +1,31 @@
 from sanic import Sanic
 from sanic.response import text
+import sys
 
 app = Sanic(log_config=None)
+WORKERS = int(sys.argv[-1])
 
 
 @app.route("/")
 async def index(request):
-    return text('')
+    return text("")
 
 
-@app.route("/user/<id:int>", methods=['GET'])
+@app.route("/user/<id:int>", methods=["GET"])
 async def user_info(request, id):
     return text(str(id))
 
 
-@app.route("/user", methods=['POST'])
+@app.route("/user", methods=["POST"])
 async def user(request):
-    return text('')
+    return text("")
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=False,
+        access_log=False,
+        workers=WORKERS,
+        port=3000,
+        host="0.0.0.0",
+    )
