9a6aec7b3565c4ae6a6862ce5a25fdf8ba69b6c5
Francis Caron <francis.caron@terraspec.ca>
2019-05-04 09:08:54 -0400

    clean up
    clean up

--- kotlin/ktor/src/Application.kt
+++ kotlin/ktor/src/Application.kt
@@ -9,15 +9,14 @@ import io.ktor.http.*
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     routing {
         get("/") {
             call.respondText("", contentType = ContentType.Text.Plain, status = HttpStatusCode.OK)
         }
         get("/user/{id}") {
-            val id = call.parameters["id"]
-            call.respondText(id ?: "", contentType = ContentType.Text.Plain, status = HttpStatusCode.OK)
+            val id = call.parameters["id"]!!
+            call.respondText(id, contentType = ContentType.Text.Plain, status = HttpStatusCode.OK)
         }
         post("/user") {
             call.respondText("", contentType = ContentType.Text.Plain, status = HttpStatusCode.OK)
