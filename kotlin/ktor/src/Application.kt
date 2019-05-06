381705098a88d19bb330638976c4579c0499a682
Francis Caron <francis.caron@terraspec.ca>
2019-05-06 09:56:34 -0400

    Constrain id to integer
    squash! Constrain id to integer

--- kotlin/ktor/src/Application.kt
+++ kotlin/ktor/src/Application.kt
@@ -5,18 +5,23 @@ import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
 import io.ktor.http.*
+import io.ktor.locations.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+@Location("/user/{id}")
+data class UserId(val id: Int)
+
+
 @Suppress("unused") // Referenced in application.conf
 fun Application.module() {
+    install(Locations)
     routing {
         get("/") {
             call.respondText("", contentType = ContentType.Text.Plain, status = HttpStatusCode.OK)
         }
-        get("/user/{id}") {
-            val id = call.parameters["id"]!!
-            call.respondText(id, contentType = ContentType.Text.Plain, status = HttpStatusCode.OK)
+        get<UserId> { userId ->
+            call.respondText("${userId.id}", contentType = ContentType.Text.Plain, status = HttpStatusCode.OK)
         }
         post("/user") {
             call.respondText("", contentType = ContentType.Text.Plain, status = HttpStatusCode.OK)
