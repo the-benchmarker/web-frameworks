a1803e16f6752209986d0afd3eaab187cdc011cf
Vaibhav Mule <vaibhavmule135@gmail.com>
2019-05-15 18:16:21 +0530

    add integer constraits for route

--- python/masonite/routes/web.py
+++ python/masonite/routes/web.py
@@ -2,6 +2,6 @@ from masonite.routes import Get, Post
 
 ROUTES = [
     Get().route('/', 'PageController@index'),
-    Get().route('/user/@id', 'PageController@show_user'),
+    Get().route('/user/@id:int', 'PageController@show_user'),
     Post().route('/user', 'PageController@create_user'),
 ]
