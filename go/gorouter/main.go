7212fd96e0d40260aa32ac5b76f93f485ec3d740
Rafał Lorenz <vardius@gmail.com>
2019-05-17 03:39:17 +1000

    [Go] Add gorouter (#1321)
    * Add gorouter
    
    * Disable cgo
    
    * Fix methods for handlers
    
    * Update FRAMEWORKS.yml
    
    * Update go.mod
    
    * Add constrain to allow digits only
    
    * Remove unnecessary env variable
    
    * Update go.mod
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update go.mod
    
    * Update go.mod
    
    * Add go.sum, update docker file
    
    * Use CMD instead of ENTRYPOINT

---
+++ go/gorouter/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/vardius/gorouter/v4"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(""))
+}
+
+func user(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(""))
+}
+
+func userId(w http.ResponseWriter, r *http.Request) {
+    params, _ := gorouter.FromContext(r.Context())
+    id := params.Value("id")
+    w.Write([]byte(id))
+}
+
+func main() {
+    router := gorouter.New()
+
+    router.GET("/", http.HandlerFunc(index))
+    router.GET("/user/{id:[0-9]+}", http.HandlerFunc(userId))
+    router.POST("/user", http.HandlerFunc(user))
+
+    http.ListenAndServe(":3000", router)
+}
