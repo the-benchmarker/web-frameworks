a1737336c9600d6f9080bf6c56462b31b76b81b7
David Jones <djones6dev@gmail.com>
2019-05-22 15:50:03 +0100

    [Swift] Update Kitura Dockerfiles to Ubuntu 18.04 (#1346)
    * Update Kitura version to 2.7
    
    * Update Kitura Dockerfiles to Ubuntu 18.04
    
    * Update all Swift frameworks to swift:5.0.1
    
    * chore(results): Homogenize swift containers (upgrade to the last ubuntu)

--- swift/kitura/Package.swift
+++ swift/kitura/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "server",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura", .upToNextMinor(from: "2.6.0"))
+        .package(url: "https://github.com/IBM-Swift/Kitura", .upToNextMinor(from: "2.7.0"))
     ],
     targets: [
        .target(name: "server", dependencies: ["Kitura"], path: ".")
