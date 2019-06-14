04fe71870c00f47c5499fa695e15894f2046223d
Marwan Rabbâa <waghanza@gmail.com>
2019-06-14 23:31:00 +0200

    test(ci): Test c lint

--- .ci/lint.sh
+++ .ci/lint.sh
@@ -3,7 +3,7 @@ RETVAL=0
 if [[ ${LANGUAGE} == "csharp" ]] ; then
   sudo apt-get -qy install astyle
   find ${DIRECTORY} -type f -name '*.cs' > /tmp/list.txt
-  while read line ; do
+  while read file ; do
     astyle --mode=cs ${file}
     retval=$?
     if [[ $retval -ne 0 ]]; then
@@ -15,7 +15,7 @@ fi
 
 if [[ ${LANGUAGE} == "elixir" ]] ; then
   find ${DIRECTORY} -type f -name '*.exs' -or -name '*.ex' > /tmp/list.txt
-  while read line ; do
+  while read file ; do
     mix format ${file}
     retval=$?
     if [[ $retval -ne 0 ]]; then
@@ -36,9 +36,9 @@ fi
 
 if [[ ${LANGUAGE} == "cpp" ]] ; then
   find ${DIRECTORY} -type f -name '*.cpp' > /tmp/list.txt
-  while read line ; do
-    clang-format ${file}
-    [[ -z `git ls-files --modified` ]]
+  while read file ; do
+    clang-format -i ${file}
+    [[ ! -n `git ls-files --modified` ]]
     retval=$?
     if [[ $retval -ne 0 ]]; then
       RETVAL=${retval}
@@ -61,8 +61,9 @@ fi
 
 if [[ ${LANGUAGE} == "c" ]] ; then
   find ${DIRECTORY} -type f -name '*.c' > /tmp/list.txt
-  while read line ; do
-    clang-format -style=chromium ${file}
+  while read file ; do
+    echo "Linting ${file}"
+    clang-format -i ${file}
     [[ ! -n `git ls-files --modified` ]]
     retval=$?
     if [[ $retval -ne 0 ]]; then
@@ -80,7 +81,7 @@ if [[ ${LANGUAGE} == "nim" ]] ; then
   cd nimpretty
   nim c nimpretty.nim  
   find ${DIRECTORY} -type f -name '*.nim' -or -name '*.nimble'  > /tmp/list.txt
-  while read line ; do
+  while read file ; do
     nimpretty ${file}
     retval=$?
     if [[ $retval -ne 0 ]]; then
@@ -92,9 +93,9 @@ fi
 
 if [[ ${LANGUAGE} == "java" ]] ; then
   find ${DIRECTORY} -type f -name '*.java' > /tmp/list.txt
-  while read line ; do
+  while read file ; do
     clang-format --style=google ${file}
-    [[ -z `git ls-files --modified` ]]
+    [[ ! -n `git ls-files --modified` ]]
     retval=$?
     if [[ $retval -ne 0 ]]; then
       RETVAL=${retval}
@@ -105,9 +106,9 @@ fi
 
 if [[ ${LANGUAGE} == "objc" ]] ; then
   find ${DIRECTORY} -type f -name '*.m' > /tmp/list.txt
-  while read line ; do
-    clang-format ${file}
-    [[ -z `git ls-files --modified` ]]
+  while read file ; do
+    clang-format -i ${file}
+    [[ ! -n `git ls-files --modified` ]]
     retval=$?
     if [[ $retval -ne 0 ]]; then
       RETVAL=${retval}
@@ -141,7 +142,7 @@ fi
 if [[ ${LANGUAGE} == "rust" ]] ; then
   rustup component add rustfmt --toolchain stable-x86_64-unknown-linux-gnu
   find ${DIRECTORY} -type f -name '*.rs' > /tmp/list.txt
-  while read line ; do
+  while read file ; do
     rustfmt --check ${file}
     retval=$?
     if [[ $retval -ne 0 ]]; then
