cbcce36367cddc6e5c826e38989bd9b58bb68d53
Marwan Rabbâa <waghanza@gmail.com>
2019-06-19 07:21:52 +0200

    feat(lint): Add rubocop configuration file

--- .ci/lint.sh
+++ .ci/lint.sh
@@ -125,6 +125,11 @@ if [[ ${LANGUAGE} == "ruby" ]] ; then
 fi
 
 if [[ ${LANGUAGE} == "crystal" ]] ; then
+  curl -sSL https://dist.crystal-lang.org/apt/setup.sh | sudo bash
+  curl -sL "https://keybase.io/crystal/pgp_keys.asc" | sudo apt-key add -
+  echo "deb https://dist.crystal-lang.org/apt crystal main" | sudo tee /etc/apt/sources.list.d/crystal.list
+  sudo apt-get update
+  sudo apt install crystal
   crystal tool format --check ${DIRECTORY}
   RETVAL=$?
 fi
