219eea80b8b5ae46209d3063b84d79a7c89edee6
Marwan Rabbâa <waghanza@gmail.com>
2019-07-10 12:13:20 +0200

    feat(turbo_polka): Restrict user id to numeric values

--- node/turbo_polka/app.js
+++ node/turbo_polka/app.js
@@ -6,7 +6,7 @@ polka({ server })
   .get('/', (req, res) => {
     res.end('')
   })
-  .get('/user/:id', (req, res) => {
+  .get('/user/:id(\\d+)', (req, res) => {
     res.end(req.params.id)
   })
   .post('/user', (req, res) => {
