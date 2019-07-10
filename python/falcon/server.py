4ca279001767091fdf9744ae09b77902cea464a9
Marwan Rabbâa <waghanza@gmail.com>
2019-07-10 11:00:59 +0200

    feat(falcon): Restrict user id to numeric values

--- python/falcon/server.py
+++ python/falcon/server.py
@@ -6,18 +6,18 @@ patch.patch_all()
 
 class EmptyResponse:
     def on_get(self, req, resp):
-        resp.data = bytes("", "utf8")
+        resp.data = "".encode()
 
     def on_post(self, req, resp):
-        resp.data = bytes("", "utf8")
+        resp.data = "".encode()
 
 
 class StringResponse:
-    def on_get(self, req, resp, id):
-        resp.data = bytes(id, "utf8")
+    def on_get(self, req, resp, id: int):
+        resp.data = f"{id}".encode()
 
 
 app = falcon.API()
 app.add_route("/", EmptyResponse())
 app.add_route("/user", EmptyResponse())
-app.add_route("/user/{id}", StringResponse())
+app.add_route("/user/{id:int}", StringResponse())
