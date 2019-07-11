d69cc2a2ac3be6f5ea3b61a3a78b862e6909b083
Marwan Rabbâa <waghanza@gmail.com>
2019-07-11 17:59:59 +0200

    feat(vapor): Restric user id to numeric values

--- swift/vapor/main.swift
+++ swift/vapor/main.swift
@@ -5,15 +5,21 @@ services.register(MiddlewareConfig())
 
 let router = EngineRouter.default()
 let empty = HTTPResponse()
+
 router.get("/") { _ in
     return empty
 }
-router.get("user", String.parameter) { req in
-    return try req.parameters.next(String.self)
+router.get("user", Int.parameter) { req -> String in
+    let id = try req.parameters.next(Int.self)
+    return "\(id)"
 }
 router.post("user") { _ in
     return empty
 }
+
+var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+services.register(middlewares)
 services.register(router, as: Router.self)
 
 let app = try Application(config: .default(), environment: .detect(), services: services)
