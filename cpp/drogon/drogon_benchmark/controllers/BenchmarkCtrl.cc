5c173775faa60d59db7c45832275e160a25d7815
Marwan Rabbâa <waghanza@gmail.com>
2019-07-16 11:05:34 +0200

    refactor(drogon): Remove unused files

--- cpp/drogon/drogon_benchmark/controllers/BenchmarkCtrl.cc
+++
@@ -1,9 +0,0 @@
-#include "BenchmarkCtrl.h"
-void BenchmarkCtrl::asyncHandleHttpRequest(
-    const HttpRequestPtr &req,
-    std::function<void(const HttpResponsePtr &)> &&callback)
-{
-    auto resp = HttpResponse::newHttpResponse();
-    resp->setExpiredTime(0);
-    callback(resp);
-}
