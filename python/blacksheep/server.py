0b9e11fe9eb8ea666f2e7a2e699e9203511c76a0
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2019-07-18 16:28:57 +0200

    [Python] Introduce blacksheep (#1516)
    * feat(python): Introduce blacksheep
    
    * feat(blacksheep): Enable CI
    
    * feat(blacksheep): Enable dependabot
    
    * refactor(python): Lint source
    
    * chore: Add blacksheep to resoult
    
    * fix(blacksheep): Add requirements.txt

---
+++ python/blacksheep/server.py
@@ -0,0 +1,29 @@
+from blacksheep.server import Application, ServerOptions
+import multiprocessing
+from blacksheep.server.responses import text
+
+
+app = Application(
+    ServerOptions(
+        host="0.0.0.0", port=3000, processes_count=multiprocessing.cpu_count()
+    ),
+    debug=False,
+)
+
+
+@app.router.get("/")
+async def home(_):
+    return text(f"")
+
+
+@app.router.get("/user/:id")
+async def user_info(_, id: int):
+    return text(f"{id}")
+
+
+@app.router.post("/user")
+async def user(_):
+    return text(f"")
+
+
+app.start()
