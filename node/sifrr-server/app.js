9001c85bea9db4cab32b4375f9f581346e837b77
Aaditya Taparia <aadityataparia@gmail.com>
2019-07-27 14:34:04 +0900

    feat: add sifrr-server

---
+++ node/sifrr-server/app.js
@@ -0,0 +1,15 @@
+const { App } = require("@sifrr/server");
+
+const app = new App();
+
+app
+  .get("/", res => {
+    res.end("");
+  })
+  .get("/user/:id", (res, req) => {
+    res.end(req.getParameter(0));
+  })
+  .post("/user", res => {
+    res.end("");
+  })
+  .listen(3000);
