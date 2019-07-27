7123aaf0e536ba2fc696382832d147cb229b18ac
Marwan Rabbâa <waghanza@gmail.com>
2019-07-27 11:42:00 +0200

    refactor(swoft): Remove unused files

--- php/swoft/app/Helper/Functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Custom global functions
- */
-
-function user_func(): string
-{
-    return 'hello';
-}
--- php/swoft/app/Model/Dao/UserDao.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-
-namespace App\Model\Dao;
-
-class UserDao
-{
-}
