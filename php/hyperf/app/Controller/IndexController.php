d4c91e1ef793e3d37ce3f9b0b5fbe8c17fdd65b9
李铭昕 <715557344@qq.com>
2019-07-27 20:32:09 +0800

    [PHP] Added hyperf framework. (#1573)
    * Added hyperf framwork for php.
    
    * Update
    
    * FIxed.
    
    * Fixed .
    
    * Update
    
    * fix(hyperf): Remove extra data that fails composer
    
    * refactor(hyperf): Remove unsed files
    
    * feat(hyperf): Complete test suite
    
    * refactor(hyperf): Clean dockerfile
    
    * Fixed Dockerfile.
    
    * Fixed .
    
    * Fixed api.
    
    * Deleted useless code.
    
    * Delete useless code.
    
    * Optimized code.
    
    * Update server.php
    
    * php(hyperf): Simplify composer manifest
    
    * feat(hyperf): Add under dependabot eyes
    
    * feat(hyperf): Add on CI

---
+++ php/hyperf/app/Controller/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Controller;
+
+class IndexController
+{
+    public function index()
+    {
+        return "";
+    }
+    public function get(int $id)
+    {
+        return (string) $id;
+    }
+    public function create()
+    {
+        return "";
+    }
+}
