96db31217af6af048bcebb31a788f1c4d56f30b9
Marwan Rabbâa <waghanza@gmail.com>
2019-07-30 09:57:11 +0200

    refactor(nanoexpress): Add space before curl brackets

--- node/nanoexpress/app.js
+++ node/nanoexpress/app.js
@@ -1,13 +1,13 @@
 const nanoexpress = require('nanoexpress')
 
 nanoexpress()
-  .get('/', {isRaw: true}, (req, res) => {
+  .get('/', { isRaw: true }, (req, res) => {
     res.end('')
   })
-  .get('/user/:id', {isRaw: true}, (req, res) => {
+  .get('/user/:id', { isRaw: true }, (req, res) => {
     res.end(req.getParameter(0))
   })
-  .post('/user', {isRaw: true}, (req, res) => {
+  .post('/user', { isRaw: true }, (req, res) => {
     res.end('')
   })
   .listen(3000)
