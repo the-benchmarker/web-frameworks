d2328abf518974885842072e9177d07ac01d5f38
Taoufik <abacidtaoufik@gmail.com>
2019-08-16 01:50:08 +0200

    Headers are set automatically

--- python/responder/server.py
+++ python/responder/server.py
@@ -5,18 +5,15 @@ app = responder.API()
 
 @app.route("/")
 async def index(req, resp):
-    resp.headers["Content-Type"] = "text/plain"
     resp.text = ""
 
 
-@app.route("/user/{id}")
+@app.route("/user/{id:int}")
 async def user_info(req, resp, *, id):
-    resp.headers["Content-Type"] = "text/plain"
     resp.text = id
 
 
 @app.route("/user")
 async def user(req, resp):
     if req.method == "post":
-        resp.headers["Content-Type"] = "text/plain"
         resp.text = ""
