de5d75b21a534f0b9044dcfadf5b159d8e9c3a85
antao <antao2002@gmail.com>
2019-08-31 20:03:57 +0800

    Update drogon to 1.0.0-beta7

--- cpp/drogon/main.cc
+++ cpp/drogon/main.cc
@@ -8,7 +8,6 @@ int main(int argc, char const *argv[])
         std::cout << "please input the config file name" << std::endl;
         return -1;
     }
-    drogon::app().loadConfigFile(argv[1]);
-    drogon::app().run();
+    drogon::app().loadConfigFile(argv[1]).run();
     return 0;
 }
