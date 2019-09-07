8e5c60e37214f110368d79644040e199a1fd275a
Yurun <admin@yurunsoft.com>
2019-09-07 18:52:07 +0800

    Add imi framework for php

---
+++ php/imi/ApiServer/Controller/IndexController.php
@@ -0,0 +1,49 @@
+<?php
+namespace ImiApp\ApiServer\Controller;
+
+use Imi\Controller\HttpController;
+use Imi\Server\View\Annotation\View;
+use Imi\Server\Route\Annotation\Route;
+use Imi\Server\Route\Annotation\Action;
+use Imi\Server\Route\Annotation\Controller;
+
+/**
+ * @Controller
+ * @View(renderType="html")
+ */
+class IndexController extends HttpController
+{
+    /**
+     * @Action
+     * @Route(url="/", method="GET")
+     *
+     * @return void
+     */
+    public function index()
+    {
+        return $this->response->write('');
+    }
+
+    /**
+     * @Action
+     * @Route(url="/user/{id}", method="GET")
+     *
+     * @return void
+     */
+    public function get($id)
+    {
+        return $this->response->write($id);
+    }
+
+    /**
+     * @Action
+     * @Route(url="/user", method="POST")
+     *
+     * @return void
+     */
+    public function create()
+    {
+        return $this->response->write('');
+    }
+
+}
---
+++ php/imi/Main.php
@@ -0,0 +1,14 @@
+<?php
+namespace ImiApp;
+
+use Imi\Main\AppBaseMain;
+
+class Main extends AppBaseMain
+{
+    public function __init()
+    {
+        // 这里可以做一些初始化操作，如果需要的话
+        
+    }
+
+}
