0772e4a749d683b794726e0a9acd1bd60a60c8d3
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-09-13 04:45:49 -0400

    Adds BasicPHP nano-framework

---
+++ php/basicphp/basicphp/config.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Start Session
+|--------------------------------------------------------------------------
+*/
+
+// session_start();
+
+/*
+|--------------------------------------------------------------------------
+| Register The Class Autoloader
+|--------------------------------------------------------------------------
+|
+| Folders containing classes that need to be autoloaded should be added to
+| the array variable $class_folders.
+|
+*/
+
+// Add class folders to autoload
+// $class_folders[] = 'classes';
+// $class_folders[] = 'models';
+$class_folders[] = 'controllers';
+
+define('AUTOLOAD_CLASSES', $class_folders);
+
+spl_autoload_register(function ($class_name) {
+
+	foreach (AUTOLOAD_CLASSES as $folder) {
+
+		if (file_exists('../' . $folder . '/' . $class_name . '.php') && is_readable('../' . $folder . '/' . $class_name . '.php')) {
+
+			require_once '../' . $folder . '/' . $class_name . '.php';
+
+		}
+
+	}
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| Set The Environment
+|--------------------------------------------------------------------------
+|
+| When working in a development environment, define 'ENVIRONMENT' as
+| 'development'. When working in a production environment, define
+| 'ENVIRONMENT' as 'production'. Error reporting is turned ON in development,
+| and OFF in production environment.
+|
+*/
+
+define('ENVIRONMENT', 'development');
+
+switch (ENVIRONMENT) {
+    case 'development':
+        error_reporting(E_ALL);
+        break;
+    case 'production':
+        error_reporting(0);
+        break;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Set BASE_URL
+|--------------------------------------------------------------------------
+|
+| Define 'BASE_URL' as the domain with '/' at the end, such as
+| 'http://example.com/' or 'https://example.com/'.
+|
+*/
+
+define('BASE_URL', 'http://localhost/');
+
+/*
+|--------------------------------------------------------------------------
+| Set URL Parse Method
+|--------------------------------------------------------------------------
+|
+| Sets the $_SERVER[''] global variable to parse the URL
+|
+*/
+
+define('URL_PARSE', 'PATH_INFO');
+
+/*
+|--------------------------------------------------------------------------
+| Set Homepage
+|--------------------------------------------------------------------------
+*/
+
+define('HOME_PAGE', 'UserController@index');
+
+/*
+|--------------------------------------------------------------------------
+| Set Controller Suffix
+|--------------------------------------------------------------------------
+|
+| If you are using 'ClassController' convention, set to 'Controller'.
+|
+*/
+
+define('CONTROLLER_SUFFIX', 'Controller');
+
+/*
+|--------------------------------------------------------------------------
+| Set Default Method
+|--------------------------------------------------------------------------
+|
+| If the second URL string is empty, set this method as the default method.
+*/
+
+define('METHOD_DEFAULT', 'index');
---
+++ php/basicphp/basicphp/controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+class UserController
+{
+
+	public function index()
+	{
+		$this->list();
+	}
+	
+	public function list()
+	{
+
+		return '';
+
+	}
+
+	public function view()
+	{
+
+		return url_value(2);
+
+	}
+
+	public function add()
+	{
+
+		return '';
+
+	}
+
+}
