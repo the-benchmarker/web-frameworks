73689a255aa3c038b46cc0bde92685462b473b47
Marwan <waghanza@gmail.com>
2019-09-13 14:27:55 +0200

    refactor(basicphp): Re-organize controllers

--- php/basicphp/controllers/UserController.php
+++ php/basicphp/controllers/UserController.php
@@ -5,28 +5,8 @@ class UserController
 
 	public function index()
 	{
-		$this->list();
-	}
-	
-	public function list()
-	{
 
 		return '';
 
 	}
-
-	public function view()
-	{
-
-		return url_value(2);
-
-	}
-
-	public function add()
-	{
-
-		return '';
-
-	}
-
 }
