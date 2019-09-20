609461373cd98002403ce93d908f9e349406812d
tanszhe <1018595261@qq.com>
2019-09-21 00:02:25 +0800

    del mysql config

--- php/one/App/config.php
+++ php/one/App/config.php
@@ -1,5 +1,5 @@
 <?php
-\One\Database\Mysql\Connect::setConfig(config('mysql', true));
+//\One\Database\Mysql\Connect::setConfig(config('mysql', true));
 \One\Log::setConfig(config('log', true));
 \One\Http\Router::setConfig(['path' => _APP_PATH_ . '/Config/router.php']);
 \One\Cache\File::setConfig(config('cache.file', true));
