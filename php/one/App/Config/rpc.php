8f9734cdc9a237908e16bf7db4d3bc000195976f
tanszhe <1018595261@qq.com>
2019-09-20 17:03:36 +0800

    add one framework

---
+++ php/one/App/Config/rpc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/12/6
+ * Time: 15:50
+ */
+
+use \One\Swoole\RpcServer;
+
---
+++ php/one/ide_helper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @param string $str
+ * @return mixed
+ */
+function msgpack_unpack($str)
+{
+
+}
+
+/**
+ * @param mixed $str
+ * @return string
+ */
+
+function msgpack_pack($str)
+{
+
+}
