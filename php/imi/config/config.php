8af35a7c562ae638eb827673966ad5efcc92d453
Yurun <admin@yurunsoft.com>
2019-09-28 16:38:11 +0800

    [PHP] Update imi (#1816)

--- php/imi/config/config.php
+++ php/imi/config/config.php
@@ -21,9 +21,23 @@ return [
         'host'      =>  '0.0.0.0',
         'port'      =>  3000,
         'configs'   =>  [
-            'worker_num'        => swoole_cpu_num() * 2,
+            'worker_num'        => swoole_cpu_num(),
             'open_tcp_nodelay'  => true,
             'tcp_fastopen'      => true,
+        ],
+    ],
+
+    'imi'   =>  [
+        'beanScan'  =>  [
+            'Imi\Config',
+            'Imi\Bean',
+            'Imi\Aop',
+            'Imi\Annotation',
+            'Imi\Server',
+            'Imi\Log',
+            'Imi\Listener',
+            'Imi\Tool',
+            'Imi\Process',
         ],
     ],
 ];
