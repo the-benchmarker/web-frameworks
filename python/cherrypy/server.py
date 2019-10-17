a615d4a357774bcbd8ad70fdcbb1d8b05a59860d
Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
2019-10-17 22:31:46 +0200

    Make a method dispatcher cherrypy implementation

--- python/cherrypy/server.py
+++ python/cherrypy/server.py
@@ -1,20 +1,49 @@
+#! /usr/bin/env python3
 import cherrypy
 
 
-class Root:
+IS_STANDALONE = __name__ == '__main__'
+
+
+class WebRoot:
     @cherrypy.expose
     def index(self):
-        return ""
+        return ''
 
-    @cherrypy.expose
-    def user(self, *args):
-        if cherrypy.request.method == "POST" and len(args) == 0:
-            return ""
-        elif cherrypy.request.method == "GET" and len(args) == 1:
-            return str(args[0])
-        
-        return cherrypy.HTTPError(404)
 
-cherrypy.config.update({'engine.autoreload.on': False})
+@cherrypy.expose
+class UserAPI:
+    def GET(self, user_id):
+        return user_id
 
-app = cherrypy.tree.mount(Root())
+    def POST(self):
+        return ''
+
+
+WebRoot.user = UserAPI()
+
+
+global_config = {
+    'environment': 'production' if IS_STANDALONE else 'embedded',
+}
+config = {
+    '/user': {
+        'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+    },
+}
+
+
+cherrypy.config.update(global_config)
+
+if not IS_STANDALONE:
+    # on top of another WSGI server
+    # https://docs.cherrypy.org/en/latest/deploy.html#uwsgi
+    cherrypy.server.unsubscribe()
+    cherrypy.engine.start()
+
+app = cherrypy.tree.mount(WebRoot(), '', config)
+
+
+# standalone self-test
+# python3 -m this_module_name
+IS_STANDALONE and cherrypy.quickstart(app)
