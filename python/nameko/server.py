804004e162a8643e6150a633f7e651f2a561726a
Marwan <waghanza@gmail.com>
2019-10-18 11:57:20 +0200

    feat(python): Add readme

---
+++ python/nameko/server.py
@@ -0,0 +1,16 @@
+from nameko.web.handlers import http
+
+class HttpService:
+    name = "http_service"
+
+    @http('GET', '/')
+    def index(self, request):
+        return ""
+
+    @http('GET', '/user/<int:id>')
+    def get_user(self, request, id):
+        return str(id)
+
+    @http('POST', '/user')
+    def do_post(self, request):
+        return ""
