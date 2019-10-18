116d09e939490e5bb891afbed6daa9d34c3946a3
Marwan <waghanza@gmail.com>
2019-10-18 12:01:28 +0200

    refactor(nameko): Lint

--- python/nameko/server.py
+++ python/nameko/server.py
@@ -1,16 +1,17 @@
 from nameko.web.handlers import http
 
+
 class HttpService:
     name = "http_service"
 
-    @http('GET', '/')
+    @http("GET", "/")
     def index(self, request):
         return ""
 
-    @http('GET', '/user/<int:id>')
+    @http("GET", "/user/<int:id>")
     def get_user(self, request, id):
         return str(id)
 
-    @http('POST', '/user')
+    @http("POST", "/user")
     def do_post(self, request):
         return ""
