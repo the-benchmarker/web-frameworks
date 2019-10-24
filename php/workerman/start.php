88d7b7546ca0a509f780fad4d598f6ff8271c8a1
walkor <walkor@workerman.net>
2019-10-24 13:14:04 +0800

    add workerman

---
+++ php/workerman/start.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Workerman\Worker;
+$worker = new Worker('http://0.0.0.0:8080');
+$worker->count = shell_exec('nproc') ? shell_exec('nproc') : 32;
+$worker->onMessage = function($connection, $data)
+{
+    $request_uri = $data['server']['REQUEST_URI'];
+    switch($request_uri) {
+        case '/':
+        case '/user':
+            $connection->send('');
+            return;
+        default:
+            if (preg_match('/^\/user\/(.+)$/', $request_uri, $match)) {
+                $connection->send($match[1]);
+                return;
+            }
+            $connection->send('');
+    }
+};
+Worker::runAll();
