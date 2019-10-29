3de139e3f44a6b40b837c31428b9192430c88a11
jcheron <myaddressmail@gmail.com>
2019-10-30 00:55:51 +0100

    Fix Dockerfile merge pb

--- php/ubiquity/index.php
+++ php/ubiquity/index.php
@@ -1,8 +1,8 @@
 <?php
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', __DIR__.DS.'app'.DS);
-$config=include_once ROOT.'config/config.php';
-require_once ROOT.'./../vendor/autoload.php';
+\define('DS', \DIRECTORY_SEPARATOR);
+\define('ROOT', __DIR__ . \DS . 'app' . \DS);
+$config = include_once \ROOT . 'config/config.php';
+require_once ROOT . './../vendor/autoload.php';
 \Ubiquity\cache\CacheManager::startProd($config);
 \Ubiquity\controllers\Router::start();
 \Ubiquity\controllers\Startup::run($config);
