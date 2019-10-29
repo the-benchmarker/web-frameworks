f4c537f2f6927f6276ce93e076af1eec48880e30
jcheron <myaddressmail@gmail.com>
2019-10-29 18:15:11 +0100

    PSR-2 Fix

--- php/ubiquity/app/config/routesApp.php
+++ php/ubiquity/app/config/routesApp.php
@@ -1,22 +1,24 @@
 <?php
 use Ubiquity\controllers\Router;
 
-/*******************************************
+/**
+ * **************************
  * Ubiquity micro application
-********************************************/
+ * **************************
+ */
 
-Router::get('_default',function(){ 
-	echo '';
+Router::get('_default', function () {
+    echo '';
 });
 
-Router::get('/user/{id}', function($id){
-	echo $id;
+Router::get('/user/{id}', function ($id) {
+    echo $id;
 });
 
-Router::post('/user',function(){
-	echo '';
+Router::post('/user', function () {
+    echo '';
 });
 
-Router::addCallableRoute('{page}',function($page){
-	\http_response_code(404);
+Router::addCallableRoute('{page}', function ($page) {
+    \http_response_code(404);
 });
