a00746e7069c31d219a531484d9defd413046621
neomatrix <josuecevedo@gmail.com>
2019-10-31 11:58:09 -0600

    specify version

--- julia/merly/deps.jl
+++ julia/merly/deps.jl
@@ -1,3 +1,6 @@
 using Pkg
 
-Pkg.add("Merly")
+ Pkg.add(PackageSpec(name="Merly", version="0.2.1"))
+
+#Pkg.add(PackageSpec(url="https://github.com/JuliaLang/Example.jl", rev="master")) # From url to remote gitrepo
+#Pkg.add(PackageSpec(url="/remote/mycompany/juliapackages/OurPackage"))` # From path to local gitrepo
