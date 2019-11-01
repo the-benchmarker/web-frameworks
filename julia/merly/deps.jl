1117082089d18c7172ea5f28b6692972b7f41f7d
codeneomatrix <josuecevedo@gmail.com>
2019-11-01 12:47:45 -0600

    indicating a specific range of versions

--- julia/merly/deps.jl
+++ julia/merly/deps.jl
@@ -1,6 +1,4 @@
 using Pkg
 
- Pkg.add(PackageSpec(name="Merly", version="0.2.1"))
-
-#Pkg.add(PackageSpec(url="https://github.com/JuliaLang/Example.jl", rev="master")) # From url to remote gitrepo
-#Pkg.add(PackageSpec(url="/remote/mycompany/juliapackages/OurPackage"))` # From path to local gitrepo
+Pkg.activate(pwd())
+Pkg.resolve()
