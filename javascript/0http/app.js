ac1f2ff55f82016e5a998a4f9e108c8f5855794b
Marwan <waghanza@gmail.com>
2019-12-24 10:48:13 +0100

    chore: Lint json configuration files

--- javascript/0http/app.js
+++ javascript/0http/app.js
@@ -2,15 +2,15 @@ const cero = require("0http");
 const { router, server } = cero();
 
 router.on("GET", "/", (req, res) => {
-	res.end();
+  res.end();
 });
 
 router.on("GET", "/user/:id", (req, res, params) => {
-	res.end(params.id);
+  res.end(params.id);
 });
 
 router.on("POST", "/user", (req, res) => {
-	res.end();
+  res.end();
 });
 
 server.listen(3000);
--- javascript/nanoexpress-pro/app.js
+++ javascript/nanoexpress-pro/app.js
@@ -1,13 +1,13 @@
 const nanoexpressPro = require("nanoexpress-pro");
 
 nanoexpressPro()
-	.get("/", (req, res) => {
-		res.end("");
-	})
-	.get("/user/:id", (req, res) => {
-		res.end(req.params.id);
-	})
-	.post("/user", (req, res) => {
-		res.end("");
-	})
-	.listen(3000);
+  .get("/", (req, res) => {
+    res.end("");
+  })
+  .get("/user/:id", (req, res) => {
+    res.end(req.params.id);
+  })
+  .post("/user", (req, res) => {
+    res.end("");
+  })
+  .listen(3000);
