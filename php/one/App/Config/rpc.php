5618a8102c305ecf200e4fc428735514e92a35a5
tanszhe <1018595261@qq.com>
2019-12-24 11:19:29 +0800

    install

--- php/one/App/Config/rpc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2018/12/6
- * Time: 15:50
- */
-
-use \One\Swoole\RpcServer;
--- php/one/App/shell.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2018/11/7
- * Time: 14:32
- */
-define('_SHELL_', true);
-require __DIR__ . '/index.php';
