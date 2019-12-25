06406199bd292cb31a0bd66cb730be8b94b036b9
Marwan <waghanza@gmail.com>
2019-12-25 10:40:30 +0100

    feat(python): Introduce pyramid

---
+++ python/pyramid/server.py
@@ -0,0 +1,22 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+from pyramid.response import Response
+
+
+def empty(request):
+    return Response()
+
+
+def show(request):
+    return Response(request.matchdict["id"])
+
+
+with Configurator() as config:
+    config.add_route("index", "/")
+    config.add_route("create_user", "/user", request_method="POST")
+    config.add_route("get_user", "/user/{id}")
+    config.add_view(empty, route_name="index")
+    config.add_view(empty, route_name="create_user")
+    config.add_view(show, route_name="get_user")
+    app = config.make_wsgi_app()
