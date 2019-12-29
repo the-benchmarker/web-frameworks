2644ba1fc74537729464deef1132c0dc629fd5c2
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2019-12-29 08:19:14 +0100

    [PHP] Introduce hamlet (#2071)

--- javascript/0http/app.js
+++ javascript/0http/app.js
@@ -2,15 +2,15 @@ const cero = require("0http");
 const { router, server } = cero();
 
 router.on("GET", "/", (req, res) => {
-  res.end();
+	res.end();
 });
 
 router.on("GET", "/user/:id", (req, res) => {
-  res.end(req.params.id);
+	res.end(req.params.id);
 });
 
 router.on("POST", "/user", (req, res) => {
-  res.end();
+	res.end();
 });
 
 server.listen(3000);
