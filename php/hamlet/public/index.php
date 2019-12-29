9242e8c46036136746225d22d4b900cf0a5bfe92
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2019-12-29 08:19:14 +0100

    [PHP] Introduce hamlet (#2071)

---
+++ php/hamlet/public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Application\Application;
+use Hamlet\Http\Bootstraps\ServerBootstrap;
+
+$application = new Application();
+ServerBootstrap::run($application);
---
+++ php/hamlet/src/Application.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Application;
+
+use Application\Resources\ApplicationResource;
+use Application\Resources\UserResource;
+use Application\Resources\UserIDResource;
+use Cache\Adapter\PHPArray\ArrayCachePool;
+use Hamlet\Http\Applications\AbstractApplication;
+use Hamlet\Http\Requests\Request;
+use Hamlet\Http\Resources\HttpResource;
+use Hamlet\Http\Resources\NotFoundResource;
+use Hamlet\Http\Responses\Response;
+use Hamlet\Http\Responses\ServerErrorResponse;
+use Psr\Cache\CacheItemPoolInterface;
+
+class Application extends AbstractApplication
+{
+    /** @var CacheItemPoolInterface */
+    private $cache;
+
+    public function __construct()
+    {
+        $this->cache = new ArrayCachePool;
+    }
+
+    public function findResource(Request $request): HttpResource
+    {
+        $path = $request->getPath();
+        $parts = \explode(DIRECTORY_SEPARATOR, $path);
+
+        if ($path === '/user' && $request->getMethod() === 'POST') {
+            return new UserResource;
+        }
+        if ($request->getMethod() === 'GET') {
+            if ($path === '/') {
+                return new ApplicationResource;
+            } elseif (count($parts) === 3 && $parts[1] === 'user') {
+                return new UserIDResource($parts[2]);
+            }
+        }
+        return new NotFoundResource;
+    }
+
+    protected function getCache(Request $request): CacheItemPoolInterface
+    {
+        return $this->cache;
+    }
+}
