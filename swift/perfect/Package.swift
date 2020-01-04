6fd7b10ee4def14db4c1d618fc360ebc8ac21390
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2020-01-04 17:17:41 +0100

    [Swift] Wrong version for perfect (#2115)

--- swift/perfect/Package.swift
+++ swift/perfect/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "server",
     dependencies: [
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .upToNextMinor(from: "3.0.0"))
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .upToNextMinor(from: "3.1.0"))
     ],
     targets: [
         .target(name: "server", dependencies: ["PerfectHTTPServer"], path: ".")
