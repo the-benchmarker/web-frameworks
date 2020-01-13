bfa5f88bf4355207f49186011dd0ef491550d119
Joseph Mancuso <idmann509@gmail.com>
2020-01-13 12:30:48 -0500

    Update wsgi.py

--- python/masonite/wsgi.py
+++ python/masonite/wsgi.py
@@ -13,10 +13,8 @@ and the application configuration file before they boot.
 container = App()
 
 container.bind("WSGI", app)
-container.bind("Application", application)
 container.bind("Container", container)
 
-container.bind("ProvidersConfig", providers)
 container.bind("Providers", [])
 container.bind("WSGIProviders", [])
 
@@ -28,7 +26,7 @@ only run once when the server is started. Providers will be ran
 once if the wsgi attribute on a provider is False.
 """
 
-for provider in container.make("ProvidersConfig").PROVIDERS:
+for provider in providers.PROVIDERS:
     located_provider = provider()
     located_provider.load_app(container).register()
     if located_provider.wsgi:
