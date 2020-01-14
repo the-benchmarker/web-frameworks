2c93f873c6314d64b8f854c76ad92b41c4115b5d
Giovanni Barillari <gi0baro@d4net.org>
2020-01-14 12:02:39 +0100

    [Python] Add Emmett 2.0.0a1 (#2149)

---
+++ python/emmett/server.py
@@ -0,0 +1,19 @@
+from emmett import App
+
+app = App(__name__)
+app.config.handle_static = False
+
+
+@app.route("/", output='bytes')
+async def index():
+    return b""
+
+
+@app.route("/user/<any:id>", output='str')
+async def user_info(id):
+    return id
+
+
+@app.route("/user", methods="post", output='bytes')
+async def user():
+    return b""
