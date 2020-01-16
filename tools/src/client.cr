f1077cf86b06f7a63f83b7d481a91e36d9281aca
Marwan Rabbâa <waghanza@gmail.com>
2020-01-16 10:47:45 +0100

    chore: Parse sieger output from cli

--- tools/src/client.cr
+++ tools/src/client.cr
@@ -2,8 +2,7 @@ require "admiral"
 require "pg"
 
 PIPELINES = {
-  "GET":  File.expand_path("../../" + "pipeline.lua", __FILE__),
-  "POST": File.expand_path("../../" + "pipeline_post.lua", __FILE__),
+  "GET": File.expand_path("../../" + "pipeline.lua", __FILE__),
 }
 
 def insert(framework_id, metric, value)
@@ -12,7 +11,7 @@ def insert(framework_id, metric, value)
     row.move_next
     metric_id = row.read(Int)
 
-    row = db.query("INSERT INTO values (key_id, value) VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING id", metric_id, value)
+    row = db.query("INSERT INTO values (key_id, value) VALUES ($1, $2) RETURNING id", metric_id, value)
     row.move_next
     value_id = row.read(Int)
 
@@ -21,9 +20,9 @@ def insert(framework_id, metric, value)
 end
 
 class Client < Admiral::Command
-  define_flag threads : Int32, description: "# of threads", default: 16, long: "threads", short: "t"
-  define_flag connections : Int32, description: "# of opened connections", default: 1000, long: "connections", short: "c"
-  define_flag duration : Int32, description: "Time to test, in seconds", default: 15, long: "duration", short: "d"
+  define_flag threads : Int32, description: "# of threads", default: 8, long: "threads", short: "t"
+  define_flag connections : Int32, description: "# of opened connections", default: 500, long: "connections", short: "c"
+  define_flag duration : Int32, description: "Time to test, in seconds", default: 5, long: "duration", short: "d"
   define_flag language : String, description: "Language used", required: true, long: "language", short: "l"
   define_flag framework : String, description: "Framework used", required: true, long: "framework", short: "f"
   define_flag routes : Array(String), long: "routes", short: "r", default: ["GET:/"]
@@ -50,35 +49,49 @@ class Client < Admiral::Command
       method, uri = route.split(":")
       url = "http://#{address}:3000#{uri}"
 
-      pipeline = PIPELINES[method]
+      options = {} of String => String | Int32
+      options["duration"] = "#{flags.duration}s"
+      options["connections"] = flags.connections
+      options["timeout"] = 8
+      options["threads"] = flags.threads
+      if method == "POST"
+        options["script"] = File.expand_path("../../" + "pipeline_post.lua", __FILE__)
+      end
+      params = [] of String
+      params << "--latency"
+      options.each do |key, value|
+        params << "--#{key}"
+        params << value.to_s
+      end
+      params << url
 
-      command = "wrk -H 'Connection: keep-alive' --latency -d #{flags.duration}s -s #{pipeline} -c #{flags.connections} --timeout 8 -t #{flags.threads} #{url}"
+      stdout = IO::Memory.new
+      process = Process.new("wrk", params, output: stdout)
+      status = process.wait
+      output = stdout.to_s
+      lines = output.split("\n")
+      requests_per_seconds = lines.grep(/Requests/).first.split(":").pop.strip.to_f
 
-      io = IO::Memory.new
-      Process.run(command, shell: true, error: io)
+      # insert(framework_id, "request_duration", result[0])
+      # insert(framework_id, "request_total", result[1])
+      insert(framework_id, "request_per_second", requests_per_seconds)
+      # insert(framework_id, "request_bytes", result[3])
 
-      result = io.to_s.split(",")
+      # insert(framework_id, "error_socket", result[4])
+      # insert(framework_id, "error_read", result[5])
+      # insert(framework_id, "error_write", result[6])
+      # insert(framework_id, "error_http", result[7])
+      # insert(framework_id, "error_timeout", result[8])
 
-      insert(framework_id, "request_duration", result[0])
-      insert(framework_id, "request_total", result[1])
-      insert(framework_id, "request_per_second", result[2])
-      insert(framework_id, "request_bytes", result[3])
+      # insert(framework_id, "latency_minimum", result[9])
+      # insert(framework_id, "latency_maximum", result[10])
+      # insert(framework_id, "latency_average", result[11])
+      # insert(framework_id, "latency_deviation", result[12])
 
-      insert(framework_id, "error_socket", result[4])
-      insert(framework_id, "error_read", result[5])
-      insert(framework_id, "error_write", result[6])
-      insert(framework_id, "error_http", result[7])
-      insert(framework_id, "error_timeout", result[8])
-
-      insert(framework_id, "latency_minimum", result[9])
-      insert(framework_id, "latency_maximum", result[10])
-      insert(framework_id, "latency_average", result[11])
-      insert(framework_id, "latency_deviation", result[12])
-
-      insert(framework_id, "percentile_fifty", result[13])
-      insert(framework_id, "percentile_ninety", result[14])
-      insert(framework_id, "percentile_ninety_nine", result[15])
-      insert(framework_id, "percentile_ninety_nine_ninety", result[16])
+      # insert(framework_id, "percentile_fifty", result[13])
+      # insert(framework_id, "percentile_ninety", result[14])
+      # insert(framework_id, "percentile_ninety_nine", result[15])
+      # insert(framework_id, "percentile_ninety_nine_ninety", result[16])
     end
 
     db.close
