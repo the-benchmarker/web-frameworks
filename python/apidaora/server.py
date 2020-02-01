2b8217c38e50460946b4b9e0285eb09fe9390faf
Marwan Rabbâa <waghanza@gmail.com>
2020-02-01 10:19:07 +0100

    fix(apidaora): Use plaintext responses

--- python/apidaora/server.py
+++ python/apidaora/server.py
@@ -1,19 +1,26 @@
 from apidaora import appdaora, route
+from apidaora.asgi.router import Route, make_router
+from apidaora.asgi.app import asgi_app
+from apidaora.method import MethodType
+from apidaora.asgi.responses import PLAINTEXT_RESPONSE
 
 
-@route.get('/')
-async def index() -> str:
-    return ''
+def index(path, query, headers, body) -> str:
+    return PLAINTEXT_RESPONSE, b""
 
 
-@route.post('/user')
-async def user_info() -> str:
-    return ''
+def create(path, query, headers, body) -> str:
+    return PLAINTEXT_RESPONSE, b""
 
 
-@route.get('/user/{id}')
-async def user_id(id: str) -> str:
-    return str(id)
+def show(path, query, headers, body) -> str:
+    return PLAINTEXT_RESPONSE, bytes(path.get('id'), encoding='utf-8')
 
 
-app = appdaora((index, user_info, user_id))
+routes = (
+    Route("/", MethodType.GET, index),
+    Route("/user", MethodType.POST, create),
+    Route("/user/{id}", MethodType.GET, show),
+)
+router = make_router(routes)
+app = asgi_app(router)
