5684dfaa93b84752e17dd188f0e3c7d4335a2c9b
German Khokhlov <lkg0dzre@gmail.com>
2020-02-05 21:22:15 +0300

    Removed excess code

--- php/yii/web/index.php
+++ php/yii/web/index.php
@@ -3,7 +3,6 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'prod');
-defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', false);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
