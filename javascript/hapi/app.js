aef78efe852a710255b76bcef4bce50edcf832e7
Marwan Rabbâa <1440729+waghanza@users.noreply.github.com>
2020-02-06 15:45:46 +0100

    [Java / Kotlin] Update to gradle 6 (#2237)

--- javascript/hapi/app.js
+++ javascript/hapi/app.js
@@ -4,45 +4,45 @@ const Hapi = require("@hapi/hapi");
 
 // Create a server with a host and port
 const server = Hapi.server({
-	host: "0.0.0.0",
-	port: 3000
+  host: "0.0.0.0",
+  port: 3000
 });
 
 // Add the route
 server.route({
-	method: "GET",
-	path: "/",
-	handler: function(req, handler) {
-		return "";
-	}
+  method: "GET",
+  path: "/",
+  handler: function(req, handler) {
+    return "";
+  }
 });
 
 server.route({
-	method: "GET",
-	path: "/user/{id}",
-	handler: function(req, handler) {
-		return req.params.id;
-	}
+  method: "GET",
+  path: "/user/{id}",
+  handler: function(req, handler) {
+    return req.params.id;
+  }
 });
 
 server.route({
-	method: "POST",
-	path: "/user",
-	handler: function(req, handler) {
-		return "";
-	}
+  method: "POST",
+  path: "/user",
+  handler: function(req, handler) {
+    return "";
+  }
 });
 
 // Start the server
 async function start() {
-	try {
-		await server.start();
-	} catch (err) {
-		console.log(err);
-		process.exit(1);
-	}
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-	console.log("Server running at:", server.info.uri);
+  console.log("Server running at:", server.info.uri);
 }
 
 start();
--- javascript/nanoexpress/app.js
+++ javascript/nanoexpress/app.js
@@ -1,13 +1,13 @@
 const nanoexpress = require("nanoexpress");
 
 nanoexpress()
-	.get("/", (req, res) => {
-		res.end("");
-	})
-	.get("/user/:id", (req, res) => {
-		res.end(req.params.id);
-	})
-	.post("/user", (req, res) => {
-		res.end("");
-	})
-	.listen(3000);
+  .get("/", (req, res) => {
+    res.end("");
+  })
+  .get("/user/:id", (req, res) => {
+    res.end(req.params.id);
+  })
+  .post("/user", (req, res) => {
+    res.end("");
+  })
+  .listen(3000);
