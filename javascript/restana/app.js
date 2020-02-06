774f67c79675c8b30e0d529ad17e7059b54f0e45
Marwan Rabbâa <waghanza@gmail.com>
2020-02-06 09:21:08 +0100

    style: Format json files

--- javascript/restana/app.js
+++ javascript/restana/app.js
@@ -1,18 +1,18 @@
 const restana = require("restana");
 const service = restana({
-	disableResponseEvent: true
+  disableResponseEvent: true
 });
 
 service.get("/", function(req, res) {
-	res.send(200);
+  res.send(200);
 });
 
 service.get("/user/:id", function(req, res) {
-	res.send(req.params.id);
+  res.send(req.params.id);
 });
 
 service.post("/user", function(req, res) {
-	res.send(200);
+  res.send(200);
 });
 
 service.start(3000);
