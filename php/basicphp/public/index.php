317f08566ac6841864278c1479d3c45d42ccd2cb
ray-ang <46805160+ray-ang@users.noreply.github.com>
2020-02-08 15:21:30 -0500

    Updated index.php

--- php/basicphp/public/index.php
+++ php/basicphp/public/index.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * BasicPHP - A PHP Nano-Framework for Decoupled Application and Business Logic,
- *          - and Presentation. The aim of the project is for developers to build
- *          - applications that are framework-independent by decoupling the Model,
- *          - View and Controller from any framework, making the application portable
- *          - and compatible with the developer's framework of choice or plain PHP.
+ * BasicPHP - A frameworkless library-based approach for building web applications
+ *          - and application programming interfaces or API's.
+ *          - The aim of the project is for developers to build applications that are
+ *          - framework-independent using vanilla PHP, and native functions and API's.
  *          -
  *          - To embed the application to any framework, copy BasicPHP's configuration
  *          - file (config.php), functions library (functions.php), and the 'classes',
@@ -18,18 +17,12 @@
  * @license  MIT License
  */
 
-// Register the start time/memory as a float value
-$time_start = microtime(true);
-$memory_start = memory_get_usage();
+// // Register the start time/memory as a float value
+// $time_start = microtime(TRUE);
+// $memory_start = memory_get_usage();
 
-// Bootstrap configuration
-require_once '../config.php';
-
-// Functions library
-require_once '../functions.php';
-
-// Routing configuration
-require_once '../routes.php';
+// Load application
+require_once '../app.php';
 
 // // Register the end time/memory as a float value
 // $time_end = microtime(TRUE);
@@ -39,13 +32,12 @@ require_once '../routes.php';
 // $memory_used = $memory_end - $memory_start;
 // echo 'Lapse Time: ' . $time_lapse . ' seconds<br />';
 // echo 'Memory Usage: ' . $memory_used . ' bytes<br />';
-/*
-// Compute average load speed. Set $_SESSION['speed'] as an array.
-if (! isset($_SESSION['speed'])) { $_SESSION['speed'] = []; }
-$_SESSION['speed'][] = $time_lapse;
-// Average load speed
-echo 'The average load speed is: ' . (array_sum($_SESSION['speed'])/count($_SESSION['speed']));
-var_dump($_SESSION['speed']);
-// Place a comment on session_destroy() to start computing average load speed.
-session_destroy();
-*/
+
+// // Compute average load speed. Set $_SESSION['speed'] as an array.
+// if (! isset($_SESSION['speed'])) { $_SESSION['speed'] = []; }
+// $_SESSION['speed'][] = $time_lapse;
+// // Average load speed
+// echo 'The average load speed is: ' . (array_sum($_SESSION['speed'])/count($_SESSION['speed']));
+// var_dump($_SESSION['speed']);
+// // Place a comment on session_destroy() to start computing average load speed.
+// session_destroy();
