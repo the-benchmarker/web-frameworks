7caf6434224aad6a90561ab4686e5e16c664ba57
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2020-02-10 12:12:23 +0400

    [Crystal] Update grip to new syntax (#2254)

--- crystal/grip/src/server.cr
+++ crystal/grip/src/server.cr
@@ -1,38 +1,48 @@
 require "grip"
 
-class IndexHttpConsumer < Grip::HttpConsumer
-  def get(req)
-    headers("Content-Type", "text/html")
-    req.response.print(nil)
-  end
-end
-
-class UsersHttpConsumer < Grip::HttpConsumer
-  def get(req)
-    headers("Content-Type", "text/html")
-    req.response.print(url["id"])
-  end
-end
-
-class UserHttpConsumer < Grip::HttpConsumer
-  def post(req)
-    headers("Content-Type", "text/html")
-    req.response.print(nil)
-  end
-end
-
-get "/", IndexHttpConsumer
-get "/user/:id", UsersHttpConsumer
-post "/user", UserHttpConsumer
-
 Grip.config do |cfg|
   cfg.env = "production"
   cfg.logging = false
 end
 
+
+class IndexHttpConsumer < Grip::HttpConsumer
+  def get(req)
+    html(
+      nil
+    )
+  end
+end
+
+class UsersHttpConsumer < Grip::HttpConsumer
+  def get(req)
+    html(
+      url["id"]
+    )
+  end
+end
+
+class UserHttpConsumer < Grip::HttpConsumer
+  def post(req)
+    html(
+      nil
+    )
+  end
+end
+
+class Api < Grip::Application
+  scope do
+    get "/", IndexHttpConsumer
+    get "/user/:id", UsersHttpConsumer
+    post "/user", UserHttpConsumer
+  end
+end
+
+api = Api.new
+
 System.cpu_count.times do |_|
   Process.fork do
-    Grip.run do |config|
+    api.run do |config|
       server = config.server.not_nil!
       server.bind_tcp "0.0.0.0", 3000, reuse_port: true
     end
