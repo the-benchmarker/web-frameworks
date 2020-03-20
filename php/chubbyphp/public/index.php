d42107c319fd908684f90c1c17d543ecea85cedf
Dominik Zogg <dominik.zogg@gmail.com>
2020-03-20 16:26:42 +0100

    add chubbyphp

---
+++ php/chubbyphp/public/index.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Chubbyphp\Framework\Application;
+use Chubbyphp\Framework\ErrorHandler;
+use Chubbyphp\Framework\Middleware\ExceptionMiddleware;
+use Chubbyphp\Framework\Middleware\RouterMiddleware;
+use Chubbyphp\Framework\RequestHandler\CallbackRequestHandler;
+use Chubbyphp\Framework\Router\FastRouteRouter;
+use Chubbyphp\Framework\Router\Route;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Psr7\Factory\ServerRequestFactory;
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+set_error_handler([new ErrorHandler(), 'errorToException']);
+
+$responseFactory = new ResponseFactory();
+
+$app = new Application([
+    new ExceptionMiddleware($responseFactory, true),
+    new RouterMiddleware(new FastRouteRouter([
+        Route::get('/', 'home', new CallbackRequestHandler(
+            function () use ($responseFactory) {
+                $response = $responseFactory->createResponse();
+                $response->getBody()->write('');
+
+                return $response;
+            }
+        )),
+        Route::get('/user/{id}', 'user_view', new CallbackRequestHandler(
+            function (ServerRequestInterface $request) use ($responseFactory) {
+                $response = $responseFactory->createResponse();
+                $response->getBody()->write($request->getAttribute('id'));
+
+                return $response;
+            }
+        )),
+        Route::get('/user', 'user_list', new CallbackRequestHandler(
+            function () use ($responseFactory) {
+                $response = $responseFactory->createResponse();
+                $response->getBody()->write('');
+
+                return $response;
+            }
+        ))
+    ]), $responseFactory),
+]);
+
+$app->emit($app->handle((new ServerRequestFactory())->createFromGlobals()));
