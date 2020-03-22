98e84196ba2221c8726efb9c9e6eb63a0ce1ebb7
Mathieu DARTIGUES <mathieu@dartic.fr>
2020-03-22 16:34:16 +0100

    Add FeathersJS * test 3 routes seems ok * external repo available on https://github.com/mdartic/web-frameworks-feathersjs

---
+++ javascript/feathersjs/app.js
@@ -0,0 +1,20 @@
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+
+const app = express(feathers());
+
+app.set("etag", false);
+
+app.get("/", function(req, res) {
+  res.send("");
+});
+
+app.get("/user/:id", function(req, res) {
+  res.send(req.params.id);
+});
+
+app.post("/user", function(req, res) {
+  res.send("");
+});
+
+app.listen(3000, function() {});
