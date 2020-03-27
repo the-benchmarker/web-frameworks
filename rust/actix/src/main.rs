e2a0ede24cb01cbfea0d240286e918d99e504754
Oisín Mac Fhearaí <denpashogai@gmail.com>
2020-03-27 18:21:27 +0000

    Improvements from PR comments

--- rust/actix/src/main.rs
+++ rust/actix/src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> std::io::Result<()> {
             .route("/user", web::post().to(|| HttpResponse::Ok()))
             .route("/user/{id}", web::get().to(get_user))
     })
-    .bind("127.0.0.1:3000")?
+    .bind("0.0.0.0:3000")?
     .run()
     .await
 }
