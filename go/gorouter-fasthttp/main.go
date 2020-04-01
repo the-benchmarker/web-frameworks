4c187ee17c9543f042036c02b24ce85d9272e844
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2020-04-01 10:05:17 +0200

    Code refactor

--- go/gorouter-fasthttp/main.go
+++ go/gorouter-fasthttp/main.go
@@ -6,13 +6,9 @@ import (
 	"github.com/vardius/gorouter/v4/context"
 )
 
-func index(ctx *fasthttp.RequestCtx) {
-	ctx.SetBodyString("")
-}
+func index(ctx *fasthttp.RequestCtx) {}
 
-func user(ctx *fasthttp.RequestCtx) {
-	ctx.SetBodyString("")
-}
+func user(ctx *fasthttp.RequestCtx) {}
 
 func userID(ctx *fasthttp.RequestCtx) {
 	params := ctx.UserValue("params").(context.Params)
@@ -23,7 +19,7 @@ func main() {
 	router := gorouter.NewFastHTTPRouter()
 
 	router.GET("/", index)
-	router.GET("/user/{id:[0-9]+}", userID)
+	router.GET("/user/{id}", userID)
 	router.POST("/user", user)
 
 	fasthttp.ListenAndServe(":3000", router.HandleFastHTTP)
