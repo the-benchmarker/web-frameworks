e1080955998632369766bb18239385c2dc20eddc
Luffy <52o@qq52o.cn>
2020-04-03 13:21:11 +0800

    [PHP] Remove redundant configuration for simps (#2433)

--- php/simps/config/servers.php
+++ php/simps/config/servers.php
@@ -19,12 +19,8 @@ return [
         ],
         'settings' => [
             'enable_coroutine' => true,
-            'worker_num' => swoole_cpu_num() * 2,
-            'open_tcp_nodelay' => true,
+            'worker_num' => (int) shell_exec('nproc') ?? 32,
             'max_coroutine' => 100000,
-            'open_http2_protocol' => true,
-            'max_request' => 100000,
-            'socket_buffer_size' => 2 * 1024 * 1024,
         ],
     ],
 ];
