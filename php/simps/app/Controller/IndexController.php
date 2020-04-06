ff551ce1551e8860c4323706d3ee60317b78e1fa
Luffy <52o@qq52o.cn>
2020-04-06 20:57:03 +0800

    Update IndexController.php

--- php/simps/app/Controller/IndexController.php
+++ php/simps/app/Controller/IndexController.php
@@ -20,7 +20,7 @@ class IndexController
         $first_line = \strstr($data, "\r\n", true);
         $tmp = \explode(' ', $first_line, 3);
         $path = isset($tmp[1]) ? $tmp[1] : '/';
-        if (0 === \strpos($path, '/user/id/') && isset($path[6])) {
+        if (0 === \strpos($path, '/user/') && isset($path[6])) {
             $response = SimpleResponse::build(\substr($path, 6));
         } else {
             $response = SimpleResponse::build("");
