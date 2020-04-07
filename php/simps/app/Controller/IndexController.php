a1667c60e0515c72cbeabd50d43ccd6c2e88cbe7
Luffy <52o@qq52o.cn>
2020-04-07 15:20:14 +0800

    [PHP] Update for simps (#2444)

--- php/simps/app/Controller/IndexController.php
+++ php/simps/app/Controller/IndexController.php
@@ -9,23 +9,24 @@ declare(strict_types=1);
  * @license  https://github.com/simple-swoole/simps/blob/master/LICENSE
  */
 
-
 namespace App\Controller;
+
+use Simps\Server\Protocol\HTTP\SimpleResponse;
 
 class IndexController
 {
-    public function index($request, $response)
+    public function index($server, $fd)
     {
-        $response->end("");
+        $server->send($fd, SimpleResponse::build(""));
     }
 
-    public function get($request, $response, $data)
+    public function get($server, $fd, $data)
     {
-        $response->end((string)$data["id"] ?? "");
+        $server->send($fd, SimpleResponse::build((string)$data["id"] ?? ""));
     }
 
-    public function create($request, $response)
+    public function create($server, $fd)
     {
-        $response->end("");
+        $server->send($fd, SimpleResponse::build(""));
     }
 }
