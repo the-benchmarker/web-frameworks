f78634aa472424db212b7cbf5392bbd8b7257a8e
Fenny <25108519+Fenny@users.noreply.github.com>
2020-04-09 19:51:23 +0200

    Update main.go

--- go/fiber/main.go
+++ go/fiber/main.go
@@ -10,11 +10,27 @@ func main() {
 		CaseSensitive: true,
 		StrictRouting: true,
 	})
+	app.Get("/", func(c *fiber.Ctx) {
+		c.SendString("")
+	})
 	app.Get("/user/:id", func(c *fiber.Ctx) {
 		c.SendString(c.Params("id"))
 	})
-	app.Use(func(c *fiber.Ctx) {
-		return
+	app.Get("/user/1", func(c *fiber.Ctx) {
+		c.JSON()
 	})
+	app.Post("/user", func(c *fiber.Ctx) {
+		c.JSON()
+	})
+	app.Put("/user/1", func(c *fiber.Ctx) {
+		c.JSON()
+	})
+	app.Patch("/user/1", func(c *fiber.Ctx) {
+		c.JSON()
+	})
+	app.Delete("/user/1", func(c *fiber.Ctx) {
+		c.JSON()
+	})
+
 	app.Listen(3000)
 }
