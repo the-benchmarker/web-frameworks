d335257fc20aa35910c57edce364b753e6983e32
Fenny <25108519+Fenny@users.noreply.github.com>
2020-04-13 16:21:13 +0200

    [Go] Update fiber to v1.9.0 (#2462)

--- go/fiber/main.go
+++ go/fiber/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"strings"
-
 	"github.com/gofiber/fiber"
 )
-
-const pathUser = "/user/"
 
 func main() {
 	app := fiber.New(&fiber.Settings{
@@ -14,10 +10,27 @@ func main() {
 		CaseSensitive: true,
 		StrictRouting: true,
 	})
-	app.Use(func(c *fiber.Ctx) {
-		if strings.HasPrefix(c.Path(), pathUser) {
-			c.SendString(c.Path()[6:])
-		}
+	app.Get("/", func(c *fiber.Ctx) {
+		return
 	})
+	app.Get("/user/:id", func(c *fiber.Ctx) {
+		c.SendString(c.Params("id"))
+	})
+	// app.Get("/user/1", func(c *fiber.Ctx) {
+	// 	c.JSON("")
+	// })
+	app.Post("/user", func(c *fiber.Ctx) {
+		return
+	})
+	// app.Put("/user/1", func(c *fiber.Ctx) {
+	// 	c.JSON("")
+	// })
+	// app.Patch("/user/1", func(c *fiber.Ctx) {
+	// 	c.JSON("")
+	// })
+	// app.Delete("/user/1", func(c *fiber.Ctx) {
+	// 	c.JSON("")
+	// })
+
 	app.Listen(3000)
 }
