5f9980ed3e9f3a3ffd763c5b4bfd473a9f4fd864
Marwan Rabbâa <waghanza@gmail.com>
2020-04-14 22:22:01 +0200

    fix(ci): Consider using TRAVIS_PULL_REQUEST_BRANCH for PRs

--- .ci/has_to_run.sh
+++ .ci/has_to_run.sh
@@ -1,33 +1,53 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -euo pipefail
-IFS=$'\n\t'
-set -vx
+travis_terminate() {
+  if [[ ! "${TRAVIS_OS_NAME}" ]]; then
+    return
+  fi
 
-# Always runs if master
-if [ "${TRAVIS_BRANCH}" == "master" ] ; then
-  exit 0
-fi
+  "_travis_terminate_${TRAVIS_OS_NAME}" "${@}"
+}
+
+_travis_terminate_linux() {
+  _travis_terminate_unix "${@}"
+}
+
+_travis_terminate_osx() {
+  _travis_terminate_unix "${@}"
+}
+
+_travis_terminate_unix() {
+  set +e
+  [[ "${TRAVIS_FILTERED}" == redirect_io && -e /dev/fd/9 ]] &&
+    sync &&
+    command exec 1>&9 2>&9 9>&- &&
+    sync
+  pgrep -u "${USER}" | grep -v -w "${$}" >"${TRAVIS_TMPDIR}/pids_after"
+  awk 'NR==FNR{a[$1]++;next};!($1 in a)' "${TRAVIS_TMPDIR}"/pids_{before,after} |
+    xargs kill &>/dev/null || true
+  pkill -9 -P "${$}" &>/dev/null || true
+  exit "${1}"
+}
+
 
 # List of updated files
-git diff master.. --name-only  > /tmp/files.txt
+git diff master.. --name-only > /tmp/list.txt
 
-FWK=`echo ${FRAMEWORK} | awk -F '.' '{print $1}'`
-LNG=`echo ${FRAMEWORK} | awk -F '.' '{print $2}'`
+echo "Modified files are"
+cat /tmp/list.txt
 
-grep "${FWK}" /tmp/files.txt
+CURRENT_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 
-if [ $? -eq 0 ]; then
-  echo "${FWK} has been updated, running CI"
+if [[ "${CURRENT_BRANCH}" == "master" ]] ; then
+  echo "We are on master, run ..."
   exit 0
 fi
 
-grep "${LNG}" /tmp/files.txt
+LNG=`echo ${FRAMEWORK} | awk -F '.' '{print $1}'`
+FWK=`echo ${FRAMEWORK} | awk -F '.' '{print $2}'`
 
-if [ $? -eq 0 ]; then
-  echo "${LNG} has been updated, running CI"
-  exit 0
-fi
+grep "${FWK}" /tmp/list.txt && exit 0
+grep "${LNG}" /tmp/list.txt && exit 0
 
-echo "No modification detected, exiting ..."
+echo "No modification, exiting ..."
 exit 1
