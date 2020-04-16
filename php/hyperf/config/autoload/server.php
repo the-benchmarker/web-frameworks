487e9b091be06254bd91a2a9255cf826eb1eb752
李铭昕 <715557344@qq.com>
2020-04-17 03:30:48 +0800

    [PHP] Upgrade hyperf to 1.1.* (#2486)

--- php/hyperf/config/autoload/server.php
+++ php/hyperf/config/autoload/server.php
@@ -7,9 +7,8 @@ declare(strict_types=1);
  * @link     https://www.hyperf.io
  * @document https://doc.hyperf.io
  * @contact  group@hyperf.io
- * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-
 use Hyperf\Server\Server;
 use Hyperf\Server\SwooleEvent;
 
@@ -23,7 +22,7 @@ return [
             'port' => 3000,
             'sock_type' => SWOOLE_SOCK_TCP,
             'callbacks' => [
-                SwooleEvent::ON_REQUEST => [Hyperf\HttpServer\Server::class, 'onRequest'],
+                SwooleEvent::ON_REQUEST => [App\Kernel\FastServer::class, 'onRequest'],
             ],
         ],
     ],
@@ -34,7 +33,7 @@ return [
         'open_tcp_nodelay' => true,
         'max_coroutine' => 100000,
         'open_http2_protocol' => true,
-        'max_request' => 100000,
+        'max_request' => 0,
         'socket_buffer_size' => 2 * 1024 * 1024,
     ],
     'callbacks' => [
