4e4976619360f94b77a3ab3c9030e7751fad1dde
Edgar Espina <espina.edgar@gmail.com>
2020-04-18 13:43:36 -0300

    jooby implementation - Added the Kotlin version too (kooby)

---
+++ kotlin/kooby/src/main/kotlin/benchmark/App.kt
@@ -0,0 +1,19 @@
+package benchmark
+
+import io.jooby.ExecutionMode.EVENT_LOOP
+import io.jooby.StatusCode
+import io.jooby.runApp
+
+fun main(args: Array<String>) {
+  runApp(args, EVENT_LOOP) {
+    serverOptions {
+      port = 3000
+      // Turn off Date and Server Response headers.
+      defaultHeaders = false
+    }
+
+    get("/") { ctx.send(StatusCode.OK) }
+    get("/user/{id}") { ctx.send(ctx.path("id").value()) }
+    get("/user") { ctx.send(StatusCode.OK) }
+  }
+}
