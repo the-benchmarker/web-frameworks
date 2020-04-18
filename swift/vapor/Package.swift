6b291212be39a4fa5c293a508dbb5c9a847f92d6
Marwan Rabbâa <waghanza@gmail.com>
2020-04-18 13:43:55 +0200

    chore(swift): Update vapor to 4.3

--- swift/vapor/Package.swift
+++ swift/vapor/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "which_is_the_fastest",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0"))
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "4.3.0"))
     ],
     targets: [
         .target(name: "server", dependencies: ["Vapor"], path: ".")
