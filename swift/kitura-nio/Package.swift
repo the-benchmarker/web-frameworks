811982ce30b969d7b7b76918c356f2e04bee9dae
Marwan Rabbâa <waghanza@gmail.com>
2020-04-18 18:12:44 +0200

    [Swift]  Update kitura to 2.9 (#2501)

--- swift/kitura-nio/Package.swift
+++ swift/kitura-nio/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "server",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura", .upToNextMinor(from: "2.8.0"))
+        .package(url: "https://github.com/IBM-Swift/Kitura", .upToNextMinor(from: "2.9.0"))
     ],
     targets: [
        .target(name: "server", dependencies: ["Kitura"], path: ".")
