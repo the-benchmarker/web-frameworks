0c6f4d5ff055d85018aff23acf140e893129ae1e
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2020-04-20 21:51:19 +0700

    add rails-jruby

---
+++ ruby/rails-jruby/config/application.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'rails'
+require 'action_controller/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ServerRails
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+  end
+end
---
+++ ruby/rails-jruby/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Be sure to restart your server when you modify this file.
+
+# ApplicationController.renderer.defaults.merge!(
+#   http_host: 'example.org',
+#   https: false
+# )
