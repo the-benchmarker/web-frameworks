43bfdc5fd81d920c42845eabd840ff2dc119f1ed
Tim <0xtimc@gmail.com>
2020-04-20 14:15:53 +0100

    Add Vapor as a dependency

--- swift/vapor/Package.swift
+++ swift/vapor/Package.swift
@@ -1,13 +1,21 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "which_is_the_fastest",
+    name: "WhichIsTheFastest",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0"))
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.3.0")
     ],
     targets: [
-        .target(name: "server", dependencies: ["Vapor"], path: ".")
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Run",
+            dependencies: [
+//                .product(name: "Vapor", package: "vapor")
+            ]),
     ]
 )
