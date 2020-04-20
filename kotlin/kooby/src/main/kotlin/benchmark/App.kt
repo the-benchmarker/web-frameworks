a236165c1d449ba797f058cdbc8aad7faed60dae
Marwan Rabbâa <waghanza@gmail.com>
2020-04-20 13:11:17 +0200

    chore(kooby): Format source code

--- kotlin/kooby/src/main/kotlin/benchmark/App.kt
+++ kotlin/kooby/src/main/kotlin/benchmark/App.kt
@@ -5,15 +5,14 @@ import io.jooby.StatusCode
 import io.jooby.runApp
 
 fun main(args: Array<String>) {
-  runApp(args, EVENT_LOOP) {
-    serverOptions {
-      port = 3000
-      // Turn off Date and Server Response headers.
-      defaultHeaders = false
+    runApp(args, EVENT_LOOP) {
+        serverOptions {
+        port = 3000
+        defaultHeaders = false
     }
 
     get("/") { ctx.send(StatusCode.OK) }
     get("/user/{id}") { ctx.send(ctx.path("id").value()) }
     post("/user") { ctx.send(StatusCode.OK) }
-  }
+    }
 }
