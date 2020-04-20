2b8cc25e958ef7727d95cb908fc0b7194e98b8c2
Tim <0xtimc@gmail.com>
2020-04-20 14:36:17 +0100

    Add same routes as before

--- swift/vapor-framework/Sources/Run/main.swift
+++ swift/vapor-framework/Sources/Run/main.swift
@@ -1,8 +1,20 @@
-//import Vapor
-//
-//let app = Application()
-//defer { app.shutdown() }
-//
-//
-//
-//try app.run()
+import Vapor
+
+let app = Application()
+defer { app.shutdown() }
+
+let response = Response()
+
+app.get { _ in
+    response
+}
+
+app.get("user", ":userID") { req in
+    req.parameters.get("userID") ?? ""
+}
+
+app.post("empty") { _ in
+    return response
+}
+
+try app.run()
