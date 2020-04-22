6378f3facda9b4f7b259c205a250350326d404be
Marwan Rabbâa <waghanza@gmail.com>
2020-04-22 14:46:30 +0200

    fix(http4s): Change hostname and port

--- scala/http4s/src/main/scala/the/benchmarker/http4s/Main.scala
+++ scala/http4s/src/main/scala/the/benchmarker/http4s/Main.scala
@@ -20,7 +20,7 @@ object Main extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO]
-      .bindHttp(8080, "localhost")
+      .bindHttp(3000, "0.0.0.0")
       .withHttpApp(helloWorldService)
       .serve
       .compile
