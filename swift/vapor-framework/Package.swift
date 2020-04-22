c4041b6220d9d8c828f0009132ea154db57c1969
Marwan Rabbâa <waghanza@gmail.com>
2020-04-22 09:19:31 +0200

    [Swift] Upgrade vapor to 4.3 (#2534)

---
+++ swift/vapor-framework/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "server",
+    platforms: [
+        .macOS("10.15")
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "4.3.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "server",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor")
+            ],
+            path: "."),
+    ]
+)
