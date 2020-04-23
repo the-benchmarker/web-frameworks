a696612209be66793eea7bcfb2f22ed5357bdf60
zieglar <zieglar@gmail.com>
2020-04-23 15:31:42 +0800

    submit nestjs fastify/express source code

--- javascript/nestjs-express/nest/app.controller.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class AppController {
-    getHello(): string;
-    getUserId(id: string): string;
-    postUser(): string;
-}
---
+++ javascript/nestjs-fastify/src/app.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(3000);
+}
+bootstrap();
