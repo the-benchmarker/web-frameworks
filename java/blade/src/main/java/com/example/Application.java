ea6983d9c332b1ede45369d7d7f6aa89d8a14714
Marco F <whiplash@users.noreply.github.com>
2020-05-04 09:58:21 +0200

    Fixes (mainly Content-Type for responses).

--- java/blade/src/main/java/com/example/Application.java
+++ java/blade/src/main/java/com/example/Application.java
@@ -5,6 +5,12 @@ import com.blade.Blade;
 public class Application {
 
     public static void main(String[] args) {
-        Blade.of().get("/", ctx -> ctx.text("")).start(Application.class, args);
+        Blade.of()
+          .get("/", ctx -> ctx.text(""))
+          .get("/user/:id", (request, response) -> {
+            response.text(request.pathString("id"));
+          })
+          .post("/user", ctx -> ctx.text(""))
+          .start(Application.class, args);
     }
 }
