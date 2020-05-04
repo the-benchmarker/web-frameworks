462a2eec0f937a5da4fb65e7283e2cdf7736b639
Marco F <whiplash@users.noreply.github.com>
2020-05-04 17:43:31 +0200

    Initial work for clojure.luminus. Addresses #1391

---
+++ clojure/luminus/env/dev/clj/luminus/dev_middleware.clj
@@ -0,0 +1,11 @@
+(ns luminus.dev-middleware
+  (:require
+    [ring.middleware.reload :refer [wrap-reload]]
+    [selmer.middleware :refer [wrap-error-page]]
+    [prone.middleware :refer [wrap-exceptions]]))
+
+(defn wrap-dev [handler]
+  (-> handler
+      wrap-reload
+      wrap-error-page
+      (wrap-exceptions {:app-namespaces ['luminus]})))
