869ecb4fdf19134744c3477030e2e56444bd8ec8
Adam Hopkins <admhpkns@gmail.com>
2020-05-19 12:44:36 +0300

    Modify logging to use WARNING like other frameworks

--- python/sanic/server.py
+++ python/sanic/server.py
@@ -3,7 +3,55 @@ import sys
 from sanic import Sanic
 from sanic.response import text
 
-app = Sanic("benchmark")
+LOGGING_CONFIG = dict(
+    version=1,
+    disable_existing_loggers=False,
+    loggers={
+        "sanic.root": {"level": "WARNING", "handlers": ["console"]},
+        "sanic.error": {
+            "level": "WARNING",
+            "handlers": ["error_console"],
+            "propagate": True,
+            "qualname": "sanic.error",
+        },
+        "sanic.access": {
+            "level": "WARNING",
+            "handlers": ["access_console"],
+            "propagate": True,
+            "qualname": "sanic.access",
+        },
+    },
+    handlers={
+        "console": {
+            "class": "logging.NullHandler",
+            "formatter": "generic",
+        },
+        "error_console": {
+            "class": "logging.NullHandler",
+            "formatter": "generic",
+        },
+        "access_console": {
+            "class": "logging.NullHandler",
+            "formatter": "access",
+        },
+    },
+    formatters={
+        "generic": {
+            "format": "%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter",
+        },
+        "access": {
+            "format": "%(asctime)s - (%(name)s)[%(levelname)s][%(host)s]: "
+            + "%(request)s %(message)s %(status)d %(byte)d",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter",
+        },
+    },
+)
+
+
+app = Sanic("benchmark", log_config=LOGGING_CONFIG)
 
 
 @app.route("/")
