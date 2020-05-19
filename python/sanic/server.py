c9585a0f8e0b13f145a923ca9f8d39c42a833f82
Adam Hopkins <adam@amhopkins.com>
2020-05-19 21:53:01 +0300

    [Python] Avoid gunicorn for sanic (#2685)

--- python/sanic/server.py
+++ python/sanic/server.py
@@ -1,7 +1,57 @@
+import multiprocessing
+
 from sanic import Sanic
 from sanic.response import text
 
-app = Sanic(log_config=None)
+LOGGING_CONFIG = dict(
+    version=1,
+    disable_existing_loggers=False,
+    loggers={
+        "sanic.root": {"level": "WARNING", "handlers": ["console"]},
+        "sanic.error": {
+            "level": "WARNING",
+            "handlers": ["error_console"],
+            "propagate": True,
+            "qualname": "sanic.error",
+        },
+        "sanic.access": {
+            "level": "WARNING",
+            "handlers": ["access_console"],
+            "propagate": True,
+            "qualname": "sanic.access",
+        },
+    },
+    handlers={
+        "console": {
+            "class": "logging.NullHandler",
+            "formatter": "generic",
+        },
+        "error_console": {
+            "class": "logging.NullHandler",
+            "formatter": "generic",
+        },
+        "access_console": {
+            "class": "logging.NullHandler",
+            "formatter": "access",
+        },
+    },
+    formatters={
+        "generic": {
+            "format": "%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter",
+        },
+        "access": {
+            "format": "%(asctime)s - (%(name)s)[%(levelname)s][%(host)s]: "
+            + "%(request)s %(message)s %(status)d %(byte)d",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter",
+        },
+    },
+)
+
+
+app = Sanic("benchmark", log_config=LOGGING_CONFIG)
 
 
 @app.route("/")
@@ -17,3 +67,8 @@ async def user_info(request, id):
 @app.route("/user", methods=["POST"])
 async def user(request):
     return text("")
+
+if __name__ == "__main__":
+    workers = multiprocessing.cpu_count()
+    app.run(host='0.0.0.0', port=3000, workers=workers,
+            debug=False, access_log=False)
