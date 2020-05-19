c1a6fa13f4f88e07d66f0fba05ffd306f26f1601
Adam Hopkins <adam@amhopkins.com>
2020-05-19 21:53:01 +0300

    [Python] Avoid gunicorn for sanic (#2685)

--- javascript/hapi/app.js
+++ javascript/hapi/app.js
@@ -5,32 +5,32 @@ const Hapi = require("@hapi/hapi");
 // Create a server with a host and port
 const server = Hapi.server({
   host: "0.0.0.0",
-  port: 3000
+  port: 3000,
 });
 
 // Add the route
 server.route({
   method: "GET",
   path: "/",
-  handler: function(req, handler) {
+  handler: function (req, handler) {
     return "";
-  }
+  },
 });
 
 server.route({
   method: "GET",
   path: "/user/{id}",
-  handler: function(req, handler) {
+  handler: function (req, handler) {
     return req.params.id;
-  }
+  },
 });
 
 server.route({
   method: "POST",
   path: "/user",
-  handler: function(req, handler) {
+  handler: function (req, handler) {
     return "";
-  }
+  },
 });
 
 // Start the server
