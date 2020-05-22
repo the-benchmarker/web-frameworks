56ea07565ced3d9aab2501e06384b6eeed3a5119
Marwan Rabbâa <waghanza@gmail.com>
2020-05-23 00:03:28 +0200

    feat(php): Stabilize builds

--- php/imi/config/beans.php
+++ php/imi/config/beans.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'hotUpdate'    =>    [
-        'status'    =>    false, // 关闭热更新去除注释，不设置即为开启，建议生产环境关闭
+        'status'    =>    false,
     ],
     'Logger'    =>    [
         'coreHandlers'    =>    [
--- php/one/App/Config/router.php
+++ php/one/App/Config/router.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * 路由设置
- */
-
 use One\Http\Router;
 
 Router::get('/', \App\Controllers\IndexController::class . '@index');
