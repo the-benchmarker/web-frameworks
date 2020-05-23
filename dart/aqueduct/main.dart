5a0532293fcaf70f685b19cb4cf2125be84a7dd0
Marwan Rabbâa <waghanza@gmail.com>
2020-05-23 18:05:52 +0200

    chore: Enfore styles

--- dart/aqueduct/main.dart
+++ dart/aqueduct/main.dart
@@ -4,30 +4,22 @@ import 'dart:math';
 
 import 'package:aqueduct/aqueduct.dart';
 
-
 class AqueductDartChannel extends ApplicationChannel {
   @override
   Controller get entryPoint {
     final router = Router();
 
-    router.route("/").linkFunction((request) => 
-      Response.ok("")
-        ..contentType = new ContentType("text", "plain")
-    );
+    router.route("/").linkFunction((request) =>
+        Response.ok("")..contentType = new ContentType("text", "plain"));
 
-    router.route("/user/:id").linkFunction(
-        (Request request) =>
-          Response.ok(request.path.variables['id'])
-            ..contentType = new ContentType("text", "plain")
-      );
-        
-        
-    router.route("/user").linkFunction(
-        (Request request) {
-        if(request.method != 'POST') return Response.notFound();
-        return Response.ok("")
-          ..contentType = new ContentType("text", "plain");
-        });
+    router.route("/user/:id").linkFunction((Request request) =>
+        Response.ok(request.path.variables['id'])
+          ..contentType = new ContentType("text", "plain"));
+
+    router.route("/user").linkFunction((Request request) {
+      if (request.method != 'POST') return Response.notFound();
+      return Response.ok("")..contentType = new ContentType("text", "plain");
+    });
 
     return router;
   }
