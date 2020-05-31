eacbb2844132b41837554ed3588e67f1e769b0af
Restyled.io <commits@restyled.io>
2020-05-31 21:37:39 +0000

    Restyled by php-cs-fixer

--- php/one-fpm/App/Config/cache.php
+++ php/one-fpm/App/Config/cache.php
@@ -3,20 +3,20 @@ return [
     'drive' => 'file', // [file | redis]
 
     'file' => [
-        'path' => _APP_PATH_ . '/RunCache/cache', 
-        'prefix' => 'one_' 
+        'path' => _APP_PATH_ . '/RunCache/cache',
+        'prefix' => 'one_'
     ],
 
-    'redis' => [ 
-        'default' => [ 
+    'redis' => [
+        'default' => [
             'max_connect_count' => 10,
             'host' => '127.0.0.1',
             'port' => 6379,
             'prefix' => 'one_',
 //            'auth' => '123456'
         ],
-        'default_cluster' => [ 
-            'max_connect_count' => 10, 
+        'default_cluster' => [
+            'max_connect_count' => 10,
             'args' => [
                 null,
                 ['192.168.1.10:7000','192.168.1.10:7001'],
