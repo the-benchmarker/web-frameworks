4a16f78960dce90e702a222af3a03bf06ad87d72
Ryan Riley <ryanriley@live.com>
2020-06-04 21:10:21 -0500

    Add Falco

---
+++ fsharp/falco/Program.fs
@@ -0,0 +1,20 @@
+open Falco
+open Microsoft.AspNetCore.Http
+
+let homeHandler = textOut ""
+
+let userIdHandler =
+    fun next (ctx:HttpContext) ->
+        let userId = ctx.TryGetRouteValue "id" |> Option.defaultValue ""
+        textOut userId next ctx
+
+let userHandler = textOut ""
+
+[<EntryPoint>]
+let main args =
+    webApp {
+        get "/" homeHandler
+        get "/user/{id}" userIdHandler
+        post "/user" userHandler
+    }
+    0
