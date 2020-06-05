a51ed673fe39c344420ca93592405c83f3bd22df
Ryan Riley <ryanriley@live.com>
2020-06-04 20:55:26 -0500

    Add Giraffe

--- fsharp/giraffe/Program.fs
+++ fsharp/giraffe/Program.fs
@@ -9,17 +9,28 @@ open Giraffe
 // ---------------------------------
 
 let webApp =
+<<<<<<< HEAD
     choose
         [ GET >=> choose
                       [ route "/" >=> text ""
                         routef "/user/%s" text ]
           POST >=> route "/user" >=> text ""
           setStatusCode 404 >=> text "Not Found" ]
+=======
+  choose [ 
+    GET >=> choose [
+        route "/" >=> text ""
+        routef "/user/%s" text ]
+    POST >=> route "/user" >=> text ""
+    setStatusCode 404 >=> text "Not Found"
+  ]
+>>>>>>> 4a479bc3... Add Giraffe
 
 // ---------------------------------
 // Config and Main
 // ---------------------------------
 
+<<<<<<< HEAD
 let configureApp (app: IApplicationBuilder) = app.UseGiraffe(webApp)
 
 let configureServices (services: IServiceCollection) = services.AddGiraffe() |> ignore
@@ -29,3 +40,20 @@ let main _ =
     WebHostBuilder().UseKestrel().Configure(Action<IApplicationBuilder> configureApp)
         .ConfigureServices(configureServices).Build().Run()
     0
+=======
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe(webApp)
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
+    0
+>>>>>>> 4a479bc3... Add Giraffe
