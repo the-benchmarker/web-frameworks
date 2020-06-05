57528abe4256cf32294115b5031ceb531be66d9a
Ryan Riley <ryanriley@live.com>
2020-06-04 22:51:20 -0500

    Add WebSharper

---
+++ fsharp/websharper/Main.fs
@@ -0,0 +1,57 @@
+namespace web
+
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open WebSharper
+open WebSharper.AspNetCore
+open WebSharper.Sitelets
+open WebSharper.UI
+open WebSharper.UI.Server
+
+type EndPoint =
+    | [<EndPoint "GET /">] Home
+    | [<EndPoint "GET /user">] GetUser of id:string
+    | [<EndPoint "POST /user">] User
+
+module Site =
+
+    let Main =
+        Application.MultiPage (fun ctx endpoint ->
+            match endpoint with
+            | Home -> Content.Text ""
+            | GetUser id -> Content.Text id
+            | User -> Content.Text ""
+            |> Content.WithContentType "text/plain"
+        )
+
+type Website(config: IConfiguration) =
+    inherit SiteletService<EndPoint>()
+    override val Sitelet = Site.Main
+
+type Startup() =
+
+    member this.ConfigureServices(services: IServiceCollection) =
+        services.AddSitelet<Website>()
+        |> ignore
+
+    member this.Configure(app: IApplicationBuilder) =
+        app
+            .UseWebSharper()
+            .Run(fun context ->
+                context.Response.StatusCode <- 404
+                context.Response.WriteAsync("Page not found"))
+
+module Program =
+
+    [<EntryPoint>]
+    let main args =
+        WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .Build()
+            .Run()
+        0
