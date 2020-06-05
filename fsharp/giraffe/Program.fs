514d7ae196246a10f0269cd787940e532dea0f6b
Ryan Riley <ryanriley@live.com>
2020-06-04 21:36:45 -0500

    Include frameworks in build

--- fsharp/giraffe/Program.fs
+++ fsharp/giraffe/Program.fs
@@ -9,28 +9,17 @@ open Giraffe
 // ---------------------------------
 
 let webApp =
-<<<<<<< HEAD
     choose
         [ GET >=> choose
                       [ route "/" >=> text ""
                         routef "/user/%s" text ]
           POST >=> route "/user" >=> text ""
           setStatusCode 404 >=> text "Not Found" ]
-=======
-  choose [ 
-    GET >=> choose [
-        route "/" >=> text ""
-        routef "/user/%s" text ]
-    POST >=> route "/user" >=> text ""
-    setStatusCode 404 >=> text "Not Found"
-  ]
->>>>>>> 4a479bc3... Add Giraffe
 
 // ---------------------------------
 // Config and Main
 // ---------------------------------
 
-<<<<<<< HEAD
 let configureApp (app: IApplicationBuilder) = app.UseGiraffe(webApp)
 
 let configureServices (services: IServiceCollection) = services.AddGiraffe() |> ignore
@@ -40,20 +29,3 @@ let main _ =
     WebHostBuilder().UseKestrel().Configure(Action<IApplicationBuilder> configureApp)
         .ConfigureServices(configureServices).Build().Run()
     0
-=======
-let configureApp (app : IApplicationBuilder) =
-    app.UseGiraffe(webApp)
-
-let configureServices (services : IServiceCollection) =
-    services.AddGiraffe() |> ignore
-
-[<EntryPoint>]
-let main _ =
-    WebHostBuilder()
-        .UseKestrel()
-        .Configure(Action<IApplicationBuilder> configureApp)
-        .ConfigureServices(configureServices)
-        .Build()
-        .Run()
-    0
->>>>>>> 4a479bc3... Add Giraffe
