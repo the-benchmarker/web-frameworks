938b4e1875707179eb1971c1243639e1f5609ee5
Marwan Rabbâa <waghanza@gmail.com>
2020-06-07 15:21:34 +0200

    chore(javascript): Trailing commas required for typescript

--- javascript/nestjs-fastify/src/app.ts
+++ javascript/nestjs-fastify/src/app.ts
@@ -8,7 +8,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
   await app.listen(3000, "0.0.0.0");
 }
