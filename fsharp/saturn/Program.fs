8a045cf69763902ad179f4e0bcf2cd770af35d63
Restyled.io <commits@restyled.io>
2020-06-07 21:14:41 +0000

    Restyled by fantomas

--- fsharp/saturn/Program.fs
+++ fsharp/saturn/Program.fs
@@ -1,21 +1,20 @@
-﻿// Learn more about F# at http://fsharp.org
+// Learn more about F# at http://fsharp.org
 
 open System
 open Giraffe.ResponseWriters
 open Giraffe.Core
 open Saturn
 
-let topRouter = router {
-    get "/" (text "")
-    getf "/user/%s" text
-    post "/user" (text "")
+let topRouter =
+    router {
+        get "/" (text "")
+        getf "/user/%s" text
+        post "/user" (text "")
 
-    not_found_handler (setStatusCode 404 >=> text "Not Found")
-}
+        not_found_handler (setStatusCode 404 >=> text "Not Found")
+    }
 
-let app = application {
-    use_router topRouter
-}
+let app = application { use_router topRouter }
 
 [<EntryPoint>]
 let main _ =
