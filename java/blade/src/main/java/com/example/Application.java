78e4ae40161b8063f46b6da90f33fece73e0b787
Marwan Rabbâa <waghanza@gmail.com>
2020-06-07 10:31:07 +0200

    chore: Rebase

--- java/blade/src/main/java/com/example/Application.java
+++ java/blade/src/main/java/com/example/Application.java
@@ -3,14 +3,11 @@ package com.example;
 import com.blade.Blade;
 
 public class Application {
-
-    public static void main(String[] args) {
-        Blade.of()
-          .get("/", ctx -> ctx.text(""))
-          .get("/user/:id", (request, response) -> {
-            response.text(request.pathString("id"));
-          })
-          .post("/user", ctx -> ctx.text(""))
-          .start(Application.class, args);
-    }
+  public static void main(String[] args) {
+    Blade.of()
+        .get("/", ctx -> ctx.text(""))
+        .get("/user/:id", (request, response) -> { response.text(request.pathString("id")); })
+        .post("/user", ctx -> ctx.text(""))
+        .start(Application.class, args);
+  }
 }
--- python/.config/gunicorn.py
+++ python/.config/gunicorn.py
@@ -4,10 +4,12 @@ import sys
 
 workers = multiprocessing.cpu_count()
 bind = "0.0.0.0:3000"
-worker_class = os.environ['WORKER_CLASS']
+worker_class = os.environ["WORKER_CLASS"]
 
 if worker_class == "meinheld.gmeinheld.MeinheldWorker":
+
     def post_fork(server, worker):
         # Disalbe access log
         import meinheld.server
+
         meinheld.server.set_access_logger(None)
