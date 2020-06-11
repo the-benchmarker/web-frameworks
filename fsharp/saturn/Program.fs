8cfe9633cc82c531b58c44e3ac3b4ea9ee64cc78
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-06-11 19:33:31 +0200

    [F#] Add Saturn 🪐 (#2897)

---
+++ fsharp/saturn/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Giraffe.ResponseWriters
+open Giraffe.Core
+open Saturn
+
+let topRouter = router {
+    get "/" (text "")
+    getf "/user/%s" text
+    post "/user" (text "")
+
+    not_found_handler (setStatusCode 404 >=> text "Not Found")
+}
+
+let app = application {
+    use_router topRouter
+}
+
+[<EntryPoint>]
+let main _ =
+    run app
+    0 // return an integer exit code
