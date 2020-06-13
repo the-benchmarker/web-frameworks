8a777cb19d2e79921f1d8f41e9449463d3246511
Marwan Rabbâa <waghanza@gmail.com>
2020-06-13 20:58:41 +0200

    feat(tonberry): Add route

--- python/tonberry/server.py
+++ python/tonberry/server.py
@@ -4,12 +4,6 @@ from tonberry import create_app, expose
 from tonberry.content_types import TextPlain
 
 
-class User:
-    @expose.get
-    async def index(self, user_id: int) -> TextPlain:
-        return f"{user_id}"
-
-
 class Root:
     @expose.get
     async def index(self) -> TextPlain:
@@ -18,6 +12,10 @@ class Root:
     @expose.post
     async def user(self) -> TextPlain:
         return ""
+
+    @expose.get
+    async def user(self, user_id: int) -> TextPlain:
+        return user_id
 
 
 if __name__ == "__main__":
