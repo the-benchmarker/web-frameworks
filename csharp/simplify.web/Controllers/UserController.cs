2aaa7da1a871ac6707a2e8ae2049210d0646e620
Ryan Riley <ryanriley@live.com>
2020-06-16 11:53:52 -0500

    [C#] Add Simplify.Web #2973

---
+++ csharp/simplify.web/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Simplify.Web;
+using Simplify.Web.Attributes;
+
+namespace web.Controllers
+{
+	[Post("/user")]
+	public class UserController : Controller
+	{
+		public override ControllerResponse Invoke() => StatusCode(200);
+	}
+}
---
+++ csharp/simplify.web/Controllers/UserIdController.cs
@@ -0,0 +1,15 @@
+﻿using Simplify.Web;
+using Simplify.Web.Attributes;
+
+namespace web.Controllers
+{
+	[Get("/user/{id}")]
+	public class UserIdController : Controller
+	{
+		public override ControllerResponse Invoke()
+		{
+			var content = this.Context.Route.Substring(6);
+			return Content(content);
+		}
+	}
+}
