c92f2c2cec1a1135af0b9164d5bd2bd82233e202
Marwan Rabbâa <waghanza@gmail.com>
2020-06-18 17:38:28 +0200

    fix(runcobo): Specify url params

--- crystal/runcobo/src/server.cr
+++ crystal/runcobo/src/server.cr
@@ -16,10 +16,9 @@ class CreateUser < BaseAction
 end
 class ShowUser < BaseAction
   get "/user/:id"
-  query NamedTuple(id: Int32)
+  url NamedTuple(id: Int32)
 
   call do
-pp params
     render_plain params[:id].to_s
   end
 end
