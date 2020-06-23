b1f26aa9b6079ff94d98b059ee589f3b139cdcc0
Marwan Rabbâa <waghanza@gmail.com>
2020-06-23 09:24:06 +0200

    [Ruby] Add pakyow (#3003)

---
+++ ruby/pakyow/config/application.rb
@@ -0,0 +1,17 @@
+Pakyow.app :pakyow, only: [:routing] do
+  # Learn more about configuring an application:
+  #
+  #   * https://pakyow.com/docs/configuration
+  #
+  configure do
+    # Application config for all environments.
+  end
+
+  configure :development do
+    # Application config for the development environment.
+  end
+
+  configure :production do
+    # Application config for the production environment.
+  end
+end
