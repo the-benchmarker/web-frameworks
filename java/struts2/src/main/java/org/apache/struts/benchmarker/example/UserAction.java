44aef761264d5d1174fd8e193371f264ea7f3363
Lukasz Lenart <lukasz.lenart@gmail.com>
2020-06-23 18:46:02 +0200

    [Java] Add struts2 (#3005)

---
+++ java/struts2/src/main/java/org/apache/struts/benchmarker/example/UserAction.java
@@ -0,0 +1,32 @@
+package org.apache.struts.benchmarker.example;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+
+public class UserAction {
+
+    private String id;
+
+    public String execute() {
+        return SUCCESS  ;
+    }
+
+    public String index() {
+        return SUCCESS  ;
+    }
+
+    public String view() {
+        return "user-id";
+    }
+
+    public String create() {
+        return SUCCESS;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
