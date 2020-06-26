bb3e0b9c68313878a4d186db22b78a8ad02f628d
pimbrouwers <pimnation@gmail.com>
2020-06-26 14:03:48 -0400

    falco upgrade to 1.2.0

--- fsharp/falco/Program.fs
+++ fsharp/falco/Program.fs
@@ -1,13 +1,42 @@
 open Falco
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+
+let defaultHandler = 
+    (textOut "")
 
 let userIdHandler =
     fun next (ctx: HttpContext) ->
         let userId = ctx.TryGetRouteValue "id" |> Option.defaultValue ""
         textOut userId next ctx
 
-webApp {
-    get "/" (textOut "")
-    get "/user/{id}" userIdHandler
-    post "/user" (textOut "")
-}
+let routes = 
+    [
+        get  "/"          defaultHandler
+        get  "/user/{id}" userIdHandler
+        post "/user"      defaultHandler
+    ]
+
+let configureServices (services : IServiceCollection) =
+    services.AddRouting() 
+    |> ignore
+
+let configureApp (app : IApplicationBuilder) = 
+    app.UseRouting()
+       .UseHttpEndPoints(routes)       
+       |> ignore 
+
+[<EntryPoint>]
+let main _ =
+    try
+        WebHostBuilder()
+            .UseKestrel()
+            .ConfigureServices(configureServices)
+            .Configure(configureApp)
+            .Build()
+            .Run()
+        0
+    with 
+        | _ -> -1
