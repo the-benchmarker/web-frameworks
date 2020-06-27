91d815dcd6dad44c500258bd2a37cec78f303776
Marwan Rabbâa <waghanza@gmail.com>
2020-06-27 22:28:35 +0200

    [F#] Update falco to 1.2 (#3035)

--- fsharp/falco/Program.fs
+++ fsharp/falco/Program.fs
@@ -1,13 +1,36 @@
 open Falco
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+
+let noContentHandler =
+    fun next (ctx: HttpContext) ->
+        setStatusCode 200 next ctx
 
 let userIdHandler =
     fun next (ctx: HttpContext) ->
         let userId = ctx.TryGetRouteValue "id" |> Option.defaultValue ""
         textOut userId next ctx
 
-webApp {
-    get "/" (textOut "")
-    get "/user/{id}" userIdHandler
-    post "/user" (textOut "")
-}
+let routes =
+    [
+        get "/" noContentHandler
+        get "/user/{id}" userIdHandler
+        post "/user" noContentHandler
+    ]
+
+// Enable services (routing required for Falco)
+let configureServices (services : IServiceCollection) =
+    services.AddRouting() |> ignore
+
+// Activate middleware
+let configureApp (app : IApplicationBuilder) = 
+    app.UseRouting().UseHttpEndPoints(routes)
+        .UseNotFoundHandler(setStatusCode 404 >=> textOut "Not found") |> ignore
+
+[<EntryPoint>]
+let main _ =
+    WebHostBuilder().UseKestrel().ConfigureServices(configureServices)
+        .Configure(configureApp).Build().Run()
+    0
