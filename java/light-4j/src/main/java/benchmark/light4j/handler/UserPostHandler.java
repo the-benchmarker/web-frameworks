b54193c9c53b8bbb0a52b29a39a0bc411d384581
tsuilouis <louis.tsui@live.ca>
2020-07-02 11:15:57 -0400

    Update handlers to return required response bodies

--- java/light-4j/src/main/java/benchmark/light4j/handler/UserPostHandler.java
+++ java/light-4j/src/main/java/benchmark/light4j/handler/UserPostHandler.java
@@ -2,16 +2,11 @@ package benchmark.light4j.handler;
 
 import com.networknt.handler.LightHttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.util.HttpString;
-import java.util.HashMap;
-import java.util.Map;
 
 public class UserPostHandler implements LightHttpHandler {
     
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
-        exchange.setStatusCode(200);
-        exchange.getResponseSender().send("{}");
+        exchange.endExchange();
     }
 }
