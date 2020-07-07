2cea0e0a8d08cb53337f332d60338b157473e252
pimbrouwers <pimnation@gmail.com>
2020-07-07 14:41:15 -0400

    falco v2.0.0-alpha

--- fsharp/falco/Program.fs
+++ fsharp/falco/Program.fs
@@ -1,57 +1,44 @@
-open Falco
+module Program 
 
+open Falco
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.Server.Kestrel.Core
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 
-module Server =
-    module Config =         
-        let configureLogging (log : ILoggingBuilder) =
-            log.ClearProviders()
-            |> ignore
+let configureWebHost 
+    (routes : HttpEndpoint list)
+    (webHost : IWebHostBuilder) =
+    let configureLogging (log : ILoggingBuilder) =
+        log.ClearProviders()
+        |> ignore
 
-        let configureServices (services : IServiceCollection) =
-            services.AddRouting() 
-            |> ignore
+    let configureServices (services : IServiceCollection) =
+        services.AddRouting() 
+        |> ignore
 
-        let configure (routes : HttpEndpoint list) (app : IApplicationBuilder) = 
-            app.UseRouting()
-               .UseHttpEndPoints(routes)       
-               |> ignore 
+    let configure (routes : HttpEndpoint list) (app : IApplicationBuilder) = 
+        app.UseRouting()
+            .UseHttpEndPoints(routes)       
+            |> ignore 
 
-    module Handlers = 
-        let defaultMsg = ""
-
-        let handleDefault : HttpHandler =  
-            textOut defaultMsg
-
-        let handleUserId : HttpHandler =
-            fun next ctx -> 
-                let userId = ctx.TryGetRouteValue "id" |> Option.defaultValue ""
-                textOut userId next ctx
-
-    let buildServer (webHostBuilder : IWebHostBuilder) =        
-        let routes = 
-            [
-                get  "/user/{id}" Handlers.handleUserId
-                post "/user"      Handlers.handleDefault
-                get  "/"          Handlers.handleDefault
-            ]
-
-        webHostBuilder     
-            .UseKestrel()
-            .ConfigureLogging(Config.configureLogging)
-            .ConfigureServices(Config.configureServices)
-            .Configure(Config.configure routes)                   
-            |> ignore
+    webHost
+        .UseKestrel()
+        .ConfigureLogging(configureLogging)
+        .ConfigureServices(configureServices)
+        .Configure(configure routes)
+        |> ignore
 
 [<EntryPoint>]
-let main args =    
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHost(fun webHost -> Server.buildServer webHost)
-        .Build()
-        .Run()
+let main args =   
+    let message = ""
+
+    Host.startWebHost 
+        args 
+        configureWebHost
+        [
+            get  "/user/{id}" (fun ctx -> Response.ofPlainText (Request.tryGetRouteValue "id" ctx |> Option.defaultValue "") ctx)
+            post "/user"      (Response.ofPlainText message)
+            get  "/"          (Response.ofPlainText message)
+        ]
     0
