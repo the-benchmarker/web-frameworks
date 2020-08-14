31ac8d26a01bff97feecd196bd2828c0eb843e20
Aber <AberSheeran@qq.com>
2020-08-14 13:40:10 +0800

    [Python] Add index.py

---
+++ python/index.py/server.py
@@ -0,0 +1,18 @@
+from indexpy import Index
+
+app = Index(try_html=False)
+
+
+@app.router.http("/", method="get")
+async def homepage(request):
+    return ""
+
+
+@app.router.http("/user/{user_id}")
+async def user(request):
+    return request.path_params["user_id"]
+
+
+@app.router.http("/user", method="post")
+async def userinfo(request):
+    return ""
