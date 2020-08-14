3ea0c6466ae77213e5ca2fb97d16530927425d11
Aber <AberSheeran@qq.com>
2020-08-14 15:21:30 +0800

    fix index.py/server.py

--- python/index.py/server.py
+++ python/index.py/server.py
@@ -8,7 +8,7 @@ async def homepage(request):
     return ""
 
 
-@app.router.http("/user/{user_id}")
+@app.router.http("/user/{user_id}", method="get")
 async def user(request):
     return request.path_params["user_id"]
 
