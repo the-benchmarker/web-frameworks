5823a3ab25f68dbdcdd30d3bf3f1ef16c3a806fb
peterliu <2202055828@qq.com>
2020-08-28 16:14:27 +0800

    change process count

--- php/mark/start.php
+++ php/mark/start.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 
 $api = new SimpleApp('tcp://0.0.0.0:3000');
 
-$api->count = 8;
+$api->count = shell_exec('nproc') ? shell_exec('nproc') - 4 : 12;
 
 $api->get('/', function ($requst) {
     return '';
