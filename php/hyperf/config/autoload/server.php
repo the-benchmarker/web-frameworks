d3884e8aa2da7a995d7c31054b9137d96b01180e
李铭昕 <715557344@qq.com>
2020-08-28 18:54:45 +0800

    Deleted useless code.

--- php/hyperf/config/autoload/server.php
+++ php/hyperf/config/autoload/server.php
@@ -27,12 +27,10 @@ return [
         ],
     ],
     'settings' => [
-        'coroutine' => false,
         'worker_num' => swoole_cpu_num() * 2,
         'enable_coroutine' => false,
         'pid_file' => BASE_PATH . '/runtime/hyperf.pid',
         'open_tcp_nodelay' => true,
-        'max_coroutine' => 100000,
         'open_http2_protocol' => true,
         'max_request' => 0,
         'socket_buffer_size' => 2 * 1024 * 1024,
