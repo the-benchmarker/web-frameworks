627e1bb5dc5d88158fc2174a6a22aabd095e737f
Barret Schloerke <schloerke@gmail.com>
2020-09-22 16:22:23 -0400

    Add plumber

---
+++ r/plumber/plumber.R
@@ -0,0 +1,29 @@
+# While plumber could be made faster, this represents the common setup
+# See https://github.com/the-benchmarker/web-frameworks/issues/3303#issuecomment-694351654
+
+
+# Rules
+# * GET /, SHOULD return a successful status code and an empty string
+# * GET /user/:id, SHOULD return a successful status code and the id
+# * POST /user, SHOULD return a successful status code and an empty string
+
+
+#* @serializer contentType list(type = "text/plain")
+#* @get /
+function() {
+  ""
+}
+
+
+#* @serializer contentType list(type = "text/plain")
+#* @get /user/<id>
+function(id) {
+  id
+}
+
+
+#* @serializer contentType list(type = "text/plain")
+#* @post /user
+function() {
+  ""
+}
