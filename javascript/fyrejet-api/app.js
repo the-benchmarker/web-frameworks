663fc6e00ecfa720d831a118cbb4b1f6fc9852ea
Nicholas Schamberg <schamberg.nicholas@gmail.com>
2020-09-24 17:07:48 +0300

    [Javascript] Add Fyrejet API mode & Fyrejet on uWebSockets.js

---
+++ javascript/fyrejet-api/app.js
@@ -0,0 +1,18 @@
+var fyrejet = require("fyrejet");
+var app = fyrejet();
+
+app.set('fyrejet mode', 'api')
+
+app.get("/", function (req, res) {
+  res.send("");
+});
+
+app.get("/user/:id", function (req, res) {
+  res.send(req.params.id);
+});
+
+app.post("/user", function (req, res) {
+  res.send("");
+});
+
+app.listen(3000, function () {});
