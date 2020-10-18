93c76ea311ddf36c3fe563592ccd9157d3239f4c
Marwan Rabbâa <waghanza@gmail.com>
2020-10-18 15:46:50 +0200

    switch to semaphoreci

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,21 @@
-require "spec"
-require "http/client"
+# frozen_string_literal: true
+
+require "net/http"
+require "yaml"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+def ip(name)
+  language, framework = name.split("/")
+  File.read(File.join(language, framework, "ip.txt")).strip
+end
