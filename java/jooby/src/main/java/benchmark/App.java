a6821291f23f5bf0ca705f111eeb96feaced3367
Marwan Rabbâa <waghanza@gmail.com>
2020-11-01 21:48:11 +0100

    fix java builds

--- java/jooby/src/main/java/benchmark/App.java
+++ java/jooby/src/main/java/benchmark/App.java
@@ -3,16 +3,16 @@ package benchmark;
 import static io.jooby.ExecutionMode.EVENT_LOOP;
 
 import io.jooby.Jooby;
-import io.jooby.ServerOptions;
+import io.jooby.benchmarkOptions;
 import io.jooby.StatusCode;
 
 public class App extends Jooby {
   {
-    ServerOptions options = new ServerOptions();
+    benchmarkOptions options = new benchmarkOptions();
     options.setPort(3000);
-    // Turn off Date and Server Response headers.
+    // Turn off Date and benchmark Response headers.
     options.setDefaultHeaders(false);
-    setServerOptions(options);
+    setbenchmarkOptions(options);
 
     get("/", ctx -> ctx.send(StatusCode.OK));
     get("/user/{id}", ctx -> ctx.send(ctx.path("id").value()));
--- java/spring-framework/src/main/java/benchmark/springframework/BenchmarkController.java
+++ java/spring-framework/src/main/java/benchmark/springframework/BenchmarkController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class BenchmarkController {
+public class benchmarkController {
   @GetMapping("/")
   public void root() {}
 
