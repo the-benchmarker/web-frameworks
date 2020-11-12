0f28dfd6f76b00d9f97ba370729fb0bcd6465e14
Ryan Riley <ryanriley@live.com>
2020-11-12 12:21:38 -0600

    Add final route

--- clojure/yada/src/server.clj
+++ clojure/yada/src/server.clj
@@ -1,5 +1,7 @@
 (ns server
-  (:require [yada.yada :refer [listener resource as-resource]]))
+  (:require
+   [clojure.tools.logging :refer :all]
+   [yada.yada :refer [listener resource as-resource]]))
 
 (def app
   ["/" (resource
@@ -7,12 +9,20 @@
          {:get
           {:produces "text/plain"
            :response ""}}})
-   [
-    ["user" (resource
-              {:methods
-               {:post
-                {:produces "text/plain"
-                 :response ""}}})]
+   [["user" (resource
+             {:methods
+              {:post
+               {:produces "text/plain"
+                :response ""}}})
+     [[["/" :name] (resource
+                    {:parameters {:path {:name String}}
+                     :produces "text/plain"
+                     :methods
+                     {:get
+                      {:response
+                       (fn [ctx]
+                         (let [name (get-in ctx [:parameters :path :name])]
+                           name))}}})]]]
     [true (as-resource nil)]]])
 
 (defn -main [& [port]]
