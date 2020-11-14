af882703a7d9240709c3836550f4fac44fe5096f
David Denton <denton.david@gmail.com>
2020-11-14 13:01:05 +0000

    add http4k benchmark

---
+++ kotlin/http4k/src/main/kotlin/org/http4k/benchmark/BenchmarkServer.kt
@@ -0,0 +1,28 @@
+package org.http4k.benchmark
+
+import org.http4k.core.ContentType.Companion.TEXT_PLAIN
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.core.then
+import org.http4k.filter.ServerFilters.SetContentType
+import org.http4k.routing.bind
+import org.http4k.routing.path
+import org.http4k.routing.routes
+import org.http4k.server.Apache4Server
+import org.http4k.server.asServer
+
+fun BenchmarkApp() = SetContentType(TEXT_PLAIN)
+    .then(
+        routes(
+            "/user/{id}" bind GET to { req: Request -> Response(OK).body(req.path("id")!!) },
+            "/user" bind POST to { Response(OK) },
+            "/" bind GET to { Response(OK) }
+        )
+    )
+
+fun main() {
+    BenchmarkApp().asServer(Apache4Server(3000)).start()
+}
