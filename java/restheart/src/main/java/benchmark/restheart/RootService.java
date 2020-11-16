c4284b221b6b2344da707f79848f8241c27b5214
Andrea Di Cesare <andrea@softinstigate.com>
2020-11-16 14:12:05 +0100

    [java] Update RESTHeart to match benchmark conventions

--- java/restheart/src/main/java/benchmark/restheart/RootService.java
+++ java/restheart/src/main/java/benchmark/restheart/RootService.java
@@ -7,12 +7,13 @@ import org.restheart.plugins.RegisterPlugin;
 import org.restheart.plugins.RegisterPlugin.MATCH_POLICY;
 import org.restheart.utils.HttpStatus;
 
-@RegisterPlugin(name = "benchmarkRootService", description = "service for root resource", defaultURI = "/", uriMatchPolicy = MATCH_POLICY.EXACT)
+@RegisterPlugin(name = "benchmarkRootService", description = "service for root resource",
+    defaultURI = "/", uriMatchPolicy = MATCH_POLICY.EXACT)
 public class RootService implements ByteArrayService {
-    @Override
-    public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
-        if (!request.isGet()) {
-            response.setStatusCode(HttpStatus.SC_NOT_IMPLEMENTED);
-        }
+  @Override
+  public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
+    if (!request.isGet()) {
+      response.setStatusCode(HttpStatus.SC_NOT_IMPLEMENTED);
     }
+  }
 }
--- java/restheart/src/main/java/benchmark/restheart/UserService.java
+++ java/restheart/src/main/java/benchmark/restheart/UserService.java
@@ -9,19 +9,19 @@ import org.restheart.exchange.ByteArrayRequest;
 import org.restheart.exchange.ByteArrayResponse;
 import org.restheart.plugins.ByteArrayService;
 import org.restheart.plugins.RegisterPlugin;
-
 import org.restheart.utils.HttpStatus;
 
-@RegisterPlugin(name = "benchmarkUserService", description = "service for user resource", defaultURI = "/user")
+@RegisterPlugin(
+    name = "benchmarkUserService", description = "service for user resource", defaultURI = "/user")
 public class UserService implements ByteArrayService {
-    @Override
-    public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
-        if (request.isGet()) {
-            response.setContent(request.getPathParam("/user/{id}", "id"));
-        } else if (request.isPost()) {
-            // nothing to do
-        } else {
-            response.setStatusCode(HttpStatus.SC_NOT_IMPLEMENTED);
-        }
+  @Override
+  public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
+    if (request.isGet()) {
+      response.setContent(request.getPathParam("/user/{id}", "id"));
+    } else if (request.isPost()) {
+      // nothing to do
+    } else {
+      response.setStatusCode(HttpStatus.SC_NOT_IMPLEMENTED);
     }
+  }
 }
