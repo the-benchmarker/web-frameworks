3e7c7e5268602d37efb15ff1f8c5f6bb0de70f0d
Marwan Rabbâa <waghanza@gmail.com>
2021-01-03 19:02:08 +0100

    update using ubiquity devtools

--- php/ubiquity/app/config/config.php
+++ php/ubiquity/app/config/config.php
@@ -1,9 +1,14 @@
 <?php
 return array(
-        "siteUrl"=>"http://127.0.0.1/ubiquity/",
-        "sessionName"=>null,
-        "test"=>false,
-        "debug"=>false,
-        "cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
-        "mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""]
+		"siteUrl"=>"http://0.0.0.0:3000",
+		"sessionName"=>"ubiquity",
+		"namespaces"=>[],
+		"test"=>false,
+		"debug"=>false,
+		"logger"=>function(){return new \Ubiquity\log\libraries\UMonolog("ubiquity",\Monolog\Logger::INFO);},
+		"cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
+		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""],
+		"isRest"=>function(){
+			return \Ubiquity\utils\http\URequest::getUrlParts()[0]==="rest";
+		}
 );
