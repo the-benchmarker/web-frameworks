305d49066c5dd90bc17f875368882623f3e06a5c
Marwan Rabbâa <waghanza@gmail.com>
2021-01-16 13:35:11 +0100

    use single thread mode for yii

--- php/yii-swoole/config/server.php
+++ php/yii-swoole/config/server.php
@@ -2,7 +2,7 @@
 return [
     'host' => '0.0.0.0',
     'port' => 3000,
-    'mode' => SWOOLE_PROCESS,
+    'mode' => SWOOLE_BASE,
     'sockType' => SWOOLE_SOCK_TCP,
     'app' => require __DIR__ . '/swoole.php',
     'options' => [
