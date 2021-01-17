ca334ac54c80a08b17db143af4e0b08a17ea1bd5
Koldo Picaza <koldobika@gotrendier.com>
2021-01-17 14:36:46 +0100

    update framework to the latest version

--- php/antidot/public/index.php
+++ php/antidot/public/index.php
@@ -4,61 +4,28 @@
 declare(strict_types=1);
 
 use Antidot\Application\Http\Application;
-use Antidot\React\PromiseResponse;
-use Laminas\Diactoros\Response\HtmlResponse;
-use Psr\Http\Message\ServerRequestInterface;
-use Ramsey\Uuid\Uuid;
-use React\EventLoop\Factory;
-use React\Http\Middleware\LimitConcurrentRequestsMiddleware;
-use React\Http\Middleware\RequestBodyBufferMiddleware;
-use React\Http\Middleware\RequestBodyParserMiddleware;
-use React\Http\Middleware\StreamingRequestMiddleware;
+use Psr\Log\LoggerInterface;
+use React\EventLoop\LoopInterface;
 use React\Http\Server;
 use React\Socket\Server as Socket;
-use function React\Promise\resolve;
 
 require 'vendor/autoload.php';
 
 call_user_func(static function () {
-    $loop = Factory::create();
     $container = require 'config/container.php';
     $application = $container->get(Application::class);
     (require 'router/middleware.php')($application, $container);
     (require 'router/routes.php')($application, $container);
 
-    $server = new Server(
-        $loop,
-        new StreamingRequestMiddleware(),
-        new LimitConcurrentRequestsMiddleware(100), // 100 concurrent buffering handlers
-        new RequestBodyBufferMiddleware(4 * 1024 * 1024), // 4 MiB
-        new RequestBodyParserMiddleware(),
-        static function (ServerRequestInterface $request) use ($application) {
-            try {
-                $response = new PromiseResponse(
-                    resolve($request)
-                        ->then(static fn ($request) => $request->withAttribute('request_id', Uuid::uuid4()->toString()))
-                        ->then(static fn ($request) => $application->handle($request))
-                );
-            } catch (Throwable $exception) {
-                if (!empty($e = $exception->getPrevious())) {
-                    $exception = $e;
-                }
+    $loop = $container->get(LoopInterface::class);
 
-                $response = new HtmlResponse(
-                    sprintf(
-                        '%s in file %s in line %s.',
-                        $exception->getMessage(),
-                        $exception->getFile(),
-                        $exception->getLine()
-                    )
-                );
-            }
+    $server = $container->get(Server::class);
+    $server->on('error', static function ($err) use ($container) {
+        $logger = $container->get(LoggerInterface::class);
+        $logger->critical($err);
+    });
 
-            return resolve($response);
-        }
-    );
-
-    $socket = new Socket('0.0.0.0:3000', $loop);
+    $socket = $container->get(Socket::class);
     $server->listen($socket);
 
     $loop->run();
