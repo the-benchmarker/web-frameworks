a99376ebf6b89453a7da6c35e0440c90f40d054c
Marc Morera <yuhu@mmoreram.com>
2021-01-19 13:50:27 +0100

    [PHP] Added DriftPHP (#3800)

---
+++ php/driftphp/Drift/config/bundles.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the DriftPHP package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ */
+
+declare(strict_types=1);
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+];
