8f647e981fc034508d65445ad280a73ebfe6480a
Barry vd. Heuvel <barryvdh@gmail.com>
2021-01-19 06:42:51 +0100

    [PHP] Cleanup (#3799)

--- php/laravel/app/Http/Controllers/Controller.php
+++ php/laravel/app/Http/Controllers/Controller.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
