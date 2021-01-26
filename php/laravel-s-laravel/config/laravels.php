ebe464fee58cdc06244d0c454e28a850701b9843
谢彪 <xiebiao@kucoin.com>
2021-01-26 10:14:54 +0800

    unify swoole config

--- php/laravel-s-laravel/config/laravels.php
+++ php/laravel-s-laravel/config/laravels.php
@@ -300,6 +300,6 @@ return [
         'log_level'          => SWOOLE_LOG_ERROR,
         'log_file'           => '/dev/null',
         'enable_coroutine'   => false,
-        'buffer_output_size' => 2 * 1024 * 1024, // required by laravel-s
+        'buffer_output_size' => 2 * 1024 * 1024, // required by laravel-s, it's default setting 2M.
     ],
 ];
