3c9282a4192a0ae55910016417d1318c61eca76d
whiplash <whiplash@users.noreply.github.com>
2021-01-26 22:20:59 +0100

    more finatra config (as suggested by @cacoco)

--- scala/finatra/src/main/scala/WebServer.scala
+++ scala/finatra/src/main/scala/WebServer.scala
@@ -1,5 +1,8 @@
 import com.twitter.finatra._
-
+import com.twitter.finagle.Http.Server
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.stack.nilStack
+import com.twitter.finagle.stats.NullStatsReceiver
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.routing.HttpRouter
 
@@ -8,6 +11,13 @@ object HelloWorldServerMain extends WebServer
 class WebServer extends HttpServer {
 
   override val defaultHttpPort: String = ":3000"
+
+  override def configureHttpServer(server: Server): Server = {
+    server
+      .withCompressionLevel(0)
+      .withStatsReceiver(NullStatsReceiver)
+      .withStack(nilStack[Request, Response])
+  }  
 
   override def configureHttp(router: HttpRouter) = {
     router.
