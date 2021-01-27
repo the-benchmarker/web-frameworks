d3f93af5c3e99d4a4c4907846705c4ed6655aadd
sy-records <52o@qq52o.cn>
2021-01-27 17:24:11 +0800

    [PHP] Update Swoole

--- php/swoole-coroutine/start.php
+++ php/swoole-coroutine/start.php
@@ -5,6 +5,10 @@ use Swoole\Coroutine\Http\Server;
 
 $worker_num = swoole_cpu_num() * 2;
 
+/**
+ * The Coroutine\Http\Server does not automatically create multiple processes,
+ * needs to be used with the Process\Pool module to take advantage of multiple cores.
+ */
 $pool = new Pool($worker_num);
 $pool->set(['enable_coroutine' => true]);
 $pool->on('workerStart', function ($pool, $id) {
