dd157fc99aacf0ac4d86db9e777ce55cc4a933a9
Rajkumar Dusad <rajkumardusad09@gmail.com>
2021-02-06 14:00:37 +0530

    add unic framework

--- php/unic/application/app/view.php
+++ php/unic/application/app/view.php
@@ -15,19 +15,4 @@ class view extends Views {
   function create(Request $req) {
     return $this->response($req->params->id);
   }
-
-  function page_not_found(Request $req) {
-    $this->response_code(404);
-    return $this->render('errors/404');
-  }
-
-  function forbidden(Request $req) {
-    $this->response_code(403);
-    return $this->render('errors/403');
-  }
-
-  function internal_server_error(Request $req) {
-    $this->response_code(500);
-    return $this->render('errors/500');
-  }
 }
--- php/unic/application/urls.php
+++ php/unic/application/urls.php
@@ -3,10 +3,3 @@
 $urlpatterns = [
   '/' => urls('app/urls.php'),
 ];
-
-//Error handler
-$errorhandlers = [
-  '404' => 'view.page_not_found',
-  '403' => 'view.forbidden',
-  '500' => 'view.internal_server_error',
-];
