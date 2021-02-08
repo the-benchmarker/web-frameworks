fdaa72ba93043e0882e06ffe0a5e9de37961e38f
Anatoly Nekhay <afenric@gmail.com>
2021-02-08 16:02:43 +0500

    [PHP] Sunrise Router / improved code (#3928)

--- php/sunrise-router-roadrunner/worker.php
+++ php/sunrise-router-roadrunner/worker.php
@@ -4,57 +4,52 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Spiral\Goridge\StreamRelay as RoadRunnerRelay;
+use Spiral\RoadRunner\PSR7Client as RoadRunnerClient;
+use Spiral\RoadRunner\Worker as RoadRunnerWorker;
+use Sunrise\Http\Factory\ResponseFactory;
+use Sunrise\Http\Factory\ServerRequestFactory;
+use Sunrise\Http\Factory\StreamFactory;
+use Sunrise\Http\Factory\UploadedFileFactory;
 use Sunrise\Http\Router\Exception\MethodNotAllowedException;
 use Sunrise\Http\Router\Exception\RouteNotFoundException;
 use Sunrise\Http\Router\RequestHandler\CallableRequestHandler;
 use Sunrise\Http\Router\RouteCollector;
 use Sunrise\Http\Router\Router;
-use Sunrise\Http\Factory\ResponseFactory;
-use Sunrise\Http\Factory\ServerRequestFactory;
-use Sunrise\Http\Factory\StreamFactory;
-use Sunrise\Http\Factory\UploadedFileFactory;
-
-use Spiral\Goridge\StreamRelay as RoadRunnerRelay;
-use Spiral\RoadRunner\PSR7Client as RoadRunnerClient;
-use Spiral\RoadRunner\Worker as RoadRunnerWorker;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$router = new Router();
 $routeCollector = new RouteCollector();
-$responsePrototype = (new ResponseFactory)
-    ->createResponse(200);
+$responseFactory = new ResponseFactory();
 
-$routeCollector->get('home', '/', new CallableRequestHandler(
-    function (ServerRequestInterface $request) use ($responsePrototype) : ResponseInterface {
-        return $responsePrototype;
+$routeCollector->route('home', '/', ['GET'], new CallableRequestHandler(
+    function (ServerRequestInterface $request) use ($responseFactory) : ResponseInterface {
+        return $responseFactory->createResponse(200);
     }
 ));
 
-$routeCollector->get('userRead', '/user/{id}', new CallableRequestHandler(
-    function (ServerRequestInterface $request) use ($responsePrototype) : ResponseInterface {
-        $responsePrototype->getBody()->write(
-            $request->getAttribute('id')
-        );
-
-        return $responsePrototype;
+$routeCollector->route('userCreate', '/user', ['POST'], new CallableRequestHandler(
+    function (ServerRequestInterface $request) use ($responseFactory) : ResponseInterface {
+        return $responseFactory->createResponse(201);
     }
 ));
 
-$routeCollector->post('userCreate', '/user', new CallableRequestHandler(
-    function (ServerRequestInterface $request) use ($responsePrototype) : ResponseInterface {
-        return $responsePrototype->withStatus(201);
+$routeCollector->route('userRead', '/user/{id}', ['GET'], new CallableRequestHandler(
+    function (ServerRequestInterface $request) use ($responseFactory) : ResponseInterface {
+        $response = $responseFactory->createResponse(200);
+        $response->getBody()->write($request->getAttribute('id'));
+
+        return $response;
     }
 ));
 
-$router->addRoute(
-    ...$routeCollector->getCollection()->all()
-);
-
-$relay = new RoadRunnerRelay(STDIN, STDOUT);
+$router = new Router();
+$router->addRoute(...$routeCollector->getCollection()->all());
 
 $client = new RoadRunnerClient(
-    new RoadRunnerWorker($relay),
+    new RoadRunnerWorker(
+        new RoadRunnerRelay(STDIN, STDOUT)
+    ),
     new ServerRequestFactory(),
     new StreamFactory(),
     new UploadedFileFactory()
@@ -62,20 +57,12 @@ $client = new RoadRunnerClient(
 
 while ($request = $client->acceptRequest()) {
     try {
-        $client->respond(
-            $router->handle($request)
-        );
+        $client->respond($router->handle($request));
     } catch (MethodNotAllowedException $e) {
-        $client->respond(
-            $responsePrototype->withStatus(405)
-        );
+        $client->respond($responseFactory->createResponse(405));
     } catch (RouteNotFoundException $e) {
-        $client->respond(
-            $responsePrototype->withStatus(404)
-        );
+        $client->respond($responseFactory->createResponse(404));
     } catch (Throwable $e) {
-        $client->respond(
-            $responsePrototype->withStatus(500)
-        );
+        $client->respond($responseFactory->createResponse(500));
     }
 }
--- php/sunrise-router/public/index.php
+++ php/sunrise-router/public/index.php
@@ -4,42 +4,53 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Sunrise\Http\Message\ResponseFactory;
+use Sunrise\Http\Factory\ResponseFactory;
+use Sunrise\Http\Factory\ServerRequestFactory;
 use Sunrise\Http\Router\Exception\MethodNotAllowedException;
 use Sunrise\Http\Router\Exception\RouteNotFoundException;
 use Sunrise\Http\Router\RequestHandler\CallableRequestHandler;
 use Sunrise\Http\Router\RouteCollector;
 use Sunrise\Http\Router\Router;
-use Sunrise\Http\ServerRequest\ServerRequestFactory;
 
 use function Sunrise\Http\Router\emit;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$router = new Router();
 $routeCollector = new RouteCollector();
+$responseFactory = new ResponseFactory();
 
-$routeCollector->get('home', '/', new CallableRequestHandler(function (ServerRequestInterface $request) : ResponseInterface {
-    return (new ResponseFactory)->createResponse(200);
-}));
+$routeCollector->route('home', '/', ['GET'], new CallableRequestHandler(
+    function (ServerRequestInterface $request) use ($responseFactory) : ResponseInterface {
+        return $responseFactory->createResponse(200);
+    }
+));
 
-$routeCollector->get('userRead', '/user/{id}', new CallableRequestHandler(function (ServerRequestInterface $request) : ResponseInterface {
-    $response = (new ResponseFactory)->createResponse(200);
-    $response->getBody()->write($request->getAttribute('id'));
+$routeCollector->route('userCreate', '/user', ['POST'], new CallableRequestHandler(
+    function (ServerRequestInterface $request) use ($responseFactory) : ResponseInterface {
+        return $responseFactory->createResponse(201);
+    }
+));
 
-    return $response;
-}));
+$routeCollector->route('userRead', '/user/{id}', ['GET'], new CallableRequestHandler(
+    function (ServerRequestInterface $request) use ($responseFactory) : ResponseInterface {
+        $response = $responseFactory->createResponse(200);
+        $response->getBody()->write($request->getAttribute('id'));
 
-$routeCollector->post('userCreate', '/user', new CallableRequestHandler(function (ServerRequestInterface $request) : ResponseInterface {
-    return (new ResponseFactory)->createResponse(201);
-}));
+        return $response;
+    }
+));
 
+$router = new Router();
 $router->addRoute(...$routeCollector->getCollection()->all());
 
+$request = ServerRequestFactory::fromGlobals();
+
 try {
-    emit($router->handle(ServerRequestFactory::fromGlobals()));
+    emit($router->handle($request));
 } catch (MethodNotAllowedException $e) {
-    emit((new ResponseFactory)->createResponse(405));
+    emit($responseFactory->createResponse(405));
 } catch (RouteNotFoundException $e) {
-    emit((new ResponseFactory)->createResponse(404));
+    emit($responseFactory->createResponse(404));
+} catch (Throwable $e) {
+	emit($responseFactory->createResponse(500));
 }
