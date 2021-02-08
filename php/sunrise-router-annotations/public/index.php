7c5d7b57695cc9a7b66a45141431e43fe3f4fb07
Anatoly Nekhay <afenric@gmail.com>
2021-02-08 14:48:45 +0500

    improved code

--- php/sunrise-router-annotations/public/index.php
+++ php/sunrise-router-annotations/public/index.php
@@ -2,34 +2,34 @@
 
 declare(strict_types=1);
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Sunrise\Http\Message\ResponseFactory;
+use Sunrise\Http\Factory\ResponseFactory;
+use Sunrise\Http\Factory\ServerRequestFactory;
 use Sunrise\Http\Router\Exception\MethodNotAllowedException;
 use Sunrise\Http\Router\Exception\RouteNotFoundException;
-use Sunrise\Http\Router\Loader\AnnotationDirectoryLoader;
+use Sunrise\Http\Router\Loader\DescriptorDirectoryLoader;
 use Sunrise\Http\Router\Router;
-use Sunrise\Http\ServerRequest\ServerRequestFactory;
+use Symfony\Component\Cache\Adapter\ApcuAdapter;
+use Symfony\Component\Cache\Psr16Cache as Cache;
 
 use function Sunrise\Http\Router\emit;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-AnnotationRegistry::registerLoader('class_exists');
-
-$loader = new AnnotationDirectoryLoader();
+$loader = new DescriptorDirectoryLoader();
+$loader->setCache(new Cache(new ApcuAdapter()));
 $loader->attach(__DIR__ . '/../src/Controller');
-
-// [!] Use cache in production...
-// @see https://www.php-fig.org/psr/psr-16/
-// $loader->setCache(...);
 
 $router = new Router();
 $router->load($loader);
 
+$request = ServerRequestFactory::fromGlobals();
+
 try {
-    emit($router->handle(ServerRequestFactory::fromGlobals()));
+    emit($router->handle($request));
 } catch (MethodNotAllowedException $e) {
     emit((new ResponseFactory)->createResponse(405));
 } catch (RouteNotFoundException $e) {
     emit((new ResponseFactory)->createResponse(404));
+} catch (Throwable $e) {
+	emit((new ResponseFactory)->createResponse(500));
 }
