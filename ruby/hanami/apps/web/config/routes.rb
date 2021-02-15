753458744edb05efd73cd5bd79acb8f8cc4ee84d
Marwan Rabbâa <waghanza@gmail.com>
2021-02-15 19:23:41 +0100

    remove hanami

--- ruby/hanami/apps/web/config/routes.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-get '/', to: 'home#index'
-get '/user/:id', to: 'home#user'
-post '/user', to: 'home#register_user'
--- ruby/hanami/config.ru
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require './config/environment'
-
-run Hanami.app
