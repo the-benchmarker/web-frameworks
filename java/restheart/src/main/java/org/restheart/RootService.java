c63642b783e4797e12fc4c9e9918694d340c8a5e
Andrea Di Cesare <andrea@softinstigate.com>
2021-02-16 14:33:31 +0100

    Cleanup code

--- java/restheart/src/main/java/org/restheart/RootService.java
+++ java/restheart/src/main/java/org/restheart/RootService.java
@@ -3,21 +3,20 @@ package org.restheart;
 import org.restheart.exchange.ByteArrayRequest;
 import org.restheart.exchange.ByteArrayResponse;
 import org.restheart.plugins.ByteArrayService;
-import org.restheart.plugins.InterceptPoint;
 import org.restheart.plugins.RegisterPlugin;
 import org.restheart.plugins.RegisterPlugin.MATCH_POLICY;
-import org.restheart.utils.HttpStatus;
 
-@RegisterPlugin(name = "benchmarkRootService", description = "service for root resource", defaultURI = "/", uriMatchPolicy = MATCH_POLICY.EXACT, dontIntercept = {
-    InterceptPoint.REQUEST_BEFORE_AUTH, InterceptPoint.RESPONSE, InterceptPoint.RESPONSE_ASYNC })
+@RegisterPlugin(name = "benchmarkRootService",
+    description = "service for root resource",
+    defaultURI = "/",
+    uriMatchPolicy = MATCH_POLICY.EXACT)
 public class RootService implements ByteArrayService {
   @Override
   public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
-    if (!request.isGet()) {
-      response.setStatusCode(HttpStatus.SC_NOT_IMPLEMENTED);
-    }
-
-    // clear cors headers for reduced response lenght
+    // RESTHeart handles several response headers including CORS headers and X-Powered-By
+    // remove them reducing the response content lenght to ~1/3 for a fairer competition :)
     response.getExchange().getResponseHeaders().clear();
+
+    // nothing to do! this just sends 200 back
   }
 }
--- java/restheart/src/main/java/org/restheart/UserService.java
+++ java/restheart/src/main/java/org/restheart/UserService.java
@@ -8,24 +8,25 @@ package org.restheart;
 import org.restheart.exchange.ByteArrayRequest;
 import org.restheart.exchange.ByteArrayResponse;
 import org.restheart.plugins.ByteArrayService;
-import org.restheart.plugins.InterceptPoint;
 import org.restheart.plugins.RegisterPlugin;
 import org.restheart.utils.HttpStatus;
 
-@RegisterPlugin(name = "benchmarkUserService", description = "service for user resource", defaultURI = "/user", dontIntercept = {
-    InterceptPoint.REQUEST_BEFORE_AUTH, InterceptPoint.RESPONSE, InterceptPoint.RESPONSE_ASYNC })
+@RegisterPlugin(name = "benchmarkUserService",
+    description = "service for user resource",
+    defaultURI = "/user")
 public class UserService implements ByteArrayService {
   @Override
   public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
-    if (request.isGet()) {
-      response.setContent(request.getPathParam("/user/{id}", "id"));
-    } else if (request.isPost()) {
-      // nothing to do
+    // RESTHeart handles several response headers including CORS headers and X-Powered-By
+    // remove them reducing the response content lenght to ~1/3 for a fairer competition :)
+    response.getExchange().getResponseHeaders().clear();
+
+    if (request.isPost()) {
+      // nothing to do! this just sends 200 back
+    } else if (request.isGet()) {
+        response.setContent(request.getPathParam("/user/{id}", "id"));
     } else {
       response.setStatusCode(HttpStatus.SC_NOT_IMPLEMENTED);
     }
-
-    // clear cors headers for reduced response lenght
-    response.getExchange().getResponseHeaders().clear();
   }
 }
