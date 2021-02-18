8f9cbd805d32195f0f4d5c21c2041295407e0516
Marwan Rabbâa <waghanza@gmail.com>
2021-02-18 21:17:08 +0100

    enable semaphore

--- .spec/spec_helper.rb
+++ .spec/spec_helper.rb
@@ -15,8 +15,7 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
 
-def address(name)
-  language, framework = name.split('/')
-  ip = File.read(File.join(language, framework, 'ip.txt')).strip
+def http
+  ip = File.read(File.join(ENV['LANGUAGE'], ENV['FRAMEWORK'],"ip-#{ENV['VARIANT']}.txt")).strip
   Net::HTTP.new(ip, 3000)
 end
