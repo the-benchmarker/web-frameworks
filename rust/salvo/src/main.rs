c531966ef8eefd69acd86db5287254c4bded3621
Chrislearn Young <chris@acroidea.com>
2021-03-11 04:10:44 +0800

    [Rust] Remove tokio from salvo dependencies (#4058)

--- rust/salvo/src/main.rs
+++ rust/salvo/src/main.rs
@@ -9,13 +9,12 @@ async fn get_user(req: &mut Request, res: &mut Response) {
     res.render_plain_text(req.params().get("id").map(|s| &**s).unwrap_or_default());
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let router = Router::new().get(index).push(
         Router::new()
             .path("user")
             .post(index)
             .push(Router::new().path("<id>").get(get_user)),
     );
-    Server::new(router).bind(([0, 0, 0, 0], 3000)).await;
+    Server::new(router).start(([0, 0, 0, 0], 3000));
 }
