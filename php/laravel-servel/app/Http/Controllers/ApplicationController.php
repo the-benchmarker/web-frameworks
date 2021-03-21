92a51b9d83354711ee51d2bd0c7c9294ae28015d
Gregori Piñeres <gregori.pineres02@gmail.com>
2021-03-21 04:31:56 -0400

    [PHP] Add swoole support for laravel (#4041)

---
+++ php/laravel-servel/app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ApplicationController extends Controller
+{
+    public function index()
+    {
+        return "";
+    }
+}
