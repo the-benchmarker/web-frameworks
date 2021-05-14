bcb8eb6eca3be39a6029781b239fdf9ae39c196a
Marwan Rabbâa <waghanza@gmail.com>
2021-05-14 17:23:20 +0200

    add workeman index file

---
+++ php/chubbyphp/.workerman/public/index.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Chubbyphp\Framework\Application;
+use Chubbyphp\Framework\Middleware\ExceptionMiddleware;
+use Chubbyphp\Framework\Middleware\RouteMatcherMiddleware;
+use Chubbyphp\Framework\RequestHandler\CallbackRequestHandler;
+use Chubbyphp\Framework\Router\FastRoute\RouteMatcher;
+use Chubbyphp\Framework\Router\Route;
+use Chubbyphp\Framework\Router\Routes;
+use Chubbyphp\WorkermanRequestHandler\OnMessage;
+use Chubbyphp\WorkermanRequestHandler\PsrRequestFactory;
+use Chubbyphp\WorkermanRequestHandler\WorkermanResponseEmitter;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Slim\Psr7\Factory\StreamFactory;
+use Slim\Psr7\Factory\UploadedFileFactory;
+use Workerman\Worker;
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+
+$responseFactory = new ResponseFactory();
+
+$app = new Application([
+    new ExceptionMiddleware($responseFactory, true),
+    new RouteMatcherMiddleware(new RouteMatcher(new Routes([
+        Route::get('/', 'home', new CallbackRequestHandler(
+            static function () use ($responseFactory) {
+                $response = $responseFactory->createResponse();
+                $response->getBody()->write('');
+
+                return $response;
+            }
+        )),
+        Route::get('/user/{id}', 'user_view', new CallbackRequestHandler(
+            static function (ServerRequestInterface $request) use ($responseFactory) {
+                $response = $responseFactory->createResponse();
+                $response->getBody()->write($request->getAttribute('id'));
+
+                return $response;
+            }
+        )),
+        Route::post('/user', 'user_list', new CallbackRequestHandler(
+            static function () use ($responseFactory) {
+                $response = $responseFactory->createResponse();
+                $response->getBody()->write('');
+
+                return $response;
+            }
+        ))
+    ])), $responseFactory),
+]);
+
+$server = new Worker('http://0.0.0.0:3000');
+
+$server->count = shell_exec('nproc') ? shell_exec('nproc') : 32;
+
+$server->onMessage = new OnMessage(
+    new PsrRequestFactory(
+        new ServerRequestFactory(),
+        new StreamFactory(),
+        new UploadedFileFactory()
+    ),
+    new WorkermanResponseEmitter(),
+    $app
+);
+
+Worker::runAll();
