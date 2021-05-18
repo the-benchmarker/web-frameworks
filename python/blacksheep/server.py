20f44f57935d1e6a8c6016b617301c585936f4b4
Roberto Prevato <roberto.prevato@gmail.com>
2021-05-18 07:36:12 +0200

    corrects blacksheep (#4304)
    * corrects blacksheep
    
    * uses a version of blacksheep with compatible httptools version

--- python/blacksheep/server.py
+++ python/blacksheep/server.py
@@ -4,25 +4,22 @@ import logging
 logging.disable()
 
 from blacksheep.server import Application
-from blacksheep.server.responses import text
+from blacksheep.server.responses import text, Response
 
 
 app = Application()
 
 
 @app.router.get("/")
-async def home(_):
-    return text(f"")
+async def home(_) -> Response:
+    return text("")
 
 
 @app.router.get("/user/:id")
-async def user_info(_, id: int):
+async def user_info(_, id: int) -> Response:
     return text(f"{id}")
 
 
 @app.router.post("/user")
-async def user(_):
-    return text(f"")
-
-
-app.start()
+async def user(_) -> Response:
+    return text("")
