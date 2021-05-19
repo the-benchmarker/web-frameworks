d9767a00ae461776ce6616a3f64385d935c2f7f2
ray-ang <46805160+ray-ang@users.noreply.github.com>
2021-05-19 17:43:56 -0400

    Use apiResponse( ) instead of echo

--- php/basicphp/index.php
+++ php/basicphp/index.php
@@ -9,13 +9,13 @@ require_once __DIR__ . '/Basic.php'; // BasicPHP class library
 */
 
 Basic::route('GET', '/', function() {
-    echo '';
+    Basic::apiResponse(200, '');
 });
 Basic::route('GET', '/user/(:num)', function() {
-    echo Basic::segment(2);
+    Basic::apiResponse(200, Basic::segment(2));
 });
 Basic::route('POST', '/user', function() {
-    echo '';
+    Basic::apiResponse(200, '');
 });
 
 Basic::apiResponse(404); // Error 404
