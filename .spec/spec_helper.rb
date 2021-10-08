b7cff11f5b0f4fa70b71581ec7cbbd25eb53ee88
Marwan Rabbâa <waghanza@gmail.com>
2021-10-08 07:17:15 +0200

    ensure ci pipeline is working

--- .spec/spec_helper.rb
+++ .spec/spec_helper.rb
@@ -18,6 +18,5 @@ end
 def address(name)
   language, framework = name.split('/')
   ip = File.read(File.join(language, framework, 'ip.txt')).strip
-  pp "Found #{ip}"
   Net::HTTP.new(ip, 3000)
 end
