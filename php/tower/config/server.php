fd489c6d1699443d69c82fdc7f1c02b737273c10
Amir <80312616+amirfaramarzi@users.noreply.github.com>
2021-11-04 11:09:00 +0330

    Update server.php

--- php/tower/config/server.php
+++ php/tower/config/server.php
@@ -4,7 +4,7 @@ return [
     'listen' => 'http://0.0.0.0:3000',
     'transport' => 'tcp',
     'context' => [],
-    'count' => cpuCoreCount() * 2,
+    'count' => 8,
     'user' => '',
     'group' => '',
     'max_request' => 1000000,
