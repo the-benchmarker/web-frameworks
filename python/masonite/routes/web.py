f44aedaeb5166200df7501728670e6f76a1f0a57
Joe Mancuso <idmann509@gmail.com>
2021-12-26 17:04:35 -0500

    removed masonite

--- python/masonite/routes/web.py
+++
@@ -1,7 +0,0 @@
-from masonite.routes import Route
-
-ROUTES = [
-    Route.get("/", "PageController@index"),
-    Route.get("/user/@id:int", "PageController@show_user"),
-    Route.post("/user", "PageController@create_user"),
-]
