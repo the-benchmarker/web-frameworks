b62bf56c72df4eb632fa31c1edc3f9c9d32cf7c0
Marwan Rabbâa <waghanza@gmail.com>
2021-12-27 08:49:50 +0100

    manual merge

--- python/masonite/app/controllers/UserController.py
+++ python/masonite/app/controllers/UserController.py
@@ -1,8 +1,12 @@
-from masonite.controllers import Controller
+"""A WelcomeController Module."""
 from masonite.views import View
+from masonite.request import Request
+from masonite.controllers import Controller
 
 
 class UserController(Controller):
+    """WelcomeController Controller Class."""
+
     def index(self, view: View):
         return view.render("")
 
