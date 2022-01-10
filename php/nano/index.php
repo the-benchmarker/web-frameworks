0c89b8c8a21201b2ac884de4723c17d7a7fb28ab
Layla Tichy <contact@laylatichy.com>
2022-01-10 19:20:18 +0000

    use HttpCode enum

--- php/nano/index.php
+++ php/nano/index.php
@@ -3,30 +3,31 @@
 require_once 'vendor/autoload.php';
 
 use laylatichy\Nano;
+use laylatichy\nano\core\HttpCode;
 
 $nano = new Nano();
 
 $nano->get(
     '/',
     function ($request) use ($nano) {
-        $nano->response->code(200);
-        $nano->response->plain('');
+        $nano->response->code(code: HttpCode::OK);
+        $nano->response->plain(body: '');
     }
 );
 
 $nano->get(
     '/user/{id}',
     function ($request, $id) use ($nano) {
-        $nano->response->code(200);
-        $nano->response->plain($id);
+        $nano->response->code(code: HttpCode::OK);
+        $nano->response->plain(body: $id);
     }
 );
 
 $nano->post(
     '/user',
     function ($request) use ($nano) {
-        $nano->response->code(200);
-        $nano->response->plain('');
+        $nano->response->code(code: HttpCode::OK);
+        $nano->response->plain(body: '');
     }
 );
 
