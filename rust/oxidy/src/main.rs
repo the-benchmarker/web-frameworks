1d2a04c80e76e5cf4595d758298c4bea8c79a25e
oxidy <oxidy.rs@gmail.com>
2022-02-05 22:01:05 +0530

    Changes as per Suggestion

--- rust/oxidy/src/main.rs
+++ rust/oxidy/src/main.rs
@@ -6,17 +6,17 @@ fn index(ctx: &mut Context) -> () {
 }
 
 fn user(ctx: &mut Context) -> () {
-    if ctx.request.header.get("method").unwrap().to_string() == "GET" {
-        ctx.response.body = ctx.request.params.get("id").unwrap().to_string();
-    } else {
-        ctx.response.body = "".to_string();
-    }
+    ctx.response.body = ctx.request.params.get("id").unwrap().to_string();
+}
+
+fn user_post(ctx: &mut Context) -> () {
+    ctx.response.body = "".to_string();
 }
 
 fn main() {
     let mut app = Server::new();
     app.get("/", index);
     app.get("/user/:id", user);
-    app.post("/user", user);
+    app.post("/user", user_post);
     app.listen("0.0.0.0:3000");
 }
