b1d666b284265374c33047c8c84edc6aff3bfcfd
Damith G <djnitehawk83@gmail.com>
2022-02-13 15:09:06 +0530

    [CSharp] rename `aspnetcore` to `aspnet-minimal-api` and cleanup project
    - enable full profile guided optimization in docker file
    - clean up aspnetcore project and rename it to aspnet-minimal-api

---
+++ csharp/aspnet-minimal-api/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = WebApplication.CreateBuilder();
+builder.WebHost.ConfigureKestrel(o => o.AddServerHeader = false);
+builder.Logging.ClearProviders();
+
+var app = builder.Build();
+
+app.MapGet("/", () => Task.CompletedTask);
+
+app.MapGet("user/{id}", (string id) => id);
+
+app.MapPost("user", () => Task.CompletedTask);
+
+app.Run();
