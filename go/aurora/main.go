e6d1a4e6101fc3ad134fa25f766ef17223793a27
Marwan Rabbâa <waghanza@gmail.com>
2022-03-04 13:15:44 +0100

    add aurora

---
+++ go/aurora/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/aurora-go/aurora/aurora"
+	"github.com/aurora-go/aurora/aurora/req"
+)
+
+func main() {
+	//获取 aurora 路由实例
+	a := aurora.New()
+
+	a.GET("/", func(request aurora.Request) interface{} {
+		return ""
+	})
+	a.GET("/user/{id}", func(request aurora.Request) interface{} {
+		return request[req.Args]
+	})
+	a.POST("/user", func(request aurora.Request) interface{} {
+		return ""
+	})
+
+	// 启动服务器 默认端口8080，更改端口号 a.Guide(”8081“) 即可
+	a.Guide("3000")
+}
