2fc219bdf5196e75a2850192ebc22fcbcf13772b
oxidy <oxidy.rs@gmail.com>
2022-05-16 12:45:55 +0530

    Edited as commented & Improved Routes

--- rust/warp/src/main.rs
+++ rust/warp/src/main.rs
@@ -3,14 +3,13 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let index = warp::path::end().map(|| "");
-    let user = warp::path!("user" / u32).map(|id: u32| id.to_string());
-    let user_post = warp::path!("user").map(|| "");
+    let index = warp::path::end().and(warp::get()).map(|| "");
 
-    let get_routes = warp::get().and(index.or(user));
-    let post_routes = warp::post().and(index.or(user_post));
+    let user = warp::path!("user" / u32).and(warp::get()).map(|id: u32| id.to_string());
 
-    let routes = get_routes.or(post_routes);
+    let user_post = warp::path("user").and(warp::post()).map(|| "");
+
+    let routes = index.or(user).or(user_post);
 
     warp::serve(routes).run(([0, 0, 0, 0], 3000)).await;
 }
