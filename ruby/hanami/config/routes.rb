5d62642cb306ca15be666240cd2bec804f7abd23
Sean Collins <sean@cllns.com>
2022-07-22 11:24:14 -0500

    Add Hanami 2.0.0.beta

---
+++ ruby/hanami/config/routes.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "hanami/routes"
+
+module Benchmark
+  class Routes < Hanami::Routes
+    define do
+      root { "" }
+
+      get "/user/:id" do
+        params[:id]
+      end
+
+      post "/user" do
+        ""
+      end
+    end
+  end
+end
