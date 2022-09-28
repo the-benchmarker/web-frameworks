6721028712552ab3faf7676e47af734b3142f1c5
Andrea Di Cesare <andrea@softinstigate.com>
2022-09-28 10:18:33 +0200

    :arrow_up: Upgrade to RESTHeart 7

--- java/restheart/src/main/java/org/restheart/RootService.java
+++ java/restheart/src/main/java/org/restheart/RootService.java
@@ -14,7 +14,7 @@ public class RootService implements ByteArrayService {
   @Override
   public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
     // RESTHeart handles several response headers including CORS headers and X-Powered-By
-    // remove them reducing the response content lenght to ~1/3 for a fairer competition :)
+    // remove them reducing the response content lenght to ~1/3 for a fair competition :)
     response.getExchange().getResponseHeaders().clear();
 
     // nothing to do! this just sends 200 back
