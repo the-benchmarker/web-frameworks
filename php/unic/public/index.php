cee6b3efc0bc99d0ca1ce834d1773d43bb18fa73
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2022-12-13 12:20:14 +0530

    Update index.php

--- php/unic/public/index.php
+++ php/unic/public/index.php
@@ -5,15 +5,15 @@ use Unic\App;
 
 $app = new App();
 
-$app->get('/', function($req, $res, $next) {
+$app->get('/', function($req, $res) {
   $res->send('');
 });
 
-$app->get('/user/{id}', function($req, $res, $next) {
+$app->get('/user/{id}', function($req, $res) {
   $res->send($req->params->id);
 });
 
-$app->post('/user', function($req, $res, $next) {
+$app->post('/user', function($req, $res) {
   $res->send('');
 });
 
