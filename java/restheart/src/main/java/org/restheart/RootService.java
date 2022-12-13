79cca676d6701f521211fed33b648108f4149b9d
Andrea Di Cesare <andrea@softinstigate.com>
2022-12-13 16:07:57 +0100

    RESTHeart: Use dontIntercept to avoid interceptors overhead (#5950)
    * Add RESTHeart
    
    * Update RESTHeart to v5.1.6
    
    * [java] Update RESTHeart to match benchmark conventions
    
    * Performance optimization in services implementation
    
    * Reduce number of io and worker threads
    
    * Update to restheart 5.1.7
    
    * Add environment variables to config.yml to override io-threads and worker-threads configuration parameters
    
    * Update restheart range dependency
    
    * Remove refactored classes
    
    * Add -XX:-RelaxAccessControlCheck java option
    
    * Remove redundant dependency
    
    * Unpdate from origin
    
    * Update configuration to reduce number of working threads. Trivial requests just require few threads
    
    * Cleanup code
    
    * Cleanup conf file
    
    * Increase working threads to 4
    
    * :arrow_up: Upgrade to RESTHeart 6
    
    * :arrow_up: Upgrade to RESTHeart 7
    
    * :arrow_up: Upgrade RESTHeart to v7
    
    * :green_heart: Remove restheart.yml file from config.yml/binaries
    
    * :wrench: Remove unused configuration option
    
    * :green_heart: Escape quotes in env var
    
    * :green_heart: Escape quotes in env var
    
    * :wrench: Set <restheart.version>[7.0,7.1)</restheart.version>
    
    * :zap: Use dontIntercept to avoid interceptors overhead

--- java/restheart/src/main/java/org/restheart/RootService.java
+++ java/restheart/src/main/java/org/restheart/RootService.java
@@ -5,19 +5,17 @@ import org.restheart.exchange.ByteArrayResponse;
 import org.restheart.plugins.ByteArrayService;
 import org.restheart.plugins.RegisterPlugin;
 import org.restheart.plugins.RegisterPlugin.MATCH_POLICY;
+import static org.restheart.plugins.InterceptPoint.*;
 
 @RegisterPlugin(name = "benchmarkRootService",
     description = "service for root resource",
     defaultURI = "/",
     uriMatchPolicy = MATCH_POLICY.EXACT,
-    blocking = false)
+    blocking = false,
+    dontIntercept = { REQUEST_BEFORE_EXCHANGE_INIT, REQUEST_BEFORE_AUTH, REQUEST_AFTER_AUTH, RESPONSE, RESPONSE_ASYNC  })
 public class RootService implements ByteArrayService {
   @Override
   public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
-    // RESTHeart handles several response headers including CORS headers and X-Powered-By
-    // remove them reducing the response content lenght to ~1/3 for a fair competition :)
-    response.getExchange().getResponseHeaders().clear();
-
     // nothing to do! this just sends 200 back
   }
 }
