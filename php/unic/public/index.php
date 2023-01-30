4215ff4bf13a5d8206c360fe24ac37a9b4f810de
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2023-01-30 18:19:01 +0530

    Update index.php

--- php/unic/public/index.php
+++ php/unic/public/index.php
@@ -5,7 +5,14 @@ use Unic\App;
 use Swoole\Http\Server;
 
 $app = new App();
-$server = new Server('0.0.0.0', 3000);
+$server = new Server('localhost', 3000);
+
+$server->set([
+    'worker_num' => swoole_cpu_num() * 2,
+    'enable_coroutine' => false,
+    'log_file' => '/dev/null',
+    'log_level' => SWOOLE_LOG_ERROR,
+]);
 
 $app->get('/', function($req, $res) {
   $res->send('');
