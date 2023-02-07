e9b4a33bd89159b47d235386026d472ec4296fde
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2023-02-07 14:46:59 +0530

    Added routejs

--- javascript/routejs/app.js
+++ javascript/routejs/app.js
@@ -3,3 +3,17 @@ const { Router } = require("@routejs/router");
 
 const app = new Router();
 
+app.get("/", function (req, res) {
+  res.end("");
+});
+
+app.get("/user/:id", function (req, res) {
+  res.end(req.params.id);
+});
+
+app.post("/user", function (req, res) {
+  res.end("");
+});
+
+const server = http.createServer(app.handler());
+server.listen(3000);
