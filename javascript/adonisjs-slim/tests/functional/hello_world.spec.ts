e9e7e9ad356582b1f5f441eeb674b5699f3bcd9c
dslingr <david@slingr.me>
2023-03-01 15:03:58 -0500

    Made recommended changes to config.yaml and package.json

--- javascript/adonisjs-slim/tests/functional/hello_world.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@japa/runner'
-
-test('display welcome page', async ({ client }) => {
-  const response = await client.get('/')
-
-  response.assertStatus(200)
-  response.assertTextIncludes('Hello world')
-})
