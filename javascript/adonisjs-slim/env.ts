3044c3cc5ae7fb617a4b3adabbe2d41e244040c9
dslingr <david@slingr.me>
2023-03-02 13:08:48 -0500

    Updated config.yaml with files key and moved assembler to dependencies in package.json

--- javascript/adonisjs-slim/env.ts
+++ javascript/adonisjs-slim/env.ts
@@ -12,13 +12,13 @@
 |
 */
 
-import Env from '@ioc:Adonis/Core/Env'
+import Env from "@ioc:Adonis/Core/Env";
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
-	APP_KEY: Env.schema.string(),
-	APP_NAME: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(['local'] as const),
-	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-})
+  HOST: Env.schema.string({ format: "host" }),
+  PORT: Env.schema.number(),
+  APP_KEY: Env.schema.string(),
+  APP_NAME: Env.schema.string(),
+  DRIVE_DISK: Env.schema.enum(["local"] as const),
+  NODE_ENV: Env.schema.enum(["development", "production", "test"] as const),
+});
