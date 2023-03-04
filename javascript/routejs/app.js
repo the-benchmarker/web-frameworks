a79bb68782442546e2ea1d2e9f6a20ed0cf1af01
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2023-03-04 12:45:21 +0530

    Added uWebSocketjs server in routejs

--- javascript/routejs/app.js
+++ javascript/routejs/app.js
@@ -6,13 +6,15 @@ const app = new Router();
 app
   .get("/", (req, res) => res.end(""))
   .get("/user/{id}", (req, res) => res.end(req.params.id))
-  .post("/user", (req, res) => res.end(""));
+  .post("/user", (req, res) => res.end(""))
+  .use((req, res) => res.writeStatus("404").end(""));
 
 uWS
   .App()
   .any("/*", (res, req) => {
     const handler = app.handler();
     handler(req, res);
-  }).listen(3000, () => {
+  })
+  .listen(3000, () => {
     console.log("Server started on port 3000");
   });
