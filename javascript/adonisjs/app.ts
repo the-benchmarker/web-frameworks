0957895d8b82410b94fac4dc66393e37a1236a5d
dslingr <david@slingr.me>
2023-03-05 08:53:32 -0500

    Add AdonisJS (#6184)
    * added adonisjs
    
    * Changed adonisjs project structure from api to slim
    
    * [AdonisJS] Updated / route to return empty body
    
    * added config.yaml file
    
    * Made recommended changes to config.yaml and package.json
    
    * Re-added adonis assembler in package.json
    
    * Updated config.yaml with files key and moved assembler to dependencies in package.json
    
    * clean up
    
    * add environment variables
    
    * remove unnecessary packages
    
    * fix routes
    
    * cleanup
    
    * use thin version
    
    * rename folder
    
    * restore rc file
    
    ---------
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>

---
+++ javascript/adonisjs/app.ts
@@ -0,0 +1,45 @@
+
+/**
+ * @adonisjs/http-server
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import proxyaddr from 'proxy-addr'
+import { createServer } from 'http'
+import { Encryption } from '@adonisjs/encryption/build/standalone'
+import { Application } from '@adonisjs/application'
+import { Server } from '@adonisjs/http-server/build/standalone'
+
+const app = new Application(__dirname, 'web', {})
+app.setup()
+
+const encryption = new Encryption({ secret: 'averylongrandom32charslongsecret' })
+
+const server = new Server(app, encryption, {
+  etag: false,
+  jsonpCallbackName: 'callback',
+  cookie: {},
+  subdomainOffset: 2,
+  generateRequestId: false,
+  trustProxy: proxyaddr.compile('loopback'),
+  allowMethodSpoofing: false,
+})
+  
+server.router.get('/', async () => {
+    return "";
+})
+server.router.post('/user', async () => {
+    return "";
+})
+server.router.get('/user/:id', async ({ params }) => {
+    return params.id;
+})
+server.optimize()
+
+createServer(server.handle.bind(server)).listen(3000, () => {
+  console.log('listening on 3000')
+})
