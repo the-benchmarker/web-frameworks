fc54d7e40ae1281ea2eef682fc305996e3a4de65
Ye Wenbin <yewenbin@winwin-inc.com>
2023-03-21 09:45:35 +0800

    [PHP] Added kuiper framework

---
+++ php/kuiper/src/container.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is automatic generator by kuiper/component-installer, don't edit it manually
+ */
+ 
+return [
+    'component_scan' => [
+        'app'
+    ],
+    'configuration' => [
+        'kuiper\\logger\\LoggerConfiguration',
+        'kuiper\\reflection\\ReflectionConfiguration',
+        'kuiper\\serializer\\SerializerConfiguration',
+        'kuiper\\swoole\\config\\FoundationConfiguration',
+        'kuiper\\swoole\\config\\GuzzleHttpMessageFactoryConfiguration',
+        'kuiper\\swoole\\config\\DiactorosHttpMessageFactoryConfiguration',
+        'kuiper\\web\\WebConfiguration',
+        'kuiper\\event\\EventConfiguration'
+    ]
+];
---
+++ php/kuiper/src/controller/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controller;
+
+use kuiper\di\attribute\Controller;
+use kuiper\web\AbstractController;
+use kuiper\web\attribute\GetMapping;
+use kuiper\web\attribute\PostMapping;
+use Psr\Http\Message\ResponseInterface;
+
+#[Controller]
+class IndexController extends AbstractController
+{
+    #[GetMapping("/")]
+    public function index(): void
+    {
+        $this->getResponse()->getBody()->write("ok\n");
+    }
+
+    #[GetMapping("/user/{id:\d+}")]
+    public function userGet(int $id): void
+    {
+        $this->getResponse()->getBody()->write((string) $id);
+    }
+
+    #[PostMapping("/user")]
+    public function userCreate(): void
+    {
+        $this->getResponse()->getBody()->write("ok\n");
+    }
+}
