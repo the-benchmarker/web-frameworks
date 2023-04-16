7e850ce1e114af90eeba1a34458c562cb9cd3006
Marwan Rabbâa <waghanza@gmail.com>
2023-04-16 13:54:54 +0200

    use latest version

---
+++ scala/zio-http/build.sbt
@@ -0,0 +1,14 @@
+
+
+  val ZioHttpVersion = "[3.0.0-RC1,3.1.0)"
+
+name := "server"
+
+scalaVersion := "3.2.2"
+
+lazy val root = (project in file("."))
+  .settings(
+    libraryDependencies ++= Seq(
+      "dev.zio" %% "zio-http" % ZioHttpVersion
+    )
+  ).enablePlugins(JavaAppPackaging)
---
+++ scala/zio-http/src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+
+import zio._
+import zio.http._
+
+object HelloWorld extends ZIOAppDefault {
+
+  val app = Http.collect[Request] {
+          case Method.GET -> !! / "" => Response.ok
+          case Method.POST -> !! / "user" => Response.ok
+          case Method.POST -> !! / "user" / id => { Response.text(id) }
+  }
+
+  override val run =
+    
+  Server.serve(app).provide(Server.defaultWithPort(3000))
+
+}
