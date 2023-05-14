35b4567e48e7315b3a6878d484e79b5afe45a255
Ethosa <social.ethosa@gmail.com>
2023-05-14 21:08:54 +0700

    hm?

--- nim/happyx/server.nim
+++ nim/happyx/server.nim
@@ -6,7 +6,10 @@ serve("0.0.0.0", 3000):
     ""
   
   get "/user$id?:int":
-    "{id}"
+    "method not allowed"
+  
+  post "/user":
+    "method not allowed"
   
   notfound:
     "method not allowed"
