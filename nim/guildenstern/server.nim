348c0673b4c97bb1c7ce05499cde41029b6e8d14
bung87 <crc32@qq.com>
2023-06-15 23:09:17 +0800

    fix

--- nim/guildenstern/server.nim
+++ nim/guildenstern/server.nim
@@ -1,19 +1,19 @@
-import cgi, strtabs, httpcore, guildenstern/[ctxheader, ctxbody], uri
+import guildenstern/[ctxheader], uri
 
 proc handleGet(ctx: HttpCtx) =
-  let uri = parseUri(ctx.getUri())
-  if uri.path == "/":
+  if ctx.getMethod() == "GET":
+    let uri = parseUri(ctx.getUri())
+    if uri.path == "/":
+      let r = ""
+      ctx.reply(r)
+    else:
+      let id = uri.path[6 .. ^1]
+      ctx.reply(id)
+  else:
     let r = ""
     ctx.reply(r)
-  else:
-    let id = uri.path[6 .. ^1]
-
-proc handlePost(ctx: HttpCtx) =
-  let r = ""
-  ctx.reply(r)
 
 var server = new GuildenServer
-server.initHeaderCtx(handleGet, 3000, false)
-server.initBodyCtx(handlePost, 3000)
+server.initHeaderCtx(handleGet, 3000)
 
 server.serve(loglevel = NONE)
