e65b303e2e60f7f2075acaf66e0822fbdd4001fd
Jim Jin <jim_king_2000@163.com>
2023-07-24 10:03:56 +0800

    fix stricjs

--- javascript/stricjs/app.ts
+++ javascript/stricjs/app.ts
@@ -1,6 +1,6 @@
 import { Router } from '@stricjs/router';  
 
 export default new Router()
-  .get('/', () => new Response(null))
+  .get('/', () => new Response(""))
   .get('/user/:id', ({ params: { id } }) => new Response(id))
-  .post('/user', () => new Response(null));
+  .post('/user', () => new Response(""));
