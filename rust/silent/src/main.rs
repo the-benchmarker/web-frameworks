5231ec46d0174f35f35e5bd0e088a2f808f5aac1
hubertshelley <hubertshelley@163.com>
2023-08-03 16:09:19 +0800

    error fix

--- rust/silent/src/main.rs
+++ rust/silent/src/main.rs
@@ -5,24 +5,16 @@ async fn index(_req: Request) -> Result<Response> {
 }
 
 fn main() {
-    let mut user_route = Route::new("user").append(
-        Route::new("<id>").get(
-            |req| async move {
-                req.get_path_params::<String>("id")
-            }
-        ),
-    );
-    user_route.get_handler_mut().insert(
-        Method::POST,
-        HandlerWrapperResponse::new(index).arc(),
-    );
-    let mut route = Route::new("")
+    let route = Route::new("")
+        .get(index)
         .append(
-            user_route
+            Route::new("user").append(
+                Route::new("<id>").get(
+                    |req| async move {
+                        req.get_path_params::<String>("id")
+                    }
+                ),
+            ).post(index)
         );
-    route.get_handler_mut().insert(
-        Method::GET,
-        HandlerWrapperResponse::new(index).arc(),
-    );
     Server::new().bind("0.0.0.0:3000".parse().unwrap()).bind_route(route).run();
 }
