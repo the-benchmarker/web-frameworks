887226a345b48074cb94443bfe5caa83a219986b
Revenity <aquaplmc@gmail.com>
2023-08-09 23:39:02 +0700

    Update app.ts

--- javascript/stricjs/app.ts
+++ javascript/stricjs/app.ts
@@ -1,12 +1,10 @@
-import { Router, macro } from "@stricjs/router";
+import { Router, macro } from '@stricjs/router';
 
-export default new Router()
-  .get(
-    "/",
-    macro(() => new Response())
-  )
-  .get("/user/:id", (req) => new Response(req.params.id))
-  .post(
-    "/user",
-    macro(() => new Response())
-  );
+export default new Router({ 
+  base: 'http://localhost:3000',
+  parsePath: false
+})
+  .get("/", macro(() => new Response()))
+  // @ts-ignore A bit hacky here
+  .get("/user/:id", macro(r => new Response(r.params.id)))
+  .post("/user", macro(() => new Response()));
