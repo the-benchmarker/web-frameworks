2e2340cb16992a490f06ec4a4491e7f35085964e
Ethosa <social.ethosa@gmail.com>
2023-08-16 18:44:13 +0700

    fix python/happyx/server.py

--- python/happyx/server.py
+++ python/happyx/server.py
@@ -5,15 +5,15 @@ app = new_server('0.0.0.0', 3000)
 
 
 @app.get('/')
-async def index():
+def index():
     return ''
 
 
 @app.get('/user/{id}')
-async def get_user(id: int):
+def get_user(id: int):
     return f'{id}'
 
 
 @app.post('/user')
-async def create_user():
+def create_user():
     return ''
