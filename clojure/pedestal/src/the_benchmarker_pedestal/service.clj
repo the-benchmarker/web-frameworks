a87e5b189b82257f102f2ab7ac4282f9861940dd
Giovanni Cornachini <giovannicornachini@gmail.com>
2023-08-16 06:26:43 -0300

    Add clojure Pedestal web framework (#6612)
    * Add gitignore
    
    * Add Pedestal
    
    * Remove unused resource paths
    
    * Update binaries
    
    * Update clojure/pedestal/config.yaml
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>
    
    * Update port from 8080 to 3000
    
    * Fix connection error when running docker container
    
    * Update config.yaml to make it simpler
    
    ---------
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>

---
+++ clojure/pedestal/src/the_benchmarker_pedestal/service.clj
@@ -0,0 +1,45 @@
+(ns the-benchmarker-pedestal.service
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]))
+
+(def empty-response
+  {:status  200
+   :headers {"content-type" "text/plain"}
+   :body    ""})
+
+(defn success-response
+  [body]
+  {:status  200
+   :headers {"content-type" "text/plain"}
+   :body    body})
+
+(defn user
+  [_]
+  empty-response)
+
+(defn home-page
+  [_]
+  empty-response)
+
+(defn get-user
+  [request]
+  (let [body (-> request
+                 :path-params
+                 :id)]
+    (success-response body)))
+
+(def common-interceptors [(body-params/body-params) http/html-body])
+
+(def routes #{["/" :get (conj common-interceptors `home-page)]
+              ["/user" :post (conj common-interceptors `user)]
+              ["/user/:id" :get (conj common-interceptors `get-user)]})
+
+(def service {:env :prod
+              ::http/routes routes
+              ::http/resource-path "/public"
+              ::http/type :jetty
+              ::http/port 3000
+              ::http/host "0.0.0.0"
+              ::http/container-options {:h2c? true
+                                        :h2? false
+                                        :ssl? false}})
