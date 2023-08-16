453e5e264b6d48ef0ff15306ce97aff077c90560
Ethosa <49402667+Ethosa@users.noreply.github.com>
2023-08-16 18:55:38 +0700

    [New framework] add `python/happyx` web-framework (#6634)
    * Update `nim/happyx/server.nimble`
    
    * Update `nim/happyx/config.yaml`
    
    `-d:numThreads` available only for HappyX
    
    * delete `-d:numThreads` from `nim/happyx`
    
    * add `python/happyx`
    
    * fix python/happyx/server.py
    
    * fix python/happyx/server.py

---
+++ python/happyx/server.py
@@ -0,0 +1,22 @@
+from happyx import new_server
+
+
+app = new_server('0.0.0.0', 3000)
+
+
+@app.get('/')
+def index():
+    return ''
+
+
+@app.get('/user/{id}')
+def get_user(id: int):
+    return f'{id}'
+
+
+@app.post('/user')
+def create_user():
+    return ''
+
+
+app.start()
