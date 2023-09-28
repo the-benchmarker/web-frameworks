b5d007ec1e4c9ca4d02f65cf5b02909080d5390d
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-28 21:31:13 +0300

    refactor(ditsmod): simplifying routing and add prettier.

--- javascript/ditsmod/src/app/app.module.ts
+++ javascript/ditsmod/src/app/app.module.ts
@@ -1,11 +1,23 @@
-import { rootModule } from '@ditsmod/core';
+import { AnyObj, Router, rootModule } from '@ditsmod/core';
 import { RouterModule } from '@ditsmod/router';
 import { BodyParserModule } from '@ditsmod/body-parser';
+import { parse, Headers } from 'get-body';
 
-import { HelloWorldController } from './hello-world.controller.js';
-
-@rootModule({
-  controllers: [HelloWorldController],
-  imports: [RouterModule, BodyParserModule]
-})
-export class AppModule {}
+@rootModule({ imports: [RouterModule, BodyParserModule] })
+export class AppModule {
+  constructor(router: Router) {
+    router
+      .on('GET', '/', async (nodeReq, nodeRes) => {
+        nodeRes.end();
+      })
+      .on('GET', '/user/:id', async (nodeReq, nodeRes, aPathParams) => {
+        const pathParams: AnyObj = {};
+        aPathParams.forEach((param) => (pathParams[param.key] = param.value));
+        nodeRes.end(pathParams.id);
+      })
+      .on('POST', '/user', async (nodeReq, nodeRes) => {
+        await parse(nodeReq, nodeReq.headers as Headers);
+        nodeRes.end();
+      });
+  }
+}
