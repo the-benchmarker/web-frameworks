fe733f0f83e273ae112873bb8524e136e3fe2357
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-30 17:19:40 +0300

    refactor(ditsmod): migration to @ditsmod/core@2.50

--- javascript/ditsmod/src/app/app.module.ts
+++ javascript/ditsmod/src/app/app.module.ts
@@ -1,23 +1,10 @@
-import { AnyObj, Router, rootModule } from '@ditsmod/core';
-import { RouterModule } from '@ditsmod/router';
+import { rootModule } from '@ditsmod/core';
+import { RoutingModule } from '@ditsmod/routing';
 import { BodyParserModule } from '@ditsmod/body-parser';
-import { parse, Headers } from 'get-body';
+import { HelloWorldController } from './hello-world.controller.js';
 
-@rootModule({ imports: [RouterModule, BodyParserModule] })
-export class AppModule {
-  constructor(router: Router) {
-    router
-      .on('GET', '/', async (nodeReq, nodeRes) => {
-        nodeRes.end();
-      })
-      .on('GET', '/user/:id', async (nodeReq, nodeRes, aPathParams) => {
-        const pathParams: AnyObj = {};
-        aPathParams.forEach((param) => (pathParams[param.key] = param.value));
-        nodeRes.end(pathParams.id);
-      })
-      .on('POST', '/user', async (nodeReq, nodeRes) => {
-        await parse(nodeReq, nodeReq.headers as Headers);
-        nodeRes.end();
-      });
-  }
-}
+@rootModule({
+  imports: [RoutingModule, BodyParserModule],
+  controllers: [HelloWorldController],
+})
+export class AppModule {}
