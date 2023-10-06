35e4cdf04dd1812e08ac51a52c1347859754a9fd
Marwan <marwan.rabbaa@winddle.com>
2023-10-06 17:04:18 +0200

    remove unecessary files

--- ruby/rails/config/application.rb
+++ ruby/rails/config/application.rb
@@ -23,5 +23,6 @@ module Benchmark
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
   end
 end
--- ruby/rails/config/environments/production.rb
+++ ruby/rails/config/environments/production.rb
@@ -23,12 +23,6 @@ Rails.application.configure do
   # Enable static file serving from the `/public` folder (turn off if using NGINX/Apache for it).
   config.public_file_server.enabled = true
 
-  # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
