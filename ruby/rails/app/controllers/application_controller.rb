d66c19d91baf3725af4e8338d727f2fcbe4668e2
Marwan <marwan.rabbaa@winddle.com>
2023-10-07 10:57:33 +0200

    syntax error

--- ruby/rails/app/controllers/application_controller.rb
+++ ruby/rails/app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-    def index
-      head 200
-    end
-  
-    def user
-      render plain: params['id']
-    end
-  
-    def register_user
-      head 200
-    end
+  def index
+    head 200
   end
-  
+
+  def user
+    render plain: params['id']
+  end
+
+  def register_user
+    head 200
+  end
 end
