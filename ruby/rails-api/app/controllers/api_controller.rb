68f5128c5ed409b35c1a6f87dc7f1f0b5adf4855
Seth T <crimsonknightstudios@gmail.com>
2023-10-27 03:24:54 -0400

    Rails 7 config updates (#6818)
    * Updated the Amber command to use the 'app' binary name that the default docker file uses
    
    * - Made some small changes to the configuration for benchmarking Rails
    - Made an Rails-api test to compare some settings and controller performance when using the ActionController::API for API-Only applications vs the ActionController::Base which is what the current Rails test is using
    
    * Update ruby/rails-api/config/environments/production.rb
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>
    
    * Added 2 simple active storage configs to satisfy Rails desire to find a config
    
    * clean ruby config files
    
    * do not update keyfiles on containers
    
    * removed the unnecessary frozen_string_literal and added some basic secrets/encrypt setup to make rails prod happy
    
    * removed the unnecessary frozen_string_literal and added some basic secrets/encrypt setup to make rails prod happy
    
    * use hard coded secret key
    
    * Changed rails to not force SSL in production mode
    
    * Skip CSRF token checking since we are not submitting a real form
    
    ---------
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>

--- ruby/rails-api/app/controllers/api_controller.rb
+++ ruby/rails-api/app/controllers/api_controller.rb
@@ -1,6 +1,4 @@
-class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-  
+class ApiController < ActionController::API
   def index
     head 200
   end
--- ruby/rails/config/routes.rb
+++ ruby/rails/config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   get '/' => 'application#index'
   get '/user/:id' => 'application#user'
