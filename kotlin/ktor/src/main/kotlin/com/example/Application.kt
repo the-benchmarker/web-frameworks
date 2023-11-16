ee54d87f91a85ad4e7a464cc525aa44c9d1ec3dd
Marwan <waghanza@gmail.com>
2023-11-16 14:35:06 +0100

    configuration

--- kotlin/ktor/src/main/kotlin/com/example/Application.kt
+++ kotlin/ktor/src/main/kotlin/com/example/Application.kt
@@ -6,8 +6,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+    embeddedServer(Netty, module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
