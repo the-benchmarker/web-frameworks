e2e88b4aaccfe8935e0008134d02e06f9a26720d
Marwan Rabbâa <waghanza@gmail.com>
2023-11-16 14:52:14 +0100

    [Kotlin] Update ktor to 2.3 (#6940)
    * upgrade with start.ktor.io
    
    * cleanup
    
    * update version in results
    
    * create fatjar
    
    * add application config
    
    * configuration
    
    * configuration
    
    * remove config file
    
    * typo

---
+++ kotlin/ktor/build.gradle.kts
@@ -0,0 +1,34 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.+"
+    id("io.ktor.plugin") version "2.3.+"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+ktor {
+    fatJar {
+        archiveFileName.set("server.jar")
+    }
+}
