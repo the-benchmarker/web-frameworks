5bfe759bb2f35b13831a57a189e4d4bba25179d3
Marwan <waghanza@gmail.com>
2023-11-16 14:44:43 +0100

    typo

--- kotlin/ktor/src/main/kotlin/com/example/Application.kt
+++ kotlin/ktor/src/main/kotlin/com/example/Application.kt
@@ -6,7 +6,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 3000, host = "0.0.0.0", mmodule = Application::module)
+    embeddedServer(Netty, port = 3000, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
