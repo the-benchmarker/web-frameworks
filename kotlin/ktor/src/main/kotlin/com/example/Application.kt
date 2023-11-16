44473b20fe1bd031a176a6895cec4df80a691271
Marwan <waghanza@gmail.com>
2023-11-16 14:39:24 +0100

    remove config file

--- kotlin/ktor/src/main/kotlin/com/example/Application.kt
+++ kotlin/ktor/src/main/kotlin/com/example/Application.kt
@@ -6,7 +6,8 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = 3000, host = "0.0.0.0", mmodule = Application::module)
+        .start(wait = true)
 }
 
 fun Application.module() {
