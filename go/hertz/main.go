da01cfea3da0f044c9fb8d0db1e5ca2737a73783
Skyenought <skyenought@qq.com>
2023-11-16 10:52:30 +0800

    Aligning hertz configuration items with gofiber

--- go/hertz/main.go
+++ go/hertz/main.go
@@ -12,6 +12,8 @@ func main() {
 		server.WithHostPorts(":3000"),
 		server.WithDisableHeaderNamesNormalizing(true),
 		server.WithDisablePrintRoute(true),
+		server.WithDisableDefaultDate(true),
+		server.WithDisableDefaultContentType(true),
 	)
 
 	r.GET("/", func(_ context.Context, ctx *app.RequestContext) {})
