3c415516d42302c08afcc7b928bb6444b44b600e
Marwan <waghanza@gmail.com>
2023-11-17 14:36:03 +0100

    upgrade blade

--- java/blade/src/main/java/com/example/Application.java
+++ java/blade/src/main/java/com/example/Application.java
@@ -1,16 +1,16 @@
 package com.example;
 
-import com.blade.Blade;
+import com.hellokaton.blade.Blade;
 
 public class Application {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Blade.of()
           .get("/", ctx -> ctx.text(""))
-          .get("/user/:id", (request, response) -> {
-            response.text(request.pathString("id"));
+          .get("/user/:id", ctx -> {
+            ctx.text(ctx.pathString("id"));
           })
           .post("/user", ctx -> ctx.text(""))
-          .start(Application.class, args);
-    }
-}
+          .start();
+    
+} }
