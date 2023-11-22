fc99d4bc7c975b87b605643be1c5b9da05228fad
Marwan <waghanza@gmail.com>
2023-11-22 18:50:03 +0100

    update using micronaut launch

---
+++ java/micronaut/build.gradle.kts
@@ -0,0 +1,58 @@
+buildscript {
+   System.setProperty("micronautVersion", "4.2.+")
+   System.setProperty("shadowVersion", "+")
+}
+
+plugins {    
+    id("com.github.johnrengelman.shadow") version System.getProperty("shadowVersion")
+    id("io.micronaut.application") version System.getProperty("micronautVersion")
+    id("io.micronaut.aot") version System.getProperty("micronautVersion")
+}
+
+version = "0.1"
+group = "com.example"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    annotationProcessor("io.micronaut:micronaut-http-validation")
+    annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    compileOnly("io.micronaut:micronaut-http-client")
+    runtimeOnly("ch.qos.logback:logback-classic")
+}
+
+
+application {
+    mainClass.set("com.example.Application")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+    targetCompatibility = JavaVersion.toVersion("17")
+}
+
+
+graalvmNative.toolchainDetection.set(false)
+micronaut {
+    runtime("netty")
+    processing {
+        incremental(true)
+        annotations("com.example.*")
+    }
+    aot {
+    // Please review carefully the optimizations enabled below
+    // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading.set(false)
+        convertYamlToJava.set(false)
+        precomputeOperations.set(true)
+        cacheEnvironment.set(true)
+        optimizeClassLoading.set(true)
+        deduceEnvironment.set(true)
+        optimizeNetty.set(true)
+    }
+}
+
+
+
