18751b8682c7ded4ece4f23820572610f5d22243
Marwan <waghanza@gmail.com>
2023-12-03 14:20:48 +0100

    remove mezzio tooling

--- php/mezzio/config/config.php
+++ php/mezzio/config/config.php
@@ -14,7 +14,6 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
-    \Mezzio\Tooling\ConfigProvider::class,
     \Mezzio\Helper\ConfigProvider::class,
     \Mezzio\Router\FastRouteRouter\ConfigProvider::class,
     \Laminas\HttpHandlerRunner\ConfigProvider::class,
