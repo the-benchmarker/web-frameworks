88c391392937b570b27aa6db85beaa12163b15a8
Fangdun Tsai <fundon@pindash.io>
2023-12-03 23:03:09 +0800

    [Rust]: viz-v0.5 (#7019)

--- rust/viz/src/main.rs
+++ rust/viz/src/main.rs
@@ -1,7 +1,8 @@
 #![deny(warnings)]
 
-use std::net::SocketAddr;
-use viz::{Error, Request, RequestExt, Result, Router, Server, ServiceMaker};
+use std::{net::SocketAddr, sync::Arc};
+use tokio::net::TcpListener;
+use viz::{serve, Request, RequestExt, Result, Router, Tree};
 
 async fn index(_: Request) -> Result<()> {
     Ok(())
@@ -18,13 +19,19 @@ async fn create_user(_: Request) -> Result<()> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let listener = TcpListener::bind(addr).await?;
+
     let app = Router::new()
         .get("/", index)
         .post("/user", create_user)
         .get("/user/:id", show_user);
 
-    Server::bind(&SocketAddr::from(([0, 0, 0, 0], 3000)))
-        .serve(ServiceMaker::from(app))
-        .await
-        .map_err(Error::normal)
+    let tree = Arc::new(Tree::from(app));
+
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        let tree = tree.clone();
+        tokio::task::spawn(serve(stream, tree, Some(addr)));
+    }
 }
