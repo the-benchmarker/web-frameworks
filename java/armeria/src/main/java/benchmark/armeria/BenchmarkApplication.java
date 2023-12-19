8de1b12b704e3da7040eb1d4178299de7807605b
Will Vuong <wvuong@gmail.com>
2023-12-19 10:19:26 -0500

    Address code review comments

--- java/armeria/src/main/java/benchmark/armeria/BenchmarkApplication.java
+++ java/armeria/src/main/java/benchmark/armeria/BenchmarkApplication.java
@@ -5,11 +5,13 @@ import com.linecorp.armeria.server.Server;
 
 public class BenchmarkApplication {
 
+    private static final HttpResponse EMPTY_HTTP_RESPONSE = HttpResponse.of("");
+
     public static void main(String... args) {
         Server server = Server.builder()
-                .http(8080)
-                .service("/", (ctx, req) -> HttpResponse.of(""))
-                .service("/user", (ctx, req) -> HttpResponse.of(""))
+                .http(3000)
+                .service("/", (ctx, req) -> EMPTY_HTTP_RESPONSE)
+                .service("/user", (ctx, req) -> EMPTY_HTTP_RESPONSE)
                 .service("/user/:userId", (ctx, req) -> HttpResponse.of(ctx.pathParam("userId")))
                 .build();
 
