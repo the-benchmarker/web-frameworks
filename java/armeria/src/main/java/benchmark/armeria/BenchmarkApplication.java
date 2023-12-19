5b1aaa815d435d5da1ede66a493a0a00e2d2528d
Will Vuong <wvuong@gmail.com>
2023-12-19 10:51:51 -0500

    Change route to POST

--- java/armeria/src/main/java/benchmark/armeria/BenchmarkApplication.java
+++ java/armeria/src/main/java/benchmark/armeria/BenchmarkApplication.java
@@ -1,6 +1,8 @@
 package benchmark.armeria;
 
+import com.linecorp.armeria.common.HttpMethod;
 import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Route;
 import com.linecorp.armeria.server.Server;
 
 public class BenchmarkApplication {
@@ -8,11 +10,14 @@ public class BenchmarkApplication {
     private static final HttpResponse EMPTY_HTTP_RESPONSE = HttpResponse.of("");
 
     public static void main(String... args) {
-        Server server = Server.builder()
+        final Server server = Server.builder()
                 .http(3000)
                 .service("/", (ctx, req) -> EMPTY_HTTP_RESPONSE)
                 .service("/user", (ctx, req) -> EMPTY_HTTP_RESPONSE)
-                .service("/user/:userId", (ctx, req) -> HttpResponse.of(ctx.pathParam("userId")))
+                .service(Route.builder()
+                        .path("/user/:userId")
+                        .methods(HttpMethod.POST)
+                        .build(), (ctx, req) -> HttpResponse.of(ctx.pathParam("userId")))
                 .build();
 
         server.closeOnJvmShutdown();
