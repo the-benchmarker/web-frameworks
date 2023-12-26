a09460153ad5da040929ca1cfce453c6e5991734
Ikhun Um <ih.pert@gmail.com>
2023-12-26 10:59:17 +0900

    Use `UNARY` type for Armeria `HttpService`
    The default `ExchangeType` for `HttpService` is `BIDI_STREAMING` that
    is not optimized for non-streaming / simple requests.
    `UNARY` type is suitable for the current benchmark.
    
    In fact, `ExchangeType` is automatically inferred for annotated
    service, gRPC, Thrift services so normal users don't need to set
    the type explicitly.

--- java/armeria/src/main/java/benchmark/armeria/BenchmarkApplication.java
+++ java/armeria/src/main/java/benchmark/armeria/BenchmarkApplication.java
@@ -1,24 +1,37 @@
 package benchmark.armeria;
 
+import com.linecorp.armeria.common.ExchangeType;
 import com.linecorp.armeria.common.HttpMethod;
+import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.HttpService;
 import com.linecorp.armeria.server.Route;
+import com.linecorp.armeria.server.RoutingContext;
 import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServiceRequestContext;
 
 public class BenchmarkApplication {
 
     public static void main(String... args) {
         final Server server = Server.builder()
                 .http(3000)
-                .service("/", (ctx, req) -> HttpResponse.of(""))
-                .service("/user/:userId", (ctx, req) -> HttpResponse.of(ctx.pathParam("userId")))
+                .service("/", (UnaryService) (ctx, req) -> HttpResponse.of(""))
+                .service("/user/:userId", (UnaryService) (ctx, req) -> HttpResponse.of(ctx.pathParam("userId")))
                 .service(Route.builder()
                         .path("/user")
                         .methods(HttpMethod.POST)
-                        .build(), (ctx, req) -> HttpResponse.of(""))
+                        .build(), (UnaryService) (ctx, req) -> HttpResponse.of(""))
                 .build();
 
         server.closeOnJvmShutdown();
         server.start().join();
+    }
+
+    @FunctionalInterface
+    private interface UnaryService extends HttpService {
+        @Override
+        default ExchangeType exchangeType(RoutingContext routingContext) {
+            return ExchangeType.UNARY;
+        }
     }
 }
