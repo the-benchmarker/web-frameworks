8d534886733afae5cb39b6fe8b324e86a2ba2387
Serg Gini <kornburn@gmail.com>
2024-01-20 02:16:14 +0200

    [D] Handy fixes
    Added proper config for server and switch-off logging output.

--- d/handy/source/app.d
+++ d/handy/source/app.d
@@ -1,17 +1,22 @@
 import handy_httpd;
-import handy_httpd.handlers.path_handler : PathHandler;
+import handy_httpd.handlers.path_handler;
 
 void main() {
+    import slf4d;
+    import slf4d.default_provider;
+
+    auto provider = new shared DefaultProvider(true, Levels.ERROR);
+    configureLoggingProvider(provider);
+
     ServerConfig cfg = ServerConfig.defaultValues();
-    cfg.workerPoolSize = 50;
-    cfg.requestQueueSize = 300;
     cfg.hostname = "0.0.0.0";
     cfg.port = 3000;
     cfg.enableWebSockets = false;
+
     auto pathHandler = new PathHandler()
         .addMapping(Method.GET, "/", (ref ctx) {ctx.response.okResponse();})
         .addMapping(Method.POST, "/user", (ref ctx) {ctx.response.okResponse();})
         .addMapping(Method.GET, "/user/:userId", (ref ctx) {ctx.response.okResponse(ctx.request.pathParams["userId"]);});
 
-    new HttpServer(pathHandler).start();
+    new HttpServer(pathHandler, cfg).start();
 }
