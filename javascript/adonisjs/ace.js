2dde04fb671a6c13ad846b92948015d07b72e552
Marwan Rabbâa <waghanza@gmail.com>
2024-02-11 09:13:52 +0100

    update

--- javascript/adonisjs/ace.js
+++ javascript/adonisjs/ace.js
@@ -19,10 +19,10 @@
 /**
  * Register hook to process TypeScript files using ts-node
  */
-import { register } from 'node:module'
-register('ts-node/esm', import.meta.url)
+import { register } from 'node:module';
+register('ts-node/esm', import.meta.url);
 
 /**
  * Import ace console entrypoint
  */
-await import('./bin/console.js')
+await import('./bin/console.js');
