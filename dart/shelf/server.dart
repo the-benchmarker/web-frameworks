5e29229fa05f9f80e2de05b61394bcaf2ac87f4b
Tien Do Nam <dev.tien.donam@gmail.com>
2024-02-18 16:17:52 +0100

    feat: enable multithreading on Dart/shelf

--- dart/shelf/server.dart
+++ dart/shelf/server.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart';
 
 void main() async {
+  for (var i = 0; i < Platform.numberOfProcessors - 1; i++) {
+    Isolate.spawn((_) => startServer(), null);
+  }
+
+  // last server running in main isolate
+  // to keep the main isolate running
+  await startServer();
+}
+
+Future<void> startServer() async {
   var app = Router();
 
   app.get('/', (Request request) => Response.ok(''));
@@ -11,5 +24,5 @@ void main() async {
 
   app.get('/user/<user>', (Request request, String user) => Response.ok(user));
 
-  await shelf_io.serve(app, '0.0.0.0', 3000);
+  await shelf_io.serve(app, '0.0.0.0', 3000, shared: true);
 }
