67686adbf9e2b57866f17c815e20a4ce6271c102
Romain Lanz <2793951+RomainLanz@users.noreply.github.com>
2024-02-23 12:40:46 +0100

    fix(adonis): remove not needed stuff (#7230)
    * fix(adonis): remove not needed stuff
    
    * fix(adonis): remove more stuff and add binaries
    
    * fix(adonis): add bin directory to exception
    
    * fix(adonis): correct dependencies list & version

--- javascript/adonisjs6-slim/start/kernel.ts
+++ javascript/adonisjs6-slim/start/kernel.ts
@@ -8,41 +8,30 @@
 |
 */
 
-import router from '@adonisjs/core/services/router'
-import server from '@adonisjs/core/services/server'
+import router from '@adonisjs/core/services/router';
+import server from '@adonisjs/core/services/server';
 
 /**
  * The error handler is used to convert an exception
  * to a HTTP response.
  */
-server.errorHandler(() => import('#exceptions/handler'))
+server.errorHandler(() => import('#exceptions/handler'));
 
 /**
  * The server middleware stack runs middleware on all the HTTP
  * requests, even if there is no route registered for
  * the request URL.
  */
-server.use([
-  () => import('#middleware/container_bindings_middleware'),
-  () => import('#middleware/force_json_response_middleware'),
-  () => import('@adonisjs/cors/cors_middleware'),
-])
+server.use([() => import('#middleware/container_bindings_middleware')]);
 
 /**
  * The router middleware stack runs middleware on all the HTTP
  * requests with a registered route.
  */
-router.use([
-  () => import('@adonisjs/core/bodyparser_middleware'),
-  () => import('@adonisjs/session/session_middleware'),
-  () => import('@adonisjs/auth/initialize_auth_middleware'),
-])
+router.use([() => import('@adonisjs/core/bodyparser_middleware')]);
 
 /**
  * Named middleware collection must be explicitly assigned to
  * the routes or the routes group.
  */
-export const middleware = router.named({
-  guest: () => import('#middleware/guest_middleware'),
-  auth: () => import('#middleware/auth_middleware'),
-})
+export const middleware = router.named({});
