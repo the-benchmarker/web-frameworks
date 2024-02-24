874521ff84acf2bda20bb224a4dd4548f0c66f0b
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2024-02-24 20:23:47 +0700

    [JavaScript] add blaze (#7252)

---
+++ javascript/blaze/services/core.js
@@ -0,0 +1,19 @@
+const { BlazeCreator } = require('@busy-hour/blaze');
+
+const list = BlazeCreator.action({
+  rest: 'GET /',
+  handler(ctx) {
+    ctx.response.set('text');
+
+    return '';
+  },
+});
+
+const service = BlazeCreator.service({
+  name: '',
+  actions: {
+    list,
+  },
+});
+
+module.exports = service;
