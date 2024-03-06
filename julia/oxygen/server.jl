d83d6804b18847df2aa5ffd5d46fc272024bd5b7
AbrJA <ajaimes@occ.com.mx>
2024-03-06 10:27:31 -0600

    Using render functions

--- julia/oxygen/server.jl
+++ julia/oxygen/server.jl
@@ -5,16 +5,15 @@ using Oxygen
 using HTTP
 
 @get "/" function()
-    ""
+    return text("")
 end
 
-@get "/user/{id}" function(request::HTTP.Request, id)
-    id
+@get "/user/{id}" function(request::HTTP.Request, id::String)
+    return text(id)
 end
 
 @post "/user" function ()
-   ""
+   return text("")
 end
 
-# Could be faster setting access_log = nothing and using multithreading with serveparallel
-serve(host = "0.0.0.0", port = 3000, docs = false, metrics = false)
+serveparallel(host = "0.0.0.0", port = 3000, docs = false, metrics = false, access_log = nothing)
