8077dced7cc62c074b3ad71a51a39a7603f5e9a1
Rolando Santamaria Maso <kyberneees@gmail.com>
2024-03-18 20:06:39 +0100

    linting

--- javascript/0http-bun/cluster.ts
+++ javascript/0http-bun/cluster.ts
@@ -2,8 +2,8 @@ import os from "node:os";
 
 const numCPUs = os.cpus().length;
 for (let i = 0; i < numCPUs; i++) {
-    Bun.spawn(["bun", "app.ts"], {
-        stdio: ["inherit", "inherit", "inherit"],
-        env: { ...process.env },
-    });
+  Bun.spawn(["bun", "app.ts"], {
+    stdio: ["inherit", "inherit", "inherit"],
+    env: { ...process.env },
+  });
 }
