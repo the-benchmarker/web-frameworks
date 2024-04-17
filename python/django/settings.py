09dab0de79b2a48e1fc63317db64a626754f045c
Andrii Kuzmin <jack.cvr@gmail.com>
2024-04-17 14:31:30 +0300

    disable unused django apps and middlewares (#7391)

--- python/django/settings.py
+++ python/django/settings.py
@@ -16,24 +16,9 @@ ALLOWED_HOSTS += ["172.17.%s.%s" % (i, j) for i in range(256) for j in range(256
 
 # Application definition
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-]
+INSTALLED_APPS = []
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+MIDDLEWARE = []
 
 ROOT_URLCONF = "app.urls"
 
