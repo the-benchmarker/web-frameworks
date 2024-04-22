6f7103ec473e1e94320df609d3278d6e20035194
Sergei Giniatulin <kornburn@gmail.com>
2024-04-22 11:33:40 +0300

    [D] Serverino debug/test
    Test other option for serverino

--- d/serverino/source/app.d
+++ d/serverino/source/app.d
@@ -13,7 +13,8 @@ mixin ServerinoMain;
         .create()
         .setHttpTimeout(10.seconds)
         .enableKeepAlive(180.seconds)
-        .addListener("0.0.0.0", 3000);
+        .addListener("0.0.0.0", 3000)
+        .setMinWorkers(8);
 }
 
 @safe
