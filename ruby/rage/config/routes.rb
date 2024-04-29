ece837094f55eb74c708f79a130927c67fd47596
Marwan Rabbâa <waghanza@gmail.com>
2024-04-29 08:09:55 +0200

    update

--- ruby/rage/config/routes.rb
+++ ruby/rage/config/routes.rb
@@ -1,5 +1,5 @@
 Rage.routes.draw do
-  get "/", to: "application#index"
-  get "/user/:id", to: "application#user"
-  post "/user", to: "application#register_user"
+  get '/', to: 'application#index'
+  get '/user/:id', to: 'application#user'
+  post '/user', to: 'application#register_user'
 end
