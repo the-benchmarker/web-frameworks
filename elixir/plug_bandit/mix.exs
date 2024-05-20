fcf8937512872d6a33ebf273a5f190790fb1cf30
Marwan Rabbâa <waghanza@gmail.com>
2024-05-20 22:23:38 +0200

    update (#7470)

--- elixir/plug_bandit/mix.exs
+++ elixir/plug_bandit/mix.exs
@@ -9,7 +9,7 @@ defmodule Server.MixProject do
       start_permanent: Mix.env() == :prod,
       releases: [server: [include_executables_for: [:unix]]],
       deps: [
-        {:plug, "~> 1.15.3"},
+        {:plug, "~> 1.16.0"},
         {:bandit, "~> 1.2.3"}
       ]
     ]
