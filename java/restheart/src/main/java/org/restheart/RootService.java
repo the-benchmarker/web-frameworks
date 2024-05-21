f2a2cc82f1fe6c0470e70f13ad3a4708b4c5da73
Andrea Di Cesare <andrea@softinstigate.com>
2024-05-21 13:12:31 +0200

    :arrow_up: Upgrade RESTHeart to v8.0.x

--- java/restheart/src/main/java/org/restheart/RootService.java
+++ java/restheart/src/main/java/org/restheart/RootService.java
@@ -3,16 +3,16 @@ package org.restheart;
 import org.restheart.exchange.ByteArrayRequest;
 import org.restheart.exchange.ByteArrayResponse;
 import org.restheart.plugins.ByteArrayService;
+import static org.restheart.plugins.InterceptPoint.ANY;
 import org.restheart.plugins.RegisterPlugin;
 import org.restheart.plugins.RegisterPlugin.MATCH_POLICY;
-import static org.restheart.plugins.InterceptPoint.*;
 
 @RegisterPlugin(name = "benchmarkRootService",
     description = "service for root resource",
     defaultURI = "/",
     uriMatchPolicy = MATCH_POLICY.EXACT,
     blocking = false,
-    dontIntercept = { REQUEST_BEFORE_EXCHANGE_INIT, REQUEST_BEFORE_AUTH, REQUEST_AFTER_AUTH, RESPONSE, RESPONSE_ASYNC  })
+    dontIntercept = ANY)
 public class RootService implements ByteArrayService {
   @Override
   public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
