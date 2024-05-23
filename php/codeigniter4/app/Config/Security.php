33016a96d02a0664ab76c1c69ab21234011bf61b
Michal Sniatala <michal@sniatala.pl>
2024-05-23 10:58:03 +0200

    [PHP] Update CodeIgniter to v4.5 (#7480)
    * Update CodeIgniter to v4.5
    
    * update config.yaml

--- php/codeigniter4/app/Config/Security.php
+++ php/codeigniter4/app/Config/Security.php
@@ -79,8 +79,10 @@ class Security extends BaseConfig
      * --------------------------------------------------------------------------
      *
      * Redirect to previous page with error on failure.
+     *
+     * @see https://codeigniter4.github.io/userguide/libraries/security.html#redirection-on-failure
      */
-    public bool $redirect = false;
+    public bool $redirect = (ENVIRONMENT === 'production');
 
     /**
      * --------------------------------------------------------------------------
