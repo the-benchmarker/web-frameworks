49800d5ccffeaf512186dbec3061a69970e2e7d6
Amir <faramarzii.amir@gmail.com>
2024-07-12 16:42:06 +0330

    fix Exceptions/Handler.php

--- php/fomo/app/Exceptions/Handler.php
+++ php/fomo/app/Exceptions/Handler.php
@@ -11,13 +11,20 @@ class Handler
     {
         return response()->json([
             'message' => 'not found'
-        ], 404);
+        ] , 404);
     }
 
     public function notAllowedHttpException(Request $request): Response
     {
         return response()->json([
             'message' => "this is route supported {$request->method()} method"
-        ], 405);
+        ] , 405);
+    }
+
+    public function InternalErrorException(Request $request, Throwable $error): Response
+    {     
+        return response()->json([
+            'message' => 'internal error'
+        ] , 500);
     }
 }
