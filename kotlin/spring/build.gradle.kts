ff388b0295f8b250ffcb111c29c162add6453010
Mario <mmaryo@users.noreply.github.com>
2024-07-13 17:42:54 +0400

    Add Kotlin "Spring" (#7605)
    * Add Kotlin "Spring"
    
    * Add Kotlin "Spring"
    
    * Add Kotlin "Spring"
    
    ---------
    
    Co-authored-by: mario <mario>

---
+++ kotlin/spring/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	id("org.springframework.boot") version "3.3.+"
+	id("io.spring.dependency-management") version "+"
+	kotlin("jvm") version "1.9.+"
+	kotlin("plugin.spring") version "1.9.+"
+}
+
+group = "benchmark.spring-boot"
+version = "0.0.1"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+}
+
+tasks.bootJar {
+	archiveFileName.set("server.jar")
+}
