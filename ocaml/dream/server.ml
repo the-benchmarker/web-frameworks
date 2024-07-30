b511441d3f433a4ed2d31c1393c7ccdf54acd267
Philip Xu <pyx@xrefactor.com>
2024-07-30 00:01:07 -0400

    [OCaml] Add dream
    Dependencies:
    
      - opam 2.x
      - OCaml 5.2.0 or newer
      - dune 2.0+
      - dream 1.0.0~alpha6
    
    Only `opam` is needed from system, everything else can be fetched from
    official repositories and installed by `opam` inside a local switch with
    `make deps` and follow the instructions. `make serve` to run.
    
    The dream server, by default, runs in a single thread (via Lwt), no
    optimization applied in code, I even deliberately convert the id
    parameter into integer and back to string for output, to mimic many
    other frameworks with auto type-conversion.

---
+++ ocaml/dream/server.ml
@@ -0,0 +1,12 @@
+let index _request = Dream.html ""
+
+let user_info request =
+  match Dream.param request "id" |> int_of_string_opt with
+  | None -> Dream.empty `Not_Found
+  | Some id -> id |> string_of_int |> Dream.html
+
+let user _request = Dream.html ""
+
+let () =
+  let open Dream in
+  run @@ router [ get "/" index; get "/user/:id" user_info; post "/user" user ]
