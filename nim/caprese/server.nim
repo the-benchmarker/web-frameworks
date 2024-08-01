6424e6a90626761b239289b9e48e85f632e54ed4
zenywallet <zenywallet@bitzeny.jp>
2024-08-01 11:57:31 +0900

    remove return

--- nim/caprese/server.nim
+++ nim/caprese/server.nim
@@ -10,8 +10,8 @@ config:
 
 server(ip = "0.0.0.0", port = 3000):
   routes:
-    get "/": return "".addActiveHeader("text").send
-    get startsWith("/user/"): return reqUrl[6..^1].addHeader("text").send
-    post "/user": return "".addActiveHeader("text").send
+    get "/": "".addActiveHeader("text").send
+    get startsWith("/user/"): reqUrl[6..^1].addHeader("text").send
+    post "/user": "".addActiveHeader("text").send
 
 serverStart()
