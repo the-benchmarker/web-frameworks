efe06ddfb9a31683eb1dee740f68a9108cfe74f3
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-08-13 22:42:18 +0330

    Update UserController.cs

--- csharp/codebehind/UserController.cs
+++ csharp/codebehind/UserController.cs
@@ -2,10 +2,10 @@ using CodeBehind;
 
 public partial class UserController : CodeBehindController
 {
-    public void PageLoad(HttpContext context)
+    public void CodeBehindConstructor()
     {
         if (Section.Count() > 0)
-            Write("0"); // path: /user/id
+            Write(Section.GetValue(0)); // path: /user/id
         else
             Write(""); // path: /user
     }
