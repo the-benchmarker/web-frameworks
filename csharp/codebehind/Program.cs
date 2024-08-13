20f0c865528a36befcdd4d943cfb8600a074b246
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-08-13 19:10:30 +0330

    Update Program.cs

--- csharp/codebehind/Program.cs
+++ csharp/codebehind/Program.cs
@@ -2,17 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-SetCodeBehind.CodeBehindCompiler.Initialization(); // Set Initialization(true) Before Publish Project
+SetCodeBehind.CodeBehindCompiler.Initialization(true);
 
-app.Run(async context =>
-{
-    CodeBehind.CodeBehindExecute execute = new CodeBehind.CodeBehindExecute();
-    string Result = execute.Run(context);
-
-    if (execute.FoundPage)
-        await context.Response.WriteAsync(Result);
-    else
-        context.Response.StatusCode = 404;
-});
+app.UseCodeBehind();
 
 app.Run();
