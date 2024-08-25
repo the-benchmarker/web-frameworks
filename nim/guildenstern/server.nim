dd9cc72b3143860c9748b4ac899500eac59d898e
Marwan Rabbâa <waghanza@gmail.com>
2024-08-25 22:15:15 +0200

    update

--- nim/guildenstern/server.nim
+++ nim/guildenstern/server.nim
@@ -1,12 +1,21 @@
+# nim r -d:release -d:threadsafe server
+
 import guildenstern/[dispatcher, httpserver]
 
-proc handleGet(ctx: HttpContext) =
-  if ctx.isUri("/") or not ctx.isMethod("GET"): ctx.reply(Http200)
-  else:
-    let id = ctx.getUri()[6 .. ^1]
-    ctx.reply(id)
+const ThreadCount = 100
 
-var server = new GuildenServer
-server.initHeaderCtx(handleGet, 3000)
+proc handle() =
+  try:
+    if not isMethod("GET") or not startsUri("/user/"): reply(Http200)
+    else:
+      let id = getUri()[6 .. ^1]
+      reply(id)
+  except: reply(Http500)
+  
+proc run() =
+  let server = newHttpServer(handle, NONE, true, NoBody)
+  server.start(3000, ThreadCount, ThreadCount)
+  joinThread(server.thread)
 
-server.serve(loglevel = NONE)
+run()
+
