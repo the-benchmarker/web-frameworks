2d0e08b2d8b84f8deb0114eab31e3463d87ce82d
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-08-27 21:25:36 +0330

    Update HomeController.cs

--- csharp/codebehind/HomeController.cs
+++ csharp/codebehind/HomeController.cs
@@ -4,6 +4,7 @@ public partial class DefaultController : CodeBehindController
 {
     public void PageLoad(HttpContext context)
     {
+        IgnoreViewAndModel = true;
         Write(""); // path: /
     }
 }
