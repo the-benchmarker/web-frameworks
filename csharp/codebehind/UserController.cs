983f7fe1c6ecefe293294e67bbf7b0bb9583b17d
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-08-29 07:59:28 +0330

    Update CodeBehind Framework (#7705)
    * Update config.yaml
    
    * Update web.csproj
    
    * Update UserController.cs
    
    * Update HomeController.cs
    
    * Update Program.cs
    
    * Update UserController.cs
    
    * Update web.csproj
    
    * Update config.yaml
    
    * Update csharp/codebehind/config.yaml
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>
    
    * Update csharp/codebehind/web.csproj
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>
    
    ---------
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>

--- csharp/codebehind/UserController.cs
+++ csharp/codebehind/UserController.cs
@@ -4,8 +4,10 @@ public partial class user : CodeBehindController
 {
     public void PageLoad(HttpContext context)
     {
+        IgnoreViewAndModel = true;
+    
         if (Section.Count() > 0)
-            Write(Section.GetValue(0)); // path: /user/id
+            Write(Section.GetValue(0)); // path: /user/{id}
         else
             Write(""); // path: /user
     }
