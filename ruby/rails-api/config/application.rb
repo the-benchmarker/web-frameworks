b95cab93b7da2d622b27728444f7eed6adbc07f3
Daniil Zhuravlev <nakrovati@outlook.com>
2024-09-05 02:08:04 +0400

    chore: update the number of puma threads to 3

--- ruby/rails-api/config/application.rb
+++ ruby/rails-api/config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Benchmark
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
