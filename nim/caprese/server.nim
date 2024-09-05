d0a9376ae38736ce8290694c232603db207a3a6e
zenywallet <zenywallet@bitzeny.jp>
2024-09-05 09:44:34 +0900

    change url path tag

--- nim/caprese/server.nim
+++ nim/caprese/server.nim
@@ -11,7 +11,7 @@ config:
 server(ip = "0.0.0.0", port = 3000):
   routes:
     get "/": "".addActiveHeader("text").send
-    get startsWith("/user/"): reqUrl[6..^1].addHeader("text").send
+    get "/user/:id": id.addHeader("text").send
     post "/user": "".addActiveHeader("text").send
 
 serverStart()
