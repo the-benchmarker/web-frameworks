8d2455178e2714cb449a5c9ebf7c400a2e8f60eb
Mario <6395323+mario-huang@users.noreply.github.com>
2024-09-11 23:28:03 +0800

    no muti cpus

--- javascript/durian.js/app.ts
+++ javascript/durian.js/app.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
--- javascript/durian.js/cluster.ts
+++ javascript/durian.js/cluster.ts
@@ -1,9 +1,3 @@
-import os from "node:os";
+import { bootstrap } from "./app";
 
-const numCPUs = os.cpus().length;
-for (let i = 0; i < numCPUs; i++) {
-  Bun.spawn(["bun", "app.ts"], {
-    stdio: ["inherit", "inherit", "inherit"],
-    env: { ...process.env },
-  });
-}
+bootstrap();
