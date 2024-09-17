b63fd86437701f8b8f54fb4133524c777acae7be
Mario <6395323+mario-huang@users.noreply.github.com>
2024-09-17 14:03:07 +0800

    add durian.js config to use muti cores

--- javascript/durian.js/src/app.ts
+++ javascript/durian.js/src/app.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+bootstrap();
 
 
