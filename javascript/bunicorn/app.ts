d164c5997dfd7bfff2886b4165bbfebbcc992222
Marwan Rabbâa <waghanza@gmail.com>
2024-09-22 22:09:45 +0200

    update

--- javascript/bunicorn/app.ts
+++ javascript/bunicorn/app.ts
@@ -1,11 +1,11 @@
 import { Router, BunicornApp } from "@bunicorn/server";
 
 const app = new BunicornApp();
-const rb = new Router();
+const router = new Router();
 app.addRoutes([
-  rb.get("/", (ctx) => ctx.ok()),
-  rb.get("/user/:id", (ctx) => ctx.raw(ctx.params.id)),
-  rb.post("/user", (ctx) => ctx.ok()),
+  router.get("/", (ctx) => ctx.ok()),
+  router.get("/user/:id", (ctx) => ctx.raw(ctx.params.id)),
+  router.post("/user", (ctx) => ctx.ok()),
 ]);
 
 app.serve({
