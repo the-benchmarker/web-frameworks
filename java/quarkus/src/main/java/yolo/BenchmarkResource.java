5b9f1323c73a9058987aa03d82c5937b485522b2
Pramoth Suwanpech <pramoth@geniustree.co.th>
2024-09-24 03:26:03 +0700

    apply  Quarkus reactive mode

--- java/quarkus/src/main/java/yolo/BenchmarkResource.java
+++ java/quarkus/src/main/java/yolo/BenchmarkResource.java
@@ -1,33 +1,35 @@
 package yolo;
 
+
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 @Path("/")
 public class BenchmarkResource {
-
+    private static final Logger log = Logger.getLogger(BenchmarkResource.class);
     @GET
     @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
-    public String root() {
-        return "";
+    public Uni<String> root() {
+        return Uni.createFrom().item("");
     }
 
     @GET
     @Path("/user/{id}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String userId(String id) {
-        return id;
+    public Uni<String> userId(String id) {
+        return Uni.createFrom().item(id);
     }
 
     @POST
     @Path("/user")
     @Produces(MediaType.TEXT_PLAIN)
-    public String user() {
-        return "";
+    public Uni<String> user() {
+        return Uni.createFrom().item("");
     }
 }
