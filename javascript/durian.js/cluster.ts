86ebe59f3a130322b6e3ddb928ca1a0eef65a6bc
Mario <6395323+mario-huang@users.noreply.github.com>
2024-09-24 09:26:32 +0800

    try to use cluster in bun

--- javascript/durian.js/cluster.ts
+++ javascript/durian.js/cluster.ts
@@ -1,3 +1,17 @@
 import { bootstrap } from "./src/app";
+import cluster from 'cluster';
+import os from "node:os";
 
-bootstrap();
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  bootstrap();
+  console.log(`Worker ${process.pid} started`);
+}
+
