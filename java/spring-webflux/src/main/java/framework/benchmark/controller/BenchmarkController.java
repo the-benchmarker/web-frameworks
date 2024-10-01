fc92cfa554b14addcc59d7f1f4d591e2197913a3
nathphon.jeamjit <nathphon.j@javaverse.co.th>
2024-10-01 12:25:49 +0700

    Remove DATE and SERVER headers from responses
    Updated EMPTY_RESPONSE and user/{id} route to exclude DATE and SERVER headers. This change improves security by not exposing server information and standardizes response headers.

--- java/spring-webflux/src/main/java/framework/benchmark/controller/BenchmarkController.java
+++ java/spring-webflux/src/main/java/framework/benchmark/controller/BenchmarkController.java
@@ -15,14 +15,25 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class BenchmarkController {
 
     private static final CacheControl MAX_AGE_15_SECONDS = CacheControl.maxAge(Duration.ofSeconds(15));
-    private static final Mono<ServerResponse> EMPTY_RESPONSE = ServerResponse.ok().cacheControl(MAX_AGE_15_SECONDS).build();
+    private static final Mono<ServerResponse> EMPTY_RESPONSE = ServerResponse.ok()
+            .cacheControl(MAX_AGE_15_SECONDS)
+            .headers(h -> {
+                h.remove(HttpHeaders.DATE);
+                h.remove(HttpHeaders.SERVER);
+            })
+            .build();
 
 
     @Bean
     public RouterFunction<ServerResponse> routes() {
         return route()
                 .GET("/", request -> EMPTY_RESPONSE)
-                .GET("/user/{id}", request -> ServerResponse.ok().cacheControl(MAX_AGE_15_SECONDS).bodyValue(request.pathVariable("id")))
+                .GET("/user/{id}", request -> ServerResponse.ok()
+                        .cacheControl(MAX_AGE_15_SECONDS)
+                        .headers(h -> {
+                            h.remove(HttpHeaders.DATE);
+                            h.remove(HttpHeaders.SERVER);
+                        }).bodyValue(request.pathVariable("id")))
                 .POST("/user", request -> EMPTY_RESPONSE)
                 .build();
     }
