be7bfcd9b0fc952f9d15974a542d8e3b2bc83a78
Mario <6395323+mario-huang@users.noreply.github.com>
2024-10-01 13:54:10 +0800

    add durian.js-fastify

---
+++ javascript/durian.js-fastify/cluster.ts
@@ -0,0 +1,18 @@
+import cluster from 'cluster';
+import os from 'node:os';
+import { bootstrap } from './src/app';
+
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code ${code} and signal ${signal}`);
+    console.log('Starting a new worker...');
+    cluster.fork();
+  });
+} else {
+  bootstrap();
+  console.log(`Worker ${process.pid} started`);
+}
