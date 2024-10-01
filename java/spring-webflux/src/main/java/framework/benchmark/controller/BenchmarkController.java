cb98516082f283b580253caa3fd1998280be533d
nathphon.jeamjit <nathphon.j@javaverse.co.th>
2024-10-01 10:37:38 +0700

    Enable caching and optimize Netty server configuration
    Added a 15-second cache control to key GET endpoints in `BenchmarkController`. Enhanced Netty server settings for performance, including connection and idle timeouts, header validation, buffer size, and response compression.

--- java/spring-webflux/src/main/java/framework/benchmark/controller/BenchmarkController.java
+++ java/spring-webflux/src/main/java/framework/benchmark/controller/BenchmarkController.java
@@ -1,27 +1,28 @@
 package framework.benchmark.controller;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
+import org.springframework.http.CacheControl;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import java.time.Duration;
+
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Component
 public class BenchmarkController {
 
-    private static final Mono<ServerResponse> EMPTY_RESPONSE = ServerResponse.ok().build();
+    private static final CacheControl MAX_AGE_15_SECONDS = CacheControl.maxAge(Duration.ofSeconds(15));
+    private static final Mono<ServerResponse> EMPTY_RESPONSE = ServerResponse.ok().cacheControl(MAX_AGE_15_SECONDS).build();
+
 
     @Bean
     public RouterFunction<ServerResponse> routes() {
         return route()
                 .GET("/", request -> EMPTY_RESPONSE)
-                .GET("/user/{id}", request -> ServerResponse.ok().bodyValue(request.pathVariable("id")))
+                .GET("/user/{id}", request -> ServerResponse.ok().cacheControl(MAX_AGE_15_SECONDS).bodyValue(request.pathVariable("id")))
                 .POST("/user", request -> EMPTY_RESPONSE)
                 .build();
     }
