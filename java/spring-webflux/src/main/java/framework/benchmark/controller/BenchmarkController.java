dd2cebee3f57c8fba1343844beedc34a70a3f13e
nathphon.jeamjit <nathphon.j@javaverse.co.th>
2024-10-02 09:53:19 +0700

    Refactor imports in BenchmarkController
    Replaced CacheControl and Duration imports with HttpHeaders and MediaType imports. This change addresses unnecessary imports and streamlines the code dependencies.

--- java/spring-webflux/src/main/java/framework/benchmark/controller/BenchmarkController.java
+++ java/spring-webflux/src/main/java/framework/benchmark/controller/BenchmarkController.java
@@ -1,13 +1,12 @@
 package framework.benchmark.controller;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-
-import java.time.Duration;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
