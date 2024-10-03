d198e998f855acb8a0b4a455a57c38d2c6a4195a
Nathphon <nathphon.jeamjit@gmail.com>
2024-10-04 02:12:07 +0700

    Optimize Netty server configuration  (#7824)
    * Refactor controller to use functional routing
    
    Replaces the annotation-based controller with a functional routing approach. Introduces a `RouterFunction` bean to handle HTTP requests, eliminates redundant response creation, and improves efficiency by directly parsing path variables.
    
    * Refactor route definitions in BenchmarkRouter
    
    Simplified the route definitions by using lambda expressions directly in the RouterFunction. This reduces unnecessary method declarations and enhances readability.
    
    * Rename BenchmarkRouter class to BenchmarkController
    
    This change aligns the class name with its intended functionality. The class was previously named incorrectly, which could cause confusion. The new name clarifies its role as a controller within the Spring WebFlux framework.
    
    * Rename package from router to controller
    
    This change corrects a package naming inconsistency in the BenchmarkController class. The package was previously listed incorrectly as "router" and has been updated to "controller" to reflect its actual content and functionality.
    
    * Enable caching and optimize Netty server configuration
    
    Added a 15-second cache control to key GET endpoints in `BenchmarkController`. Enhanced Netty server settings for performance, including connection and idle timeouts, header validation, buffer size, and response compression.
    
    * Remove DATE and SERVER headers from responses
    
    Updated EMPTY_RESPONSE and user/{id} route to exclude DATE and SERVER headers. This change improves security by not exposing server information and standardizes response headers.
    
    * Simplify response headers handling for cache control
    
    Removed unnecessary DATE header manipulations and streamlined SERVER header removal in response preparation. Changed content type to TEXT_PLAIN and added shared caching logic for consistent behavior.
    
    * Remove HttpHeaders.CONNECTION and tweak server settings
    
    Removed HttpHeaders.CONNECTION for added security in response headers. Enabled HTTP/2 and adjusted connection and idle timeouts in application properties for optimized server performance. Changed root logging level from ERROR to WARN to capture more detailed logs.
    
    * Remove cache control from BenchmarkController
    
    This commit eliminates the `CacheControl` mechanism from the `BenchmarkController`. The `maxAge` duration and related caching properties were removed for both the empty response and the user-specific response. This change might impact performance and response handling in caching-sensitive environments.
    
    * Refactor imports in BenchmarkController
    
    Replaced CacheControl and Duration imports with HttpHeaders and MediaType imports. This change addresses unnecessary imports and streamlines the code dependencies.
    
    ---------
    
    Co-authored-by: nathphon.jeamjit <nathphon.j@javaverse.co.th>

--- java/spring-webflux/src/main/java/framework/benchmark/controller/BenchmarkController.java
+++ java/spring-webflux/src/main/java/framework/benchmark/controller/BenchmarkController.java
@@ -1,28 +1,40 @@
 package framework.benchmark.controller;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Component
 public class BenchmarkController {
 
-    private static final Mono<ServerResponse> EMPTY_RESPONSE = ServerResponse.ok().build();
+    private static final Mono<ServerResponse> EMPTY_RESPONSE = ServerResponse.ok()
+            .contentType(MediaType.TEXT_PLAIN)
+            .headers(h -> {
+                h.remove(HttpHeaders.SERVER);
+                h.remove(HttpHeaders.CONNECTION);
+            })
+            .build().share();
+
 
     @Bean
     public RouterFunction<ServerResponse> routes() {
         return route()
                 .GET("/", request -> EMPTY_RESPONSE)
-                .GET("/user/{id}", request -> ServerResponse.ok().bodyValue(request.pathVariable("id")))
+                .GET("/user/{id}", request -> ServerResponse.ok()
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .headers(h -> {
+                            h.remove(HttpHeaders.SERVER);
+                            h.remove(HttpHeaders.CONNECTION);
+                        })
+                        .bodyValue(request.pathVariable("id")))
                 .POST("/user", request -> EMPTY_RESPONSE)
                 .build();
     }
+
 }
