4e0e8d53f048120a7e306746e01ad7770d6ae7c7
whiplash <mfirry@hotmail.com>
2024-10-20 12:03:00 +0200

    [Scala]: Cask framework added (#7862)
    * [Scala] cask added
    
    * [Scala] cask fixed
    
    * Update build.properties
    
    sbt version bump
    
    ---------
    
    Co-authored-by: whiplash <whiplash@users.noreply.github.com>
    Co-authored-by: Marco F. <mfirry@users.noreply.github.com>

---
+++ scala/cask/build.sbt
@@ -0,0 +1,15 @@
+val CaskVersion = "0.9.4"
+
+name := "server"
+
+scalaVersion := "3.5.1"
+
+run / fork := true
+
+lazy val root = (project in file("."))
+  .settings(
+    libraryDependencies ++= Seq(
+	"com.lihaoyi" %% "cask" % CaskVersion
+    )
+  )
+  .enablePlugins(JavaAppPackaging)
---
+++ scala/cask/src/main/scala/Main.scala
@@ -0,0 +1,26 @@
+case class MinimalRoutes()(implicit cc: castor.Context, log: cask.Logger) extends cask.Routes{
+
+  @cask.get("/")
+  def hello() = {
+    ""
+  }
+
+  @cask.post("/user")
+  def postUser(request: cask.Request) = {
+    ""
+  }
+
+  @cask.get("/user/:name") // variable path segment, e.g. HOST/user/lihaoyi
+  def getUser(name: String) = {
+    name
+  }
+
+  initialize()
+}
+
+
+object MinimalRoutesMain extends cask.Main {
+  override val port = 3000
+
+  val allRoutes = Seq(MinimalRoutes())
+}
