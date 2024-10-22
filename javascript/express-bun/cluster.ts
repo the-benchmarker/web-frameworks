43573d55fc8d2483b67578c725fa0f555db8e33e
zahidcakici <cakici19@itu.edu.tr>
2024-10-22 15:46:23 +0300

    remove unnecessary log

--- javascript/express-bun/cluster.ts
+++ javascript/express-bun/cluster.ts
@@ -7,10 +7,6 @@ if (cluster.isMaster) {
   for (let i = 0; i < cpus; i++) {
     cluster.fork();
   }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
 } else {
   await import("./app.ts");
 }
