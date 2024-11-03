e5efae1894c2b24486743526eb5dd1a20a3ef674
donywan <donywan@outlook.com>
2024-11-04 03:18:00 +0800

    add oak support (#7919)

---
+++ javascript/oak-bun/app.ts
@@ -0,0 +1,22 @@
+import { Application, Router } from "@oak/oak";
+
+
+const app = new Application();
+
+const route = new Router();
+
+route.get("/", (ctx) => {
+    ctx.response.body = ''
+})
+
+route.get("/user/:id", (ctx) => {
+    ctx.response.body = ctx.params.id
+})
+
+route.get("/user", (ctx) => {
+    ctx.response.body = ''
+})
+
+app.use(route.routes());
+
+app.listen({ port: 3000 });
