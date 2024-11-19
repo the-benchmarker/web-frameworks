3fe80882af1f87543e9a861f12818470956c9c6c
Ilya Nemtsev <ilya.nemtsev@outlook.com>
2024-11-20 01:12:35 +0700

    upgrade ktor and cleanup tests (#7968)

--- kotlin/ktor/build.gradle.kts
+++ kotlin/ktor/build.gradle.kts
@@ -23,8 +23,6 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
 ktor {
