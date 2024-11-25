515bebd2e456203a90ca73be3873e7e564d9228c
Andreas Nägeli <andr.naegeli@gmail.com>
2024-11-25 10:48:56 +0100

    Convert /user to POST

--- csharp/genhttp/Program.cs
+++ csharp/genhttp/Program.cs
@@ -6,7 +6,7 @@ var empty = "";
 var app = Inline.Create()
                 .Get(() => empty)
                 .Get("/user/:id", (string id) => id)
-                .Get("/user", () => empty);
+                .Post("/user", () => empty);
 
 return await Host.Create()
                  .Handler(app)
