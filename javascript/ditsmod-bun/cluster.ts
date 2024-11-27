1f25393125519434022abb575c513b90440b02fc
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-11-27 18:04:02 +0200

    fix(javascript/ditsmod-bun): fixed cluster mode for ditsmod-bun.

--- javascript/ditsmod-bun/cluster.ts
+++
@@ -1,9 +0,0 @@
-import os from 'node:os';
-
-const numCPUs = os.availableParallelism();
-for (let i = 0; i < numCPUs; i++) {
-  Bun.spawn(['bun', 'dist/main.js'], {
-    stdio: ['inherit', 'inherit', 'inherit'],
-    env: { ...process.env },
-  });
-}
--- javascript/ditsmod-bun/src/main.ts
+++ javascript/ditsmod-bun/src/main.ts
@@ -1,8 +1,18 @@
-import { Application } from '@ditsmod/core';
+import cluster from 'node:cluster';
 import type { ServerOptions } from 'node:http';
+import { availableParallelism } from 'node:os';
+import { Application } from '@ditsmod/core';
 
 import { AppModule } from './app/app.module.js';
 
-const serverOptions: ServerOptions = { keepAlive: true, keepAliveTimeout: 0 };
-const app = await new Application().bootstrap(AppModule, { serverOptions });
-app.server.listen(3000, '0.0.0.0');
+const numCpus = availableParallelism();
+
+if (numCpus > 1 && cluster.isPrimary) {
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+  }
+} else {
+  const serverOptions: ServerOptions = { keepAlive: true, keepAliveTimeout: 0 };
+  const app = await new Application().bootstrap(AppModule, { serverOptions });
+  app.server.listen(3000, '0.0.0.0');
+}
