a0310eac5decded614abfacaa1abd9bc7341f632
Matteo Collina <matteo.collina@gmail.com>
2024-12-11 12:50:53 +0100

    Add content-type parser to Fastify (#8039)
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- javascript/fastify/app.js
+++ javascript/fastify/app.js
@@ -11,6 +11,11 @@ app.get('/user/:id', function (request, reply) {
   reply.send(request.params.id);
 });
 
+app.addContentTypeParser('application/x-www-form-urlencoded', function (req, body, done) {
+  // The incoming request in the benchmark is empty anyway
+  done();
+});
+
 app.post('/user', function (request, reply) {
   reply.send();
 });
