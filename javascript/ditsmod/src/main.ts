4c0629a1ead11589aa4ef7897732952cd9a13237
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-24 20:00:49 +0200

    chore(JavaScript/ditsmod): upgrade to v3.

--- javascript/ditsmod/src/main.ts
+++ javascript/ditsmod/src/main.ts
@@ -4,5 +4,5 @@ import type { ServerOptions } from 'node:http';
 import { AppModule } from './app/app.module.js';
 
 const serverOptions: ServerOptions = { keepAlive: true, keepAliveTimeout: 0 };
-const app = await new Application().bootstrap(AppModule, { serverOptions });
+const app = await Application.create(AppModule, { serverOptions });
 app.server.listen(3000, '0.0.0.0');
