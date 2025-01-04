9dd00bdd532b8d98a5d3765dfd7a4f836b4a12ac
Marwan Rabbâa <waghanza@gmail.com>
2025-01-04 19:04:11 +0100

    remove cluster.ts for bun

--- javascript/vixeny-bun/app.ts
+++ javascript/vixeny-bun/app.ts
@@ -1,26 +1,5 @@
-import { wrap } from "vixeny";
+const app = await import('./server.ts')
 
-const app  = wrap()()
-  .stdPetition({
-    path: '/',
-    f: () => null
-  })
-  .stdPetition({
-    path: '/user/:id',
-    param: {
-      unique: true
-    },
-    f: ctx => ctx.param
-  })
-  .stdPetition({
-    method: 'POST',
-    path: '/user',
-    f: () => null
-  })
-  .compose()
-
-export default { 
-    port: 3000,
-    fetch: app,
-    reusePort: true
-} 
+Bun.serve({
+  fetch: await app.compose(),
+});
