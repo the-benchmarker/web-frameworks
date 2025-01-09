65aa4834267aadc3363f0aaa42910f60bec188ce
Marwan Rabbâa <waghanza@gmail.com>
2025-01-09 21:51:21 +0100

    update

--- rust/axum/src/main.rs
+++ rust/axum/src/main.rs
@@ -7,7 +7,10 @@ async fn main() {
     let router: Router = Router::new()
         .route("/", get(|| async {}))
         .route("/user", post(|| async {}))
-        .route("/user/{id}", get(|Path(id): Path<String>| async move { id }));
+        .route(
+            "/user/{id}",
+            get(|Path(id): Path<String>| async move { id }),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router.into_make_service())
