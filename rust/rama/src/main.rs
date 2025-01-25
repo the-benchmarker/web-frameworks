281056445124fd248b719fe0db4ef9a4d0556d3e
Marwan Rabbâa <waghanza@gmail.com>
2025-01-25 22:10:09 +0100

    Update rust/rama/src/main.rs

--- rust/rama/src/main.rs
+++ rust/rama/src/main.rs
@@ -1,16 +1,33 @@
 use rama::extract::Path;
-use rama::routing::{get, post};
-use rama::Router;
+use rama::{
+    http::{
+        matcher::HttpMatcher,
+        server::HttpServer,
+        service::web::{extract::Path, match_service},
+        StatusCode,
+    },
+    rt::Executor,
+};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct GetUserParams {
+    #[expect(unused)]
+    id: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let router: Router = Router::new()
-        .route("/", get(|| async {}))
-        .route("/user", post(|| async {}))
-        .route("/user/{id}", get(|Path(id): Path<String>| async move { id }));
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    rama::serve(listener, router.into_make_service())
+    HttpServer::auto(Executor::default())
+        .listen(
+            "0.0.0.0:3000",
+            match_service!{
+                HttpMatcher::get("/") => StatusCode::OK,
+                HttpMatcher::post("/user") => StatusCode::OK,
+                HttpMatcher::get("/user/:id") => |Path(_): Path<GetUserParams>| async move { StatusCode::OK },
+                _ => StatusCode::NOT_FOUND,
+            }
+        )
         .await
         .unwrap();
 }
