484339adfefb226304c64f5c7d3e01d01333add0
Andreas Nägeli <andr.naegeli@gmail.com>
2025-01-28 20:26:18 +0100

    Update to GenHTTP 9.6 and switch to basic handler (#8190)
    * Update to GenHTTP 9.6 and switch to basic handler
    
    * Switch package references to wildcard on bugfix level
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>
    
    ---------
    
    Co-authored-by: Marwan Rabbâa <waghanza@gmail.com>

---
+++ csharp/genhttp/BenchmarkHandler.cs
@@ -0,0 +1,51 @@
+﻿using GenHTTP.Api.Content;
+using GenHTTP.Api.Protocol;
+
+using Strings = GenHTTP.Modules.IO.Strings;
+
+namespace web;
+
+internal class BenchmarkHandler : IHandler
+{
+    private static readonly FlexibleContentType _TextType = FlexibleContentType.Get(ContentType.TextPlain);
+
+    private static readonly Strings.StringContent _EmptyContent = new("");
+
+    public ValueTask PrepareAsync() => new();
+
+    public ValueTask<IResponse> HandleAsync(IRequest request)
+    {
+        IResponse response = null;
+
+        var target = request.Target;
+
+        if (target.Ended)
+        {
+            response = GetEmptyResponse(request);
+        }
+        else if (target.Current.Original == "user")
+        {
+            target.Advance();
+
+            if (target.Ended)
+            {
+                response = GetEmptyResponse(request);
+            }
+            else
+            {
+                response = GetEmptyResponse(request, new Strings.StringContent(target.Current.Original));
+            }
+        }
+
+        return new(response);
+    }
+
+    private static IResponse GetEmptyResponse(IRequest request, Strings.StringContent content = null)
+    {
+        return request.Respond()
+                      .Type(_TextType)
+                      .Content(content ?? _EmptyContent)
+                      .Build();
+    } 
+
+}
