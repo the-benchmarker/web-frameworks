54592a4f9d249981f5fbf605daedc3e65ad39e35
Marwan Rabbâa <waghanza@gmail.com>
2025-01-28 20:33:57 +0100

    update

--- javascript/mapljs-bun/cluster.mjs
+++ javascript/mapljs-bun/cluster.mjs
@@ -2,12 +2,11 @@ import cluster from 'node:cluster';
 import { availableParallelism } from 'node:os';
 
 const numCpus = availableParallelism();
-import app from './app.js';
 
 if (numCpus > 1 && cluster.isPrimary) {
   for (let i = 0; i < numCpus; i++) {
     cluster.fork();
   }
 } else {
-  app.serve(3000);
+  await import(`./${process.env.NODE_APP}`);
 }
