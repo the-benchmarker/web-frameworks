d7114b5d5f1fd53afa302aeeb4441eb5efa37b0c
pim <pimnation@gmail.com>
2025-02-02 10:29:46 -0500

    🐦 Falco v5.x (#8203)

--- fsharp/falco/Program.fs
+++ fsharp/falco/Program.fs
@@ -1,39 +1,22 @@
-module Program 
+module Program
 
 open Falco
-open Falco.HostBuilder
 open Falco.Routing
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Logging
 
-let configureLogging (log : ILoggingBuilder) =
-    log.ClearProviders()
-    |> ignore
+[<EntryPoint>]
+let main args =
+    let bldr = WebApplication.CreateBuilder(args)
+    bldr.Logging.ClearProviders() |> ignore
 
-let configureServices (services : IServiceCollection) =
-    services.AddFalco() 
-    |> ignore
+    let wapp = bldr.Build()
 
-let configure (endpoints : HttpEndpoint list) (app : IApplicationBuilder) = 
-    app.UseFalco(endpoints)       
-    |> ignore 
-    
-let configureWebHost (endpoints : HttpEndpoint list) (webHost : IWebHostBuilder) =    
-    webHost        
-        .UseKestrel(fun k -> k.AddServerHeader <- false)
-        .ConfigureLogging(configureLogging)
-        .ConfigureServices(configureServices)
-        .Configure(configure endpoints)        
-
-let args = System.Environment.GetCommandLineArgs()
-
-webHost args {
-    configure configureWebHost
-    endpoints [
-        get  "/user/{id}" (Request.mapRoute (fun route -> route.Get "id" "") Response.ofPlainText)
-        post "/user"      (Response.ofEmpty)
-        get  "/"          (Response.ofEmpty)
-    ]
-}    
+    wapp.UseRouting()
+        .UseFalco([
+            mapGet  "/user/{id}" (fun r -> r.GetString "id") Response.ofPlainText
+            post "/user" Response.ofEmpty
+            get  "/" Response.ofEmpty
+        ])
+        .Run()
+    0
