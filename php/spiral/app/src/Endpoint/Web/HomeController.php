21cf3816257c92a6c69d016aaf36f77e482099df
Aleksei Gagarin <roxblnfk@ya.ru>
2025-02-14 21:43:50 +0400

    Remove unused files; configure RoadRunner and app (#8228)

--- php/spiral/app/src/Endpoint/Web/HomeController.php
+++ php/spiral/app/src/Endpoint/Web/HomeController.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Endpoint\Web;
 
-use Exception;
-use Spiral\Prototype\Traits\PrototypeTrait;
 use Spiral\Router\Annotation\Route;
 
 /**
@@ -14,13 +12,6 @@ use Spiral\Router\Annotation\Route;
  */
 final class HomeController
 {
-    /**
-     * Read more about Prototyping:
-     * @link https://spiral.dev/docs/basics-prototype/#installation
-     */
-    use PrototypeTrait;
-
-
     #[Route(route: '/', name: 'index', methods: "GET")]
     public function index(): string
     {
