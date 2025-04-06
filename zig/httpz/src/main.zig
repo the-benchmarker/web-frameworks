3d5e814560ada8fda5fbc0ea15f1cf2473cdf068
Kayden1412 <143221653+Kayden1412@users.noreply.github.com>
2025-04-07 05:48:02 +0700

    feat(httpz): update to zig 0.14

--- zig/httpz/src/main.zig
+++ zig/httpz/src/main.zig
@@ -8,25 +8,25 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     const cpu_count = try std.Thread.getCpuCount();
 
-    var server = try httpz.Server().init(allocator, .{ .address = "0.0.0.0", .port = 3000, .workers = .{
-        .count = @truncate(cpu_count / 2),
+    var server = try httpz.Server(void).init(allocator, .{ .address = "0.0.0.0", .port = 3000, .workers = .{
+        .count = @truncate(cpu_count),
         .max_conn = 8192,
         .retain_allocated_bytes = 0,
     }, .thread_pool = .{
-        .count = @truncate(cpu_count / 2),
-        .buffer_size = 1024,
+        .count = @truncate(cpu_count),
     }, .request = .{
         .max_param_count = 1,
         .max_query_count = 0,
         .max_form_count = 0,
         .max_multiform_count = 0,
-    } });
+    } }, {});
 
-    var router = server.router();
+    var router = try server.router(.{});
 
-    router.get("/", empty_handler);
-    router.post("/user", empty_handler);
-    router.get("/user/:id", user_id_handler);
+    router.get("/", empty_handler, .{});
+    router.post("/user/", empty_handler, .{});
+    router.get("/user/:id", user_id_handler, .{});
+
     try server.listen();
 }
 
