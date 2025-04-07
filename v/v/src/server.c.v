a2e4f3b991c001e15f0a9f1e7ed1b775dcd4a6db
Hitalo Souza <enghitalo@gmail.com>
2025-04-07 19:17:53 +0100

    update (#8370)

--- v/v/src/server.c.v
+++ v/v/src/server.c.v
@@ -12,182 +12,134 @@
 // - Mutex Locking: The server uses mutex locks to manage access to shared resources, ensuring thread safety while minimizing contention.
 module main
 
-import sync
+import runtime
 
-const tiny_bad_request_response = 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n'.bytes()
+const max_connection_size = 1024
+const max_thread_pool_size = runtime.nr_cpus()
+pub const tiny_bad_request_response = 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n'.bytes()
+const status_444_response = 'HTTP/1.1 444 No Response\r\nContent-Length: 0\r\nConnection: close\r\n\r\n'.bytes()
+const status_499_response = 'HTTP/1.1 499 Client Closed Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n'.bytes()
 
-$if windows {
-	#include <winsock2.h>
-} $else {
-	#include <arpa/inet.h>
-}
 #include <fcntl.h>
-#include <sys/epoll.h>
 #include <errno.h>
-#include <stdio.h>
-#include <netinet/in.h>
 
-fn C.socket(__domain int, __type int, __protocol int) int
+$if !windows {
+	#include <sys/epoll.h>
+	#include <netinet/in.h>
+}
 
-fn C.bind(sockfd int, addr &Addr, addrlen u32) int
-
+fn C.socket(socket_family int, socket_type int, protocol int) int
+fn C.bind(sockfd int, addr &C.sockaddr_in, addrlen u32) int
 fn C.send(__fd int, __buf voidptr, __n usize, __flags int) int
-
 fn C.recv(__fd int, __buf voidptr, __n usize, __flags int) int
-
 fn C.setsockopt(__fd int, __level int, __optname int, __optval voidptr, __optlen u32) int
-
 fn C.listen(__fd int, __n int) int
+fn C.perror(s &u8)
+fn C.close(fd int) int
+fn C.accept(sockfd int, address &C.sockaddr_in, addrlen &u32) int
+fn C.htons(__hostshort u16) u16
+fn C.epoll_create1(__flags int) int
+fn C.epoll_ctl(__epfd int, __op int, __fd int, __event &C.epoll_event) int
+fn C.epoll_wait(__epfd int, __events &C.epoll_event, __maxevents int, __timeout int) int
+fn C.fcntl(fd int, cmd int, arg int) int
 
-struct In_addr {
-	s_addr int
+struct C.in_addr {
+	s_addr u32
 }
 
-enum In_port_t as u16 {
-	ipproto_hopopts  = 0
-	ipproto_routing  = 43
-	ipproto_fragment = 44
-	ipproto_icmpv6   = 58
-	ipproto_none     = 59
-	ipproto_dstopts  = 60
-	ipproto_mh       = 135
-}
-
-struct Sockaddr_in {
+struct C.sockaddr_in {
 	sin_family u16
 	sin_port   u16
-	sin_addr   In_addr
+	sin_addr   C.in_addr
 	sin_zero   [8]u8
 }
 
-fn C.htons(__hostshort u16) u16
-
-@[typedef]
 union C.epoll_data {
-mut:
 	ptr voidptr
 	fd  int
 	u32 u32
 	u64 u64
 }
 
-pub struct C.epoll_event {
+struct C.epoll_event {
 	events u32
 	data   C.epoll_data
 }
 
-fn C.epoll_create1(__flags int) int
-
-fn C.epoll_ctl(__epfd int, __op int, __fd int, __event &C.epoll_event) int
-
-fn C.epoll_wait(__epfd int, __events &C.epoll_event, __maxevents int, __timeout int) int
-
-struct Server {
-mut:
-	server_socket   int
-	epoll_fd        int
-	lock_flag       sync.Mutex
-	has_clients     int
-	threads         [max_thread_pool_size]thread
-	request_handler fn (HttpRequest) ![]u8 @[required]
-}
-
-fn C.fcntl(fd int, cmd int, arg int) int
-fn C.perror(s &u8) voidptr
-fn C.close(fd int) int
-fn C.accept(sockfd int, address &C.sockaddr_in, addrlen &u32) int
-
-const sock_stream = C.SOCK_STREAM
-const sock_nonblock = C.SOCK_NONBLOCK
-
-const max_unix_path = 108
-
-const max_connection_size = 1024
-
-@[_pack: '1']
-pub struct Ip6 {
-	port      u16
-	flow_info u32
-	addr      [16]u8
-	scope_id  u32
-}
-
-@[_pack: '1']
-pub struct Ip {
-	port    u16
-	addr    [4]u8
-	sin_pad [8]u8
-}
-
-pub struct Unix {
-	path [max_unix_path]u8
-}
-
-union AddrData {
-	Unix
-	Ip
-	Ip6
-}
-
-@[_pack: '1']
-pub struct Addr {
+pub struct Server {
 pub:
-	f    u16
-	addr AddrData
+	port int = 3000
+pub mut:
+	socket_fd       int
+	epoll_fds       []int    = []int{len: max_thread_pool_size, cap: max_thread_pool_size}
+	threads         []thread = []thread{len: max_thread_pool_size, cap: max_thread_pool_size}
+	request_handler fn ([]u8, int) ![]u8 @[required]
 }
 
 fn set_blocking(fd int, blocking bool) {
 	flags := C.fcntl(fd, C.F_GETFL, 0)
 	if flags == -1 {
+		eprintln(@LOCATION)
 		return
 	}
-	if blocking {
-		C.fcntl(fd, C.F_SETFL, flags & ~C.O_NONBLOCK)
-	} else {
-		C.fcntl(fd, C.F_SETFL, flags | C.O_NONBLOCK)
-	}
+	new_flags := if blocking { flags & ~C.O_NONBLOCK } else { flags | C.O_NONBLOCK }
+	C.fcntl(fd, C.F_SETFL, new_flags)
+}
+
+fn close_socket(fd int) {
+	C.close(fd)
 }
 
 fn create_server_socket(port int) int {
-	server_fd := C.socket(2, sock_stream | sock_nonblock, 0)
+	server_fd := C.socket(C.AF_INET, C.SOCK_STREAM, 0)
 	if server_fd < 0 {
 		eprintln(@LOCATION)
 		C.perror('Socket creation failed'.str)
-		return -1
+		exit(1)
 	}
 
-	// Enable SO_REUSEPORT
+	set_blocking(server_fd, false)
+
 	opt := 1
 	if C.setsockopt(server_fd, C.SOL_SOCKET, C.SO_REUSEPORT, &opt, sizeof(opt)) < 0 {
 		eprintln(@LOCATION)
 		C.perror('setsockopt SO_REUSEPORT failed'.str)
-		C.close(server_fd)
-		return -1
+		close_socket(server_fd)
+		exit(1)
 	}
 
-	server_addr := Sockaddr_in{
-		sin_family: 2 // ip
+	server_addr := C.sockaddr_in{
+		sin_family: u16(C.AF_INET)
 		sin_port:   C.htons(port)
-		sin_addr:   In_addr{C.INADDR_ANY}
+		sin_addr:   C.in_addr{u32(C.INADDR_ANY)}
 		sin_zero:   [8]u8{}
 	}
 
-	if C.bind(server_fd, voidptr(&server_addr), sizeof(server_addr)) < 0 {
+	if C.bind(server_fd, &server_addr, sizeof(server_addr)) < 0 {
 		eprintln(@LOCATION)
 		C.perror('Bind failed'.str)
-		C.close(server_fd)
-		return -1
+		close_socket(server_fd)
+		exit(1)
 	}
+
 	if C.listen(server_fd, max_connection_size) < 0 {
 		eprintln(@LOCATION)
 		C.perror('Listen failed'.str)
-		C.close(server_fd)
-		return -1
+		close_socket(server_fd)
+		exit(1)
 	}
+
 	return server_fd
 }
 
-// Function to add a file descriptor to the epoll instance
+fn create_epoll_fd() int {
+	epoll_fd := C.epoll_create1(0)
+	if epoll_fd < 0 {
+		C.perror('epoll_create1'.str)
+	}
+	return epoll_fd
+}
+
 fn add_fd_to_epoll(epoll_fd int, fd int, events u32) int {
 	mut ev := C.epoll_event{
 		events: events
@@ -201,105 +153,149 @@ fn add_fd_to_epoll(epoll_fd int, fd int, events u32) int {
 	return 0
 }
 
-// Function to remove a file descriptor from the epoll instance
 fn remove_fd_from_epoll(epoll_fd int, fd int) {
 	C.epoll_ctl(epoll_fd, C.EPOLL_CTL_DEL, fd, C.NULL)
 }
 
-fn handle_accept(server &Server) {
+fn handle_accept_loop(mut server Server, main_epoll_fd int) {
+	mut next_worker := 0
+	mut event := C.epoll_event{}
+
 	for {
-		client_fd := C.accept(server.server_socket, C.NULL, C.NULL)
-		if client_fd < 0 {
-			// Check for EAGAIN or EWOULDBLOCK, usually represented by errno 11.
-			if C.errno == C.EAGAIN || C.errno == C.EWOULDBLOCK {
-				break // No more incoming connections; exit loop.
+		num_events := C.epoll_wait(main_epoll_fd, &event, 1, -1)
+		if num_events < 0 {
+			if C.errno == C.EINTR {
+				continue
 			}
-
-			eprintln(@LOCATION)
-			C.perror('Accept failed'.str)
-			return
+			C.perror('epoll_wait'.str)
+			break
 		}
 
-		// Set the client socket to non-blocking mode if accepted successfully
-		set_blocking(client_fd, false)
-		unsafe {
-			server.lock_flag.lock()
-			if add_fd_to_epoll(server.epoll_fd, client_fd, u32(C.EPOLLIN | C.EPOLLET)) == -1 {
-				C.close(client_fd)
-			}
-			server.lock_flag.unlock()
-		}
-	}
-}
-
-fn handle_client_closure(server &Server, client_fd int) {
-	unsafe {
-		server.lock_flag.lock()
-		remove_fd_from_epoll(client_fd, client_fd)
-		server.lock_flag.unlock()
-	}
-}
-
-fn process_events(server &Server) {
-	events := [max_connection_size]C.epoll_event{}
-	num_events := C.epoll_wait(server.epoll_fd, &events[0], max_connection_size, -1)
-	for i := 0; i < num_events; i++ {
-		if events[i].events & u32((C.EPOLLHUP | C.EPOLLERR)) != 0 {
-			handle_client_closure(server, unsafe { events[i].data.fd })
+		if num_events > 1 {
+			eprintln('More than one event in epoll_wait, this should not happen.')
 			continue
 		}
-		if events[i].events & u32(C.EPOLLIN) != 0 {
-			request_buffer := [140]u8{}
-			bytes_read := C.recv(unsafe { events[i].data.fd }, &request_buffer[0], 140 - 1,
-				0)
-			if bytes_read > 0 {
-				mut readed_request_buffer := []u8{cap: bytes_read}
 
-				unsafe {
-					readed_request_buffer.push_many(&request_buffer[0], bytes_read)
-				}
-
-				decoded_http_request := decode_http_request(readed_request_buffer) or {
-					eprintln('Error decoding request ${err}')
-					C.send(unsafe { events[i].data.fd }, tiny_bad_request_response.data,
-						tiny_bad_request_response.len, 0)
-					handle_client_closure(server, unsafe { events[i].data.fd })
+		if event.events & u32(C.EPOLLIN) != 0 {
+			for {
+				client_conn_fd := C.accept(server.socket_fd, C.NULL, C.NULL)
+				if client_conn_fd < 0 {
+					// Check for EAGAIN or EWOULDBLOCK, usually represented by errno 11.
+					if C.errno == C.EAGAIN || C.errno == C.EWOULDBLOCK {
+						break // No more incoming connections; exit loop.
+					}
+					eprintln(@LOCATION)
+					C.perror('Accept failed'.str)
 					continue
 				}
-
-				// This lock is a workaround for avoiding race condition in router.params
-				// This slows down the server, but it's a temporary solution
-				(*server).lock_flag.lock()
-				response_buffer := (*server).request_handler(decoded_http_request) or {
-					eprintln('Error handling request ${err}')
-					C.send(unsafe { events[i].data.fd }, tiny_bad_request_response.data,
-						tiny_bad_request_response.len, 0)
-					handle_client_closure(server, unsafe { events[i].data.fd })
-					(*server).lock_flag.unlock()
+				set_blocking(client_conn_fd, false)
+				// Load balance the client connection to the worker threads.
+				// this is a simple round-robin approach.
+				epoll_fd := server.epoll_fds[next_worker]
+				next_worker = (next_worker + 1) % max_thread_pool_size
+				if add_fd_to_epoll(epoll_fd, client_conn_fd, u32(C.EPOLLIN | C.EPOLLET)) < 0 {
+					close_socket(client_conn_fd)
 					continue
 				}
-				(*server).lock_flag.unlock()
-
-				C.send(unsafe { events[i].data.fd }, response_buffer.data, response_buffer.len,
-					0)
-				handle_client_closure(server, unsafe { events[i].data.fd })
-			} else if bytes_read == 0
-				|| (bytes_read < 0 && C.errno != C.EAGAIN && C.errno != C.EWOULDBLOCK) {
-				handle_client_closure(server, unsafe { events[i].data.fd })
 			}
 		}
 	}
 }
 
-fn worker_thread(server &Server) {
+fn process_events(mut server Server, epoll_fd int) {
+	mut events := [max_connection_size]C.epoll_event{}
+
 	for {
-		process_events(server)
+		num_events := C.epoll_wait(epoll_fd, &events[0], max_connection_size, -1)
+		if num_events < 0 {
+			if C.errno == C.EINTR {
+				continue
+			}
+			C.perror('epoll_wait'.str)
+			break
+		}
+
+		for i in 0 .. num_events {
+			client_conn_fd := unsafe { events[i].data.fd }
+			if events[i].events & u32(C.EPOLLHUP | C.EPOLLERR) != 0 {
+				remove_fd_from_epoll(epoll_fd, client_conn_fd)
+				close_socket(client_conn_fd)
+				continue
+			}
+
+			if events[i].events & u32(C.EPOLLIN) != 0 {
+				request_buffer := [140]u8{}
+				bytes_read := C.recv(client_conn_fd, &request_buffer[0], request_buffer.len,
+					0)
+				if bytes_read <= 0 {
+					if bytes_read == 0 || (C.errno != C.EAGAIN && C.errno != C.EWOULDBLOCK) {
+						remove_fd_from_epoll(epoll_fd, client_conn_fd)
+						close_socket(client_conn_fd)
+					}
+					continue
+				}
+
+				mut readed_request_buffer := []u8{cap: bytes_read}
+				unsafe { readed_request_buffer.push_many(&request_buffer[0], bytes_read) }
+
+				response_buffer := server.request_handler(readed_request_buffer, client_conn_fd) or {
+					eprintln('Error handling request ${err}')
+					C.send(client_conn_fd, tiny_bad_request_response.data, tiny_bad_request_response.len,
+						0)
+					remove_fd_from_epoll(epoll_fd, client_conn_fd)
+					close_socket(client_conn_fd)
+					continue
+				}
+
+				sent := C.send(client_conn_fd, response_buffer.data, response_buffer.len,
+					C.MSG_NOSIGNAL | C.MSG_ZEROCOPY)
+				if sent < 0 && C.errno != C.EAGAIN && C.errno != C.EWOULDBLOCK {
+					remove_fd_from_epoll(epoll_fd, client_conn_fd)
+					close_socket(client_conn_fd)
+				}
+			}
+		}
 	}
-	return
 }
 
-fn event_loop(server &Server) {
-	for {
-		handle_accept(server)
+pub fn (mut server Server) run() {
+	$if windows {
+		eprintln('Windows is not supported yet. Please, use WSL or Linux.')
+		exit(1)
 	}
+
+	server.socket_fd = create_server_socket(server.port)
+	if server.socket_fd < 0 {
+		return
+	}
+
+	main_epoll_fd := create_epoll_fd()
+	if main_epoll_fd < 0 {
+		close_socket(server.socket_fd)
+		exit(1)
+	}
+
+	if add_fd_to_epoll(main_epoll_fd, server.socket_fd, u32(C.EPOLLIN)) < 0 {
+		close_socket(server.socket_fd)
+		close_socket(main_epoll_fd)
+		exit(1)
+	}
+
+	for i in 0 .. max_thread_pool_size {
+		server.epoll_fds[i] = create_epoll_fd()
+		if server.epoll_fds[i] < 0 {
+			C.perror('epoll_create1'.str)
+			for j in 0 .. i {
+				close_socket(server.epoll_fds[j])
+			}
+			close_socket(main_epoll_fd)
+			close_socket(server.socket_fd)
+			exit(1)
+		}
+
+		server.threads[i] = spawn process_events(mut server, server.epoll_fds[i])
+	}
+
+	println('listening on http://localhost:${server.port}/')
+	handle_accept_loop(mut server, main_epoll_fd)
 }
