4d2d695bda873430126710c8a991b24741c0f31b
Marwan Rabbâa <waghanza@gmail.com>
2025-04-27 16:00:45 +0200

    fix vixeny

--- javascript/vixeny-deno/app.ts
+++ javascript/vixeny-deno/app.ts
@@ -1,20 +1,3 @@
-import { wrap } from "jsr:@vixeny/core@0.1.42";
+import app from "./server.ts";
 
-export default wrap()()
-  .get({
-    path: "/",
-    f: () => null,
-  })
-  .get({
-    path: "/id/:id",
-    param: {
-      unique: true,
-    },
-    f: (f) => f.param,
-  })
-  .post({
-    path: "/user",
-    f: () => null,
-  });
-
-Deno.serve({ fetch: await root.compose(), port: 3000 }, app);
+export default { fetch: await app.compose() }
