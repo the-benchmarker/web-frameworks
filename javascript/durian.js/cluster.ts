f43b3d4d19e2f748755054b7c51e1dd0a1ce408e
Marwan Rabbâa <waghanza@gmail.com>
2025-04-28 08:01:11 +0200

    New release (last before results closing #8229) (#8413)
    * update
    
    * update lihil and responder
    
    * fix vixeny
    
    * update

--- javascript/durian.js/cluster.ts
+++ javascript/durian.js/cluster.ts
@@ -3,7 +3,7 @@ import { availableParallelism } from 'node:os';
 const numCpus = availableParallelism();
 
 for (let i = 0; i < numCpus; i++) {
-  Bun.spawn(['bun', 'app.ts'], {
+  Bun.spawn(['bun', 'src/main.ts'], {
     stdio: ['inherit', 'inherit', 'inherit'],
     env: { ...process.env },
   });
--- javascript/express-deno/app.ts
+++ javascript/express-deno/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { createFetchHandler } from "./fetcher.ts"; // path to the code above
 const app = express();
 app.set("etag", false);
 
@@ -14,4 +15,6 @@ app.post("/user", function (req, res) {
   res.send("");
 });
 
-export default app;
+const fetchHandler = createFetchHandler(app);
+
+export default { fetch: fetchHandler };
