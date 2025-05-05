faeae299a17bbd4c43f01fc6a63753bd4461ff5a
Hitalo Souza <enghitalo@gmail.com>
2025-05-05 23:57:28 +0100

    vanilla

--- v/v/src/controllers.v
+++ v/v/src/controllers.v
@@ -1,6 +1,7 @@
 module main
 
 import strings
+import enghitalo.vanilla.http_server
 
 const http_ok_response = 'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 0\r\nConnection: keep-alive\r\n\r\n'.bytes()
 
@@ -17,7 +18,7 @@ fn get_users_controller(params []string) ![]u8 {
 @[direct_array_access; manualfree]
 fn get_user_controller(params []string) ![]u8 {
 	if params.len == 0 {
-		return tiny_bad_request_response
+		return http_server.tiny_bad_request_response
 	}
 	id := params[0]
 	response_body := id
