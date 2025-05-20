709ae88bbbb2e35bfaf5c8c0017d47a6c05624cc
Zava <693320+zavakid@users.noreply.github.com>
2025-05-21 04:04:02 +0800

    Update Rust crate micro-web to 0.1 (#8436)
    * Update Rust crate micro-web to 0.1
    
    * chore: Update Rust crate micro-web listens on 0.0.0.0

--- rust/micro-web/src/main.rs
+++ rust/micro-web/src/main.rs
@@ -1,6 +1,7 @@
-use http::StatusCode;
-use micro_web::router::{get, post, Router};
-use micro_web::{handler_fn, PathParams, Server};
+use micro_web::date::DateServiceDecorator;
+use micro_web::responder::Responder;
+use micro_web::router::{Router, get, post};
+use micro_web::{PathParams, Server, handler_fn, responder};
 
 async fn empty_body() -> &'static str {
     ""
@@ -10,28 +11,24 @@ async fn echo_uid<'s, 'r>(path_params: &PathParams<'s, 'r>) -> String {
     path_params.get("id").map(|s| s.to_owned()).unwrap()
 }
 
-async fn default_handler() -> (&'static str, StatusCode) {
-    ("404 not found", StatusCode::NOT_FOUND)
+async fn default_handler() -> impl Responder {
+    responder::NotFound
 }
+
+
 
 #[tokio::main]
 async fn main() {
+    
     // Build router with multiple routes and handlers
     let router = Router::builder()
         .route("/", get(handler_fn(empty_body)))
         .route("/user", post(handler_fn(empty_body)))
-        // POST route for JSON data with content-type filter
         .route("/user/{id}", get(handler_fn(echo_uid)))
-        // Add response encoding wrapper
+        .with_global_decorator(DateServiceDecorator)
         .build();
 
     // Configure and start the server
-    Server::builder()
-        .router(router)
-        .bind("0.0.0.0:3000")
-        .default_handler(handler_fn(default_handler))
-        .build()
-        .unwrap()
-        .start()
-        .await;
+    Server::builder().router(router).bind("0.0.0.0:3000").default_handler(handler_fn(default_handler)).build().unwrap().start().await;
 }
+
