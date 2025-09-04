144bd365bb6a8c6f0fdeb283fa10e0b68f03ccb1
Hubert Shelley <46239302+hubertshelley@users.noreply.github.com>
2025-09-04 13:05:34 +0800

    fix(silent): change port to 3000

--- rust/silent/src/main.rs
+++ rust/silent/src/main.rs
@@ -9,12 +9,13 @@ async fn main() {
     let route = Route::new("").get(index).append(
         Route::new("user")
             .append(
-                Route::new("<id>").get(|req| async move { req.get_path_params::<String>("id") }),
+                Route::new("<id>")
+                    .get(|req: Request| async move { req.get_path_params::<String>("id") }),
             )
             .post(index),
     );
     Server::new()
-        .bind("0.0.0.0:30001".parse().unwrap())
+        .bind("0.0.0.0:3000".parse().unwrap())
         .serve(route)
         .await;
 }
