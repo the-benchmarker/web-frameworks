138193eba054a129b05c3cd0da0f13ee698f760f
waghanza@gmail.com <Marwan Rabba>
2025-09-21 21:02:57 +0200

    update

--- javascript/brahma-firelight/cluster.ts
+++ javascript/brahma-firelight/cluster.ts
@@ -1,4 +1,4 @@
 // using high speed native addon written on top of Tokio And Hyper.
 // So no need cluster brahma-firelight runs multi-threaded by default 🔥
 
-import "./app.ts";
+import './app.ts';
