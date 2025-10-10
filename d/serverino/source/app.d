b4b2a85eed86b9e33fce528e9c10f352d21e1046
Sergei Giniatulin <kornburn@gmail.com>
2025-10-11 01:11:05 +0300

    [D] Fixed configs

--- d/serverino/source/app.d
+++ d/serverino/source/app.d
@@ -4,6 +4,7 @@ import serverino;
 import std.datetime: Duration, seconds;
 import std.array: split;
 import std.algorithm: startsWith;
+import std.parallelism: totalCPUs;
 
 mixin ServerinoMain;
 
@@ -14,7 +15,7 @@ mixin ServerinoMain;
         .setHttpTimeout(10.seconds)
         .enableKeepAlive(180.seconds)
         .addListener("0.0.0.0", 3000)
-        .setDaemonInstances(8)
+        .setDaemonInstances(totalCPUs)
         .setWorkers(4);
 }
 
