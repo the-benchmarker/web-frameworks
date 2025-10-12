324e5debf4a7014aab2ecb77ba4402bc4d4efdbb
waghanza@gmail.com <Marwan Rabba>
2025-10-12 17:51:31 +0200

    fix express and blade

--- java/blade/src/main/java/com/example/Application.java
+++ java/blade/src/main/java/com/example/Application.java
@@ -1,29 +1,32 @@
 package com.example;
 
 import com.hellokaton.blade.Blade;
+import com.hellokaton.blade.mvc.RouteContext;
 
 public class Application {
     public static void main(String[] args) {
         Blade.of()
-            // GET /
-            .get("/", ctx -> ctx.text(""))
-
-            // GET /user/:id
-            .get("/user/:id", ctx -> {
-		    Integer id = ctx.pathInt("id");
-                if (id == null) {
-                    ctx.status(400).text("Bad Request: missing or invalid id");
-                } else {
-                    ctx.text(id.toString());
-                }
-
-            })
-
-            // POST /user
-            .post("/user", ctx -> ctx.text(""))
-
-            // start app
+            .get("/", Application::index)
+            .get("/user/:id", Application::getUser)
+            .post("/user", Application::createUser)
             .start(Application.class, args);
+    }
+
+    private static void index(RouteContext ctx) {
+        ctx.text("");
+    }
+
+    private static void getUser(RouteContext ctx) {
+        Integer id = ctx.pathInt("id");
+        if (id == null) {
+            ctx.status(400).text("Bad Request: missing or invalid id");
+        } else {
+            ctx.text(id.toString());
+        }
+    }
+
+    private static void createUser(RouteContext ctx) {
+        ctx.text("");
     }
 }
 
