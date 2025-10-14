b84d39b7e4ad3cb65fd80719fa07624582baff3e
Satoshi Namai <satoshi.namai.01.ts@mixi.co.jp>
2025-10-14 20:26:13 +0100

    Remove outdated compiler option

--- swift/hummingbird-framework/Package.swift
+++ swift/hummingbird-framework/Package.swift
@@ -15,12 +15,6 @@ let package = Package(
             dependencies: [
                 .product(name: "Hummingbird", package: "hummingbird"),
             ],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
         ),
     ]
 )
