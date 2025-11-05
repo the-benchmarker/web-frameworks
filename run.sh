135393a290ef36e220c1106e8a1bea6087281d82
waghanza@gmail.com <waghanza@gmail.com>
2025-11-05 11:43:38 +0100

    update

--- run.sh
+++ run.sh
@@ -1,27 +1,18 @@
-#docker-clean && digitalocean-clean && rm -fr .neph && mkdir -p .neph/{c,clojure,cpp,crystal,csharp,dart,elixir,fsharp,go,haskell,java,javascript,julia,kotlin,nim,pony,python,ruby,rust,scala,swift} && dropdb -U postgres benchmark && createdb -U postgres benchmark && psql -U postgres benchmark < .ci/dump.sql && PROVIDER=digitalocean rake config && bin/neph --seq 
-
-#. .env/default
-#. .env/development
-#
-#doctl compute droplet create sieger --region ${DO_REGION} --image ${DO_IMAGE} --size ${DO_SIZE} --wait --ssh-keys ${DO_FINGERPRINT} --enable-private-networking
-#sleep 30
-#IP=$(doctl compute droplet list sieger --format PublicIPv4 --no-header)
-#rsync -avz -e "ssh -i ${DO_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress * root@${IP}:/root
-#scp -i ${DO_KEY} -r .ci root@${IP}:/root/
-#scp -i ${DO_KEY} -r .env root@${IP}:/root/.env
-#scp -i ${DO_KEY} ${DO_KEY} root@${IP}:${DO_KEY}
-#doctl compute ssh --ssh-key-path ${DO_KEY} sieger
-
 BASEDIR=`pwd`
 
-# Clean database
-#dropdb -U postgres benchmark
-#createdb -U postgres benchmark
-#psql -U postgres -d benchmark < dump.sql
-
-find $1 -mindepth 1 -type f -name config.yaml > ~/list.txt
-#find $1 -mindepth 2 -type f -name config.yaml > ~/list.txt
-#find . -mindepth 3 -type f -name config.yaml > ~/list.txt
+if [ "$#" -eq 0 ]; then
+	find . -mindepth 3 -type f -name config.yaml > ~/list.txt
+else
+	COUNT=`echo $1 | grep -c "/"`
+	if [ "$COUNT" -eq 0 ]; then
+		find $1 -mindepth 2 -type f -name config.yaml > ~/list.txt
+	elif [ "$COUNT" -eq 1 ] ; then
+		find $1 -mindepth 1 -type f -name config.yaml > ~/list.txt
+	else
+		echo "Illegal number of parameters" >&2
+		exit 2
+	fi
+fi
 
 while read line ; do 
   echo "*********** ${line} *************"
