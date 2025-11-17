ac4f1361bf3c4f679226f5a38d12960b88d4b902
waghanza@gmail.com <waghanza@gmail.com>
2025-11-17 21:40:25 +0100

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 BASEDIR=`pwd`
 
 if [ "$#" -eq 0 ]; then
-	find . -mindepth 3 -type f -name config.yaml | grep -Ev 'guildenstern' > ~/list.txt
+	find . -mindepth 3 -type f -name config.yaml > ~/list.txt
 else
 	COUNT=`echo $1 | grep -c "/"`
 	if [ "$COUNT" -eq 0 ]; then
