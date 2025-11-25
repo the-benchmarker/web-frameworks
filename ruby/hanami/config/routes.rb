the-benchmarker/web-frameworks

commit 7ea2f0b68bac17ad2fa6014dff77e2e68238da4e

update hanami

ruby/hanami/config/routes.rb
@@ -2,14 +2,10 @@ require "hanami/routes"
 
 module Benchmark
   class Routes < Hanami::Routes
-    root to: ->(*) { [200, {}, [""]] }
+    root to: "index.empty"
 
-    get "/user/:id" do
-      params[:id]
-    end
+    get "/user/:id", to: "user.get"
 
-    post "/user" do
-      ""
-    end
+    post "/user", to "index.empty"
   end
 end

.tasks/ci.rake
@@ -16,6 +16,7 @@ namespace :ci do
       next if file.start_with?('.')
       next if file.count(File::SEPARATOR) < 2
       next unless File.exist?(file)
+      next unless lange == 'ruby'
 
       language, framework, = file.split(File::SEPARATOR)
 

Gemfile
@@ -16,3 +16,5 @@ group :development, :test do
   gem "rubocop-rake"
   gem "rubocop-rspec"
 end
+
+gem "hanami", "~> 2.3"

ruby/hanami/app/actions/index/empty.rb
@@ -1,0 +1,11 @@
+module Benchmark
+  module Actions
+    module Home
+      class Index < Benchmark::Action
+        def handle(request, response)
+          response.body = ""
+        end
+      end
+    end
+  end
+end

ruby/hanami/app/actions/user/get.rb
@@ -1,0 +1,11 @@
+module Benchmark
+  module Actions
+    module Home
+      class Index < Benchmark::Action
+        def handle(request, response)
+          response.body = request.params[:id]
+        end
+      end
+    end
+  end
+end

commit 8a2805f9ddcf04a49172d48f3d0e61480af16d0d

Fix syntax error in Hanami routes
This was introduced by accident in https://github.com/the-benchmarker/web-frameworks/commit/7ea2f0b68bac17ad2fa6014dff77e2e68238da4e.

ruby/hanami/config/routes.rb
@@ -6,6 +6,6 @@ module Benchmark
 
     get "/user/:id", to: "user.get"
 
-    post "/user", to "index.empty"
+    post "/user", to: "index.empty"
   end
 end