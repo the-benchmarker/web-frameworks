cd000234ca87dbe17d4bcd4ef03bd35c6b35ef05
waghanza@gmail.com <waghanza@gmail.com>
2025-12-21 15:11:06 +0100

    remove yii with swoole

--- php/yii-swoole/config/swoole.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config = require __DIR__ . '/web.php';
-
-$config['components']['response']['class'] = swoole\foundation\web\Response::class;
-$config['components']['request']['class'] = swoole\foundation\web\Request::class;
-$config['components']['errorHandler']['class'] = swoole\foundation\web\ErrorHandler::class;
-
-return $config;
