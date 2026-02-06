b97867cd337905a18654641886fbe643d46d3089
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2026-02-06 23:43:23 +0330

    Update Program.cs

--- csharp/codebehind/Program.cs
+++ csharp/codebehind/Program.cs
@@ -2,10 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(c => c.AddServerHeader = false);
 builder.Logging.ClearProviders();
 
+builder.Services.AddCodeBehind();
+
 var app = builder.Build();
 
-SetCodeBehind.CodeBehindCompiler.Initialization(true);
-
-app.UseCodeBehindRoute();
+app.UseCodeBehindRouteAsync();
 
 app.Run();
+
