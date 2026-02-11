378bb5e1ef401bd6737a64abbfa818482a9ad636
尤雨东 <root@ltpp.vip>
2026-02-11 09:00:33 +0800

    feat: v0.1.0

--- rust/hyperlane/src/main.rs
+++ rust/hyperlane/src/main.rs
@@ -1,13 +1,13 @@
 use hyperlane::*;
 
-pub(crate) async fn init_server_config() -> ServerConfig {
+async fn init_server_config() -> ServerConfig {
     let server_config: ServerConfig = ServerConfig::new().await;
     server_config.port(3000).await;
     server_config.disable_nodelay().await;
     server_config
 }
 
-pub(crate) async fn init_request_config() -> RequestConfig {
+async fn init_request_config() -> RequestConfig {
     RequestConfig::low_security().await
 }
 
