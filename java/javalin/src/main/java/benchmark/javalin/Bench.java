20c4804a939510fe513de98c0e2444ebbfe9b1ba
waghanza@gmail.com <waghanza@gmail.com>
2026-03-08 15:54:45 +0100

    fix javalin

--- java/javalin/src/main/java/benchmark/javalin/Bench.java
+++ java/javalin/src/main/java/benchmark/javalin/Bench.java
@@ -2,11 +2,13 @@ package benchmark.javalin;
 
 import io.javalin.Javalin;
 
+
 public class Bench {
   public static void main(String[] args) {
-    Javalin app = Javalin.create().start(3000);
-    app.get("/", ctx -> ctx.result(""));
-    app.get("/user/{id}", ctx -> ctx.result(ctx.pathParam("id")));
-    app.post("/user", ctx -> ctx.result(""));
+	  var app = Javalin.create(config -> {
+    config.routes.get("/", ctx -> ctx.result(""));
+    config.routes.get("/user/{id}", ctx -> ctx.result(ctx.pathParam("id")));
+    config.routes.post("/user", ctx -> ctx.result(""));
+        }).start(3000);
   }
 }
