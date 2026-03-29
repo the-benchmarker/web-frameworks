b70e5d804e10ed364c5651afaf50edbc411c7c39
waghanza@gmail.com <waghanza@gmail.com>
2026-03-29 19:32:03 +0200

    update

--- python/fastpysgi-asgi/server.py
+++ python/fastpysgi-asgi/server.py
@@ -1,45 +1,61 @@
 import fastpysgi
 
+
 async def app(scope, receive, send):
-    if scope['type'] != 'http':
+    if scope["type"] != "http":
         return
 
-    path = scope['path']
-    req_method = scope.get('method', '')
-    headers = [ ]
+    path = scope["path"]
+    req_method = scope.get("method", "")
+    headers = []
 
     if req_method == "GET":
         if path == "/":
-            await send( { 'type': 'http.response.start', 'status': 200, 'headers': [ ] } )
-            await send( { 'type': 'http.response.body', 'body': b"", 'more_body': False } )
+            await send({"type": "http.response.start", "status": 200, "headers": []})
+            await send({"type": "http.response.body", "body": b"", "more_body": False})
             return
 
         if path.startswith("/user/"):
             value = path[6:]
-            await send( { 'type': 'http.response.start', 'status': 200, 'headers': [[ b'Content-Type', b'text/plain; charset=utf-8' ]] } )
-            await send( { 'type': 'http.response.body', 'body': value.encode(), 'more_body': False } )
+            await send(
+                {
+                    "type": "http.response.start",
+                    "status": 200,
+                    "headers": [[b"Content-Type", b"text/plain; charset=utf-8"]],
+                }
+            )
+            await send(
+                {
+                    "type": "http.response.body",
+                    "body": value.encode(),
+                    "more_body": False,
+                }
+            )
             return
 
     if req_method == "POST":
         if path == "/user":
-            await send( { 'type': 'http.response.start', 'status': 200, 'headers': [ ] } )
-            await send( { 'type': 'http.response.body', 'body': b"", 'more_body': False } )
+            await send({"type": "http.response.start", "status": 200, "headers": []})
+            await send({"type": "http.response.body", "body": b"", "more_body": False})
             return
 
-    await send( { 'type': 'http.response.start', 'status': 404, 'headers': [ ] } )
-    await send( { 'type': 'http.response.body', 'body': b"", 'more_body': False } )
+    await send({"type": "http.response.start", "status": 404, "headers": []})
+    await send({"type": "http.response.body", "body": b"", "more_body": False})
+
 
 if __name__ == "__main__":
     import optparse
-    parser = optparse.OptionParser("usage: %prog [options]", add_help_option = False)
-    parser.add_option("-h", "--host", dest="host", default='0.0.0.0', type="string")
+
+    parser = optparse.OptionParser("usage: %prog [options]", add_help_option=False)
+    parser.add_option("-h", "--host", dest="host", default="0.0.0.0", type="string")
     parser.add_option("-p", "--port", dest="port", default=3000, type="int")
     parser.add_option("-w", "--workers", dest="workers", default=0, type="int")
-    (opt, args) = parser.parse_args() 
+    (opt, args) = parser.parse_args()
 
     workers = opt.workers
     if workers <= 0:
         import multiprocessing
+
         workers = multiprocessing.cpu_count()
 
-    fastpysgi.run(app, opt.host, opt.port, workers = workers)
+    fastpysgi.run(app, opt.host, opt.port, workers=workers)
--- python/fastpysgi-wsgi/server.py
+++ python/fastpysgi-wsgi/server.py
@@ -1,39 +1,43 @@
 import fastpysgi
 
+
 def app(env, start_response):
     path = env["PATH_INFO"]
-    req_method = env.get('REQUEST_METHOD', '')
-    headers = [ ]
+    req_method = env.get("REQUEST_METHOD", "")
+    headers = []
 
     if req_method == "GET":
         if path == "/":
-            start_response('200 OK', headers)
-            return [ b"" ]
+            start_response("200 OK", headers)
+            return [b""]
 
         if path.startswith("/user/"):
             value = path[6:]
-            start_response('200 OK', [ ( 'Content-Type', 'text/plain; charset=utf-8' ) ])
-            return [ value.encode() ]
+            start_response("200 OK", [("Content-Type", "text/plain; charset=utf-8")])
+            return [value.encode()]
 
     if req_method == "POST":
         if path == "/user":
-            start_response('200 OK', headers)
-            return [ b"" ]
+            start_response("200 OK", headers)
+            return [b""]
 
-    start_response('404 Not Found', headers)
-    return [ b"" ]
+    start_response("404 Not Found", headers)
+    return [b""]
+
 
 if __name__ == "__main__":
     import optparse
-    parser = optparse.OptionParser("usage: %prog [options]", add_help_option = False)
-    parser.add_option("-h", "--host", dest="host", default='0.0.0.0', type="string")
+
+    parser = optparse.OptionParser("usage: %prog [options]", add_help_option=False)
+    parser.add_option("-h", "--host", dest="host", default="0.0.0.0", type="string")
     parser.add_option("-p", "--port", dest="port", default=3000, type="int")
     parser.add_option("-w", "--workers", dest="workers", default=0, type="int")
-    (opt, args) = parser.parse_args() 
+    (opt, args) = parser.parse_args()
 
     workers = opt.workers
     if workers <= 0:
         import multiprocessing
+
         workers = multiprocessing.cpu_count()
 
-    fastpysgi.run(app, opt.host, opt.port, workers = workers)
+    fastpysgi.run(app, opt.host, opt.port, workers=workers)
