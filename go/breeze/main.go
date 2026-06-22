1da675975a9b4395eceb45d387061460d34dcc07
nelthaarion <50423655+nelthaarion@users.noreply.github.com>
2026-06-22 18:50:13 +0330

    route mode added

--- go/breeze/main.go
+++ go/breeze/main.go
@@ -9,12 +9,25 @@ import (
 func main() {
 	router := breeze.NewRouter()
 
-	router.Handle(breeze.GET, "/", func(ctx *breeze.Context) { ctx.WriteString("") })
+	router.Handle(breeze.GET, "/", func(ctx *breeze.Context) {
+		ctx.Status(200)
+		ctx.WriteString("")
+	})
 
 	router.Handle(breeze.GET, "/user/", func(ctx *breeze.Context) {
+		ctx.Status(200)
+		ctx.WriteString("")
+	})
+	router.Handle(breeze.GET, "/user/:id", func(ctx *breeze.Context) {
+		ctx.Status(200)
+		ctx.WriteString(ctx.GetParam("id"))
+	})
+	router.Handle(breeze.POST, "/user/", func(ctx *breeze.Context) {
+		ctx.Status(200)
 		ctx.WriteString("")
 	})
 	router.Handle(breeze.POST, "/user/:id", func(ctx *breeze.Context) {
+		ctx.Status(200)
 		ctx.WriteString(ctx.GetParam("id"))
 	})
 	app := breeze.New(router, breeze.NewWorkerPool(runtime.NumCPU()))
