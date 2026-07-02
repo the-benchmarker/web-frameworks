35c2352b3d06625e7786622f015c284f6f73cb6e
Marwan <waghanza@gmail.com>
2026-07-02 22:48:46 +0200

    update

--- swift/hummingbird-framework/Package.swift
+++ swift/hummingbird-framework/Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version:6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// The swift-tools-version declares the minimum version of Swift required to build 
 import PackageDescription
 
 let package = Package(
