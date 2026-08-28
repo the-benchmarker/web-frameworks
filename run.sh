2dffd2559e4baf8b28e6fa53df9669fcc439a760
waghanza@gmail.com <waghanza@gmail.com>
2026-08-28 20:55:09 +0200

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 BASEDIR=`pwd`
 
 if [ "$#" -eq 0 ]; then
-	find . -mindepth 3 -type f -name config.yaml | grep -Ev 'imi-swoole' > ~/list.txt
+	find . -mindepth 3 -type f -name config.yaml | grep -Ev 'imi-swoole|guildenstern' > ~/list.txt
 else
 	COUNT=`echo $1 | grep -c "/"`
 	if [ "$COUNT" -eq 0 ]; then
