8ae370ae89075281c056a138e57de25682d36e02
Marwan Rabbâa <waghanza@gmail.com>
2026-09-20 11:56:22 +0200

    update

--- d/serverino/source/app.d
+++ d/serverino/source/app.d
@@ -21,10 +21,10 @@ mixin ServerinoMain;
 
 @safe
 @endpoint void hello(Request req, Output output) {
-    if (req.uri == "/" && req.method == Request.Method.Get)
+    if (req.path == "/" && req.method == Request.Method.Get)
         output.status = 200;
-    else if (req.uri == "/user" && req.method == Request.Method.Post)
+    else if (req.path == "/user" && req.method == Request.Method.Post)
         output.status = 200;
-    else if (req.uri.startsWith("/user/") && req.method == Request.Method.Get)
+    else if (req.path.startsWith("/user/") && req.method == Request.Method.Get)
         output ~= req.uri[6..$];
 }
